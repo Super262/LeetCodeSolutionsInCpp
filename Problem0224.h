162e767cc296d7bccd2293f7f65f3178ea899802
fengwei <wuzhongfengwei@163.com>
2021-09-26 21:09:43 -0700

    Finished Problem 224.

---
+++ Problem0224.h
@@ -0,0 +1,51 @@
+//
+// Created by Fengwei Zhang on 9/26/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0224_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0224_H
+
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+class Problem0224 {
+private:
+    int calculate(string s) {
+        stack<int> ops;
+        ops.push(1);
+        int sign = 1;
+
+        int ret = 0;
+        int n = s.length();
+        int i = 0;
+        while (i < n) {
+            if (s[i] == ' ') {
+                i++;
+            } else if (s[i] == '+') {
+                sign = ops.top();
+                i++;
+            } else if (s[i] == '-') {
+                sign = -ops.top();
+                i++;
+            } else if (s[i] == '(') {
+                ops.push(sign);
+                i++;
+            } else if (s[i] == ')') {
+                ops.pop();
+                i++;
+            } else {
+                long num = 0;
+                while (i < n && s[i] >= '0' && s[i] <= '9') {
+                    num = num * 10 + s[i] - '0';
+                    i++;
+                }
+                ret += sign * num;
+            }
+        }
+        return ret;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0224_H
