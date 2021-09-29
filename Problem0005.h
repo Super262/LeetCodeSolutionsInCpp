17d4243f54df6dfcf98c7619e7fc4cc0546c7de0
fengwei <wuzhongfengwei@163.com>
2021-09-29 00:05:30 -0700

    Finished Problem 5.

---
+++ Problem0005.h
@@ -0,0 +1,39 @@
+//
+// Created by Fengwei Zhang on 9/28/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0005_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0005_H
+
+#include <string>
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+class Problem0005 {
+private:
+    string longestPalindrome(const string &s) {
+        bool dp[s.size()][s.size()];  // s[i, j]是回文串，dp[i][j] == true（i，j从0开始）
+        memset(dp, 0, sizeof dp);
+        string result;
+        for (int length = 1; length <= (int) s.size(); ++length) {
+            for (int l = 0; l + length - 1 < (int) s.size(); ++l) {
+                auto r = l + length - 1;
+                if (length == 1) {
+                    dp[l][r] = true;
+                } else if (length == 2) {
+                    dp[l][r] = s[l] == s[r];
+                } else {
+                    dp[l][r] = s[l] == s[r] && dp[l + 1][r - 1];
+                }
+                if (dp[l][r] && r - l + 1 > (int) result.size()) {
+                    result = s.substr(l, r - l + 1);
+                }
+            }
+        }
+        return result;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0005_H
