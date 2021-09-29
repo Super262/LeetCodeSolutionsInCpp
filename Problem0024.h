1f060b3f7ac6102332ab76ccd8edcd3bbf8fb127
fengwei <wuzhongfengwei@163.com>
2021-09-29 10:16:40 -0700

    Finished Problem 24.

---
+++ Problem0024.h
@@ -0,0 +1,36 @@
+//
+// Created by Fengwei Zhang on 9/29/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0024_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0024_H
+
+class Problem0024 {
+private:
+    struct ListNode {
+        int val;
+        ListNode *next;
+
+        ListNode() : val(0), next(nullptr) {}
+
+        ListNode(int x) : val(x), next(nullptr) {}
+
+        ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+
+    ListNode *swapPairs(ListNode *head) {
+        auto dummy = new ListNode(-1);
+        dummy->next = head;
+        for (auto p = dummy; p->next && p->next->next;) {
+            auto a = p->next;
+            auto b = a->next;
+            a->next = b->next;
+            b->next = a;
+            p->next = b;
+            p = a;
+        }
+        return dummy->next;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0024_H
