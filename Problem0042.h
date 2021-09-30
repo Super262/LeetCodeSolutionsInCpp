6141fdd56818a2e6e548b5b5541a377e58bcea42
fengwei <wuzhongfengwei@163.com>
2021-09-30 01:59:57 -0700

    Finished Problem 42.

---
+++ Problem0042.h
@@ -0,0 +1,33 @@
+//
+// Created by Fengwei Zhang on 9/30/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0042_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0042_H
+
+#include <vector>
+
+using namespace std;
+
+class Problem0042 {
+private:
+    int trap(const vector<int> &height) {
+        stack<int> stk;  // 单调栈，体积递减
+        int result = 0;
+        for (int i = 0; i < (int) height.size(); ++i) {
+            int prevH = 0;
+            while (!stk.empty() && height[i] >= height[stk.top()]) {
+                result += (i - stk.top() - 1) * (height[stk.top()] - prevH);
+                prevH = height[stk.top()];
+                stk.pop();
+            }
+            if (!stk.empty()) {
+                result += (i - stk.top() - 1) * (height[i] - prevH);
+            }
+            stk.emplace(i);
+        }
+        return result;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0042_H
