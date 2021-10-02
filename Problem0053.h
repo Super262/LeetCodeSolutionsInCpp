dd38dffa777629be02f84c41fa47d05769f3c5a7
fengwei <wuzhongfengwei@163.com>
2021-10-02 12:01:53 -0700

    Finished Problem 53.

---
+++ Problem0053.h
@@ -0,0 +1,21 @@
+//
+// Created by Fengwei Zhang on 10/2/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0053_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0053_H
+
+class Problem0053 {
+public:
+    int maxSubArray(const vector<int> &nums) {
+        auto currentMax = nums[0];
+        auto result = currentMax;
+        for (int i = 1; i < (int) nums.size(); ++i) {
+            currentMax = max(nums[i], currentMax + nums[i]);
+            result = max(result, currentMax);
+        }
+        return result;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0053_H
