c58417b5452154b587b83e88ad523c5ab755fd55
fengwei <wuzhongfengwei@163.com>
2021-10-03 11:35:44 -0700

    Finished Problem 91.

---
+++ Problem0091.h
@@ -0,0 +1,38 @@
+//
+// Created by Fengwei Zhang on 10/3/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0091_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0091_H
+
+#include <vector>
+#include <stack>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+class Problem0091 {
+public:
+    int numDecodings(string s) {
+        int dp[s.size() + 1];
+        dp[0] = 1;
+        for (int len = 1; len <= (int) s.size(); ++len) {
+            dp[len] = 0;
+            if (s[len - 1] >= '1' && s[len - 1] <= '9') {
+                dp[len] += dp[len - 1];
+            }
+            if (len == 1) {
+                continue;
+            }
+            int num = (int) (s[len - 2] - '0') * 10 + (int) (s[len - 1] - '0');
+            if (num < 10 || num > 26) {
+                continue;
+            }
+            dp[len] += dp[len - 2];
+        }
+        return dp[s.size()];
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0091_H
