ef7facabdb43c5a5ebacfa58942baa1b0344d8f9
fengwei <wuzhongfengwei@163.com>
2021-10-02 17:06:32 -0700

    Finished Problem 66.

---
+++ Problem0066.h
@@ -0,0 +1,32 @@
+//
+// Created by Fengwei Zhang on 10/2/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0066_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0066_H
+
+#include <vector>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+class Problem0066 {
+public:
+    vector<int> plusOne(vector<int> &digits) {
+        reverse(digits.begin(), digits.end());
+        int t = 1;
+        for (auto &d: digits) {
+            t += d;
+            d = t % 10;
+            t /= 10;
+        }
+        if (t) {
+            digits.emplace_back(1);
+        }
+        reverse(digits.begin(), digits.end());
+        return digits;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0066_H
