e6cc277789db347729c0712b106b228ff1d1b8da
fengwei <wuzhongfengwei@163.com>
2021-10-03 09:14:28 -0700

    Finished Problem 83.

---
+++ Problem0083.h
@@ -0,0 +1,44 @@
+//
+// Created by Fengwei Zhang on 10/3/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0083_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0083_H
+
+#include <vector>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+class Solution {
+private:
+    struct ListNode {
+        int val;
+        ListNode *next;
+
+        ListNode() : val(0), next(nullptr) {}
+
+        ListNode(int x) : val(x), next(nullptr) {}
+
+        ListNode(int x, ListNode *next) : val(x), next(next) {}
+    };
+
+    ListNode *deleteDuplicates(ListNode *head) {
+        if (!head) {
+            return head;
+        }
+        auto current = head;
+        for (auto p = current->next; p; p = p->next) {
+            if (p->val == current->val) {
+                continue;
+            }
+            current->next = p;
+            current = p;
+        }
+        current->next = nullptr;  // 将新链表从原链表中分离
+        return head;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0083_H
