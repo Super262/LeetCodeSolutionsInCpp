0188d2136c8b426ad81a928b94ffd678a0dfd305
fengwei <wuzhongfengwei@163.com>
2021-10-17 09:25:06 -0700

    Finished Problem 190.

---
+++ Problem0190.h
@@ -0,0 +1,20 @@
+//
+// Created by Fengwei Zhang on 10/17/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0190_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0190_H
+
+class Problem0190 {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t result = 0;
+        for (int i = 0; i < 32; ++i) {
+            result <<= 1;
+            result += (n >> i) & 1;
+        }
+        return result;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0190_H
