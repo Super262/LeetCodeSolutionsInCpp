3c9ed5387d8e3dfa8dd35a542c261722d459bc8c
fengwei <wuzhongfengwei@163.com>
2021-10-17 10:00:18 -0700

    Finished Problem 202.

---
+++ Problem0202.h
@@ -0,0 +1,30 @@
+//
+// Created by Fengwei Zhang on 10/17/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0202_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0202_H
+
+class Problem0202 {
+public:
+    int squareSum(int x) {
+        int result = 0;
+        while (x) {
+            result += (x % 10) * (x % 10);
+            x /= 10;
+        }
+        return result;
+    }
+
+    bool isHappy(int n) {  // 类似于142题（Linked List Cycle II）
+        auto slowP = n;
+        auto fastP = squareSum(slowP);
+        while (fastP != slowP) {
+            slowP = squareSum(slowP);
+            fastP = squareSum(squareSum(fastP));
+        }
+        return fastP == 1;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0202_H
