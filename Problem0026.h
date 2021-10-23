6c63ee1da27eee43edcf62ae68791162aea9c7ab
fengwei <wuzhongfengwei@163.com>
2021-10-23 16:53:16 -0700

    Finished Problem 26.

---
+++ Problem0026.h
@@ -0,0 +1,27 @@
+//
+// Created by Fengwei Zhang on 10/23/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0026_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0026_H
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Problem0026 {
+public:
+    int removeDuplicates(vector<int> &nums) {
+        int k = 0;
+        for (int i = 0; i < (int) nums.size(); ++i) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            nums[k++] = nums[i];
+        }
+        return k;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0026_H
