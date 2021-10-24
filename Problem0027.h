09a28241ab1136b16d20a2739038885e33cdb822
fengwei <wuzhongfengwei@163.com>
2021-10-23 17:53:05 -0700

    Finished Problem 27.

---
+++ Problem0027.h
@@ -0,0 +1,27 @@
+//
+// Created by Fengwei Zhang on 10/23/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0027_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0027_H
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Problem0027 {
+public:
+    int removeElement(vector<int> &nums, const int &val) {
+        int k = 0;
+        for (int i = 0; i < (int) nums.size(); ++i) {
+            if (nums[i] == val) {
+                continue;
+            }
+            nums[k++] = nums[i];
+        }
+        return k;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0027_H
