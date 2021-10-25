3c00dce0b3108abdb17892b4c33aa4e0283203af
fengwei <wuzhongfengwei@163.com>
2021-10-25 10:31:30 -0700

    Finished Problem 50.

---
+++ Problem0050.h
@@ -0,0 +1,25 @@
+//
+// Created by Fengwei Zhang on 10/25/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0050_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0050_H
+
+class Problem0050 {
+public:
+    double myPow(double x, int n) {  // 快速幂，背诵
+        double res = 1;
+        for (long long k = abs((long long) n); k > 0; k >>= 1) {
+            if (k & 1) {
+                res *= x;
+            }
+            x *= x;
+        }
+        if (n < 0) {
+            return 1 / res;
+        }
+        return res;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0050_H
