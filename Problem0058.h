bf340ba75583ce6e868aab3512286cf9723d1935
fengwei <wuzhongfengwei@163.com>
2021-10-25 23:57:31 -0700

    Finished Problem 58.

---
+++ Problem0058.h
@@ -0,0 +1,28 @@
+//
+// Created by Fengwei Zhang on 10/25/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0058_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0058_H
+
+#include <string>
+
+using namespace std;
+
+class Problem0058 {
+    int lengthOfLastWord(const string &s) {
+        for (int i = (int) s.size() - 1; i >= 0; --i) {
+            if (s[i] == ' ') {
+                continue;
+            }
+            auto j = i;
+            while (j >= 0 && s[j] != ' ') {
+                --j;
+            }
+            return i - j;
+        }
+        return 0;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0058_H
