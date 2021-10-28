5559928be2c4f2565d6ab25de4c99db3113aa711
fengwei <wuzhongfengwei@163.com>
2021-10-28 03:12:28 -0700

    Finished Problem 80.

---
+++ Problem0080.h
@@ -0,0 +1,25 @@
+//
+// Created by Fengwei Zhang on 10/28/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0080_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0080_H
+
+#include <vector>
+
+using namespace std;
+
+class Problem0080 {
+public:
+    int removeDuplicates(vector<int> &nums) {
+        int k = 0;
+        for (auto x: nums) {
+            if (k < 2 || nums[k - 1] != x || nums[k - 2] != x) {
+                nums[k++] = x;
+            }
+        }
+        return k;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0080_H
