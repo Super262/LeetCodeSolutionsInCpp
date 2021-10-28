f7421baf7a55cf50c1922e62d4cfab187d50ec4b
fengwei <wuzhongfengwei@163.com>
2021-10-28 00:31:04 -0700

    Finished Problem 75.

---
+++ Problem0075.h
@@ -0,0 +1,31 @@
+//
+// Created by Fengwei Zhang on 10/28/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0075_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0075_H
+
+#include <vector>
+
+using namespace std;
+
+class Problem0075 {
+public:
+    void sortColors(vector<int> &nums) {
+        int i = 0, j = 0, k = (int) nums.size() - 1;
+        while (j <= k) {
+            if (nums[j] == 0) {
+                swap(nums[i], nums[j]);
+                i++;
+                j++;
+            } else if (nums[j] == 1) {
+                j++;
+            } else {
+                swap(nums[j], nums[k]);
+                k--;
+            }
+        }
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0075_H
