47a3daeb20835ddb9013083de87fbe4f3bb590e4
fengwei <wuzhongfengwei@163.com>
2021-11-02 02:02:45 -0700

    Finished Problem 100.

---
+++ Problem0100.h
@@ -0,0 +1,36 @@
+//
+// Created by Fengwei Zhang on 11/2/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0100_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0100_H
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Problem0100 {
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        if (!p && !q) {
+            return true;
+        }
+        if (!p || !q) {
+            return false;
+        }
+        if (p->val != q->val) {
+            return false;
+        }
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0100_H
