e0b092657ddc326771ae97fb6203ab90133d4e08
fengwei <wuzhongfengwei@163.com>
2021-11-06 16:42:57 -0700

    Finished Problem 114.

---
+++ Problem0114.h
@@ -0,0 +1,38 @@
+//
+// Created by Fengwei Zhang on 11/6/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0114_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0114_H
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Problem0114 {
+public:
+    void flatten(TreeNode *root) {
+        while (root) {
+            auto p = root->left;
+            if (p) {
+                while (p->right) {
+                    p = p->right;
+                }
+                p->right = root->right;
+                root->right = root->left;
+                root->left = nullptr;
+            }
+            root = root->right;
+        }
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0114_H
