3d111175137fc76308fa5354eb7d9fc99661754e
fengwei <wuzhongfengwei@163.com>
2021-11-07 13:15:04 -0800

    Finished Problem 122.

---
+++ Problem0122.h
@@ -0,0 +1,21 @@
+//
+// Created by Fengwei Zhang on 11/7/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0122_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0122_H
+
+class Problem0122 {
+public:
+    int maxProfit(const vector<int> &prices) {  // 一段时间内的交易 == 多个单天的交易
+        int result = 0;
+        for (int i = 1; i < (int) prices.size(); ++i) {
+            if (prices[i] > prices[i - 1]) {
+                result += prices[i] - prices[i - 1];
+            }
+        }
+        return result;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0122_H
