46d70df6986c2929bc910d7ba8ef92351b951317
fengwei <wuzhongfengwei@163.com>
2021-11-07 15:34:58 -0800

    Finished Problem 125.

---
+++ Problem0125.h
@@ -0,0 +1,48 @@
+//
+// Created by Fengwei Zhang on 11/7/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0125_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0125_H
+
+#include <string>
+
+using namespace std;
+
+class Problem0125 {
+public:
+    bool isPalindrome(const string &s) {
+        int l = 0;
+        int r = (int) s.size() - 1;
+        while (l < r) {
+            while (l < r && !checkCh(s[l])) {
+                ++l;
+            }
+            while (l < r && !checkCh(s[r])) {
+                --r;
+            }
+            if (l < r && tolower(s[l]) != tolower(s[r])) {
+                return false;
+            }
+            ++l;
+            --r;
+        }
+        return true;
+    }
+
+    bool checkCh(const char &ch) {
+        if (ch >= '0' && ch <= '9') {
+            return true;
+        }
+        if (ch >= 'a' && ch <= 'z') {
+            return true;
+        }
+        if (ch >= 'A' && ch <= 'Z') {
+            return true;
+        }
+        return false;
+    }
+};
+
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0125_H
