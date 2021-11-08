f081696ddbbb869ea2de50f13f82604d5e63644c
fengwei <wuzhongfengwei@163.com>
2021-11-08 01:15:04 -0800

    Finished Problem 129.

---
+++ Problem0129.h
@@ -0,0 +1,44 @@
+//
+// Created by Fengwei Zhang on 11/8/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0129_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0129_H
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Problem0129 {
+public:
+    int sumNumbers(TreeNode *root) {
+        if (!root) {
+            return 0;
+        }
+        return dfs(root, 0);
+    }
+
+    int dfs(TreeNode *root, const int prev) {
+        if (!root->left && !root->right) {
+            return prev + root->val;
+        }
+        int s = 0;
+        if (root->left) {
+            s += dfs(root->left, 10 * (prev + root->val));
+        }
+        if (root->right) {
+            s += dfs(root->right, 10 * (prev + root->val));
+        }
+        return s;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0129_H
