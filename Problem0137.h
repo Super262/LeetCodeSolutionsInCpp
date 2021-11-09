2d542ad078ecfe2261b38fef4a5e2b811847feb9
fengwei <wuzhongfengwei@163.com>
2021-11-09 15:31:09 -0800

    Finished Problem 137.

---
+++ Problem0137.h
@@ -0,0 +1,30 @@
+//
+// Created by Fengwei Zhang on 11/9/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0137_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0137_H
+
+#include <vector>
+
+using namespace std;
+
+class Problem0137 {
+public:
+    int singleNumber(const vector<int> &nums) {
+        // https://www.acwing.com/solution/content/232/
+        int answer = 0;
+        for (int offset = 0; offset < 32; ++offset) {
+            int ones = 0;
+            for (const auto &num: nums) {
+                if ((num >> offset) & 1) {
+                    ++ones;
+                }
+            }
+            answer += (ones % 3) << offset;
+        }
+        return answer;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0137_H
