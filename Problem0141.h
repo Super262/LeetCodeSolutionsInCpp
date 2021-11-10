8da8e5a77f820b47b4147e795daf4a6e5018ae5b
fengwei <wuzhongfengwei@163.com>
2021-11-10 01:40:14 -0800

    Finished Problem 141.

---
+++ Problem0141.h
@@ -0,0 +1,38 @@
+//
+// Created by Fengwei Zhang on 11/9/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0141_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0141_H
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (!head || !head->next) {
+            return false;
+        }
+        auto slow = head;
+        auto fast = head->next;
+        while (fast) {
+            slow = slow->next;
+            fast = fast->next;
+            if (!fast) {
+                return false;
+            }
+            fast = fast->next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0141_H
