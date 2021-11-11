4e8b20a40920875d7fc8ca9c8f81dce3edf37be1
fengwei <wuzhongfengwei@163.com>
2021-11-11 13:45:39 -0800

    Finished Problem 152.

--- Problem0152.h
+++ Problem0152.h
@@ -13,21 +13,20 @@ class Problem0152 {
 public:
     int maxProduct(const vector<int> &nums) {
         const int n = (int) nums.size();
-        int dp[n][2];  // dp[i][0]：所有以nums[i]结尾的子数组的乘积的最大值；dp[i][1]：所有以nums[i]结尾的子数组的乘积的最小值
+        // 滚动数组优化
+        int dp[2][2];  // dp[i][0]：所有以nums[i]结尾的子数组的乘积的最大值；dp[i][1]：所有以nums[i]结尾的子数组的乘积的最小值
         dp[0][0] = nums[0];
         dp[0][1] = nums[0];
-        for (int i = 1; i < n; ++i) {
-            if (nums[i] >= 0) {
-                dp[i][0] = max(nums[i], dp[i - 1][0] * nums[i]);
-                dp[i][1] = min(nums[i], dp[i - 1][1] * nums[i]);
-            } else {
-                dp[i][0] = max(nums[i], dp[i - 1][1] * nums[i]);
-                dp[i][1] = min(nums[i], dp[i - 1][0] * nums[i]);
-            }
-        }
         int result = dp[0][0];
         for (int i = 1; i < n; ++i) {
-            result = max(result, dp[i][0]);
+            if (nums[i] >= 0) {
+                dp[i % 2][0] = max(nums[i], dp[(i - 1) % 2][0] * nums[i]);
+                dp[i % 2][1] = min(nums[i], dp[(i - 1) % 2][1] * nums[i]);
+            } else {
+                dp[i % 2][0] = max(nums[i], dp[(i - 1) % 2][1] * nums[i]);
+                dp[i % 2][1] = min(nums[i], dp[(i - 1) % 2][0] * nums[i]);
+            }
+            result = max(result, dp[i % 2][0]);
         }
         return result;
     }
