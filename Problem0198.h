12ee56d35b20efad78c6ccde4e4a1f4e2daa209b
fengwei <wuzhongfengwei@163.com>
2021-11-13 02:46:35 -0800

    Finished Problem 198.

--- Problem0198.h
+++ Problem0198.h
@@ -9,22 +9,19 @@
 
 using namespace std;
 
-class Problem0198 {
+class Solution {
 public:
-    int rob(vector<int> &nums) {
-        if (nums.size() == 1) {
-            return nums[0];
+    int rob(const vector<int> &nums) {
+        const int n = (int) nums.size();
+        // 滚动数组优化
+        int dp[2][2];  // dp[i][0]：不选择第i个房子；dp[i][1]：选择第i个房子
+        dp[0][0] = 0;
+        dp[0][1] = nums[0];
+        for (int i = 1; i < n; ++i) {
+            dp[i % 2][0] = max(dp[(i - 1) % 2][1], dp[(i - 1) % 2][0]);
+            dp[i % 2][1] = dp[(i - 1) % 2][0] + nums[i];
         }
-        int dp[2];
-        dp[0] = 0;
-        dp[1] = nums[0];
-        for (int i = 1; i < (int) nums.size(); ++i) {
-            auto a = dp[0];
-            auto b = dp[1];
-            dp[1] = a + nums[i];
-            dp[0] = max(a, b);
-        }
-        return max(dp[0], dp[1]);
+        return max(dp[(n - 1) % 2][0], dp[(n - 1) % 2][1]);
     }
 };
 
