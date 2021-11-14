49d336c5387b911d15e08c6a77dedff6b7958ac9
fengwei <wuzhongfengwei@163.com>
2021-11-14 11:24:12 -0800

    Reviewed Problem 232.

---
+++ PRoblem0232.h
@@ -0,0 +1,66 @@
+//
+// Created by Fengwei Zhang on 11/14/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0232_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0232_H
+
+#include <stack>
+
+using namespace std;
+
+class MyQueue {
+private:
+    stack<int> storage;
+    stack<int> temp;
+
+public:
+    MyQueue() {
+
+    }
+
+    void push(int x) {
+        storage.emplace(x);
+    }
+
+    int pop() {
+        while (!storage.empty()) {
+            temp.emplace(storage.top());
+            storage.pop();
+        }
+        auto t = temp.top();
+        temp.pop();
+        while (!temp.empty()) {
+            storage.emplace(temp.top());
+            temp.pop();
+        }
+        return t;
+    }
+
+    int peek() {
+        while (!storage.empty()) {
+            temp.emplace(storage.top());
+            storage.pop();
+        }
+        auto t = temp.top();
+        while (!temp.empty()) {
+            storage.emplace(temp.top());
+            temp.pop();
+        }
+        return t;
+    }
+
+    bool empty() {
+        return storage.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0232_H
