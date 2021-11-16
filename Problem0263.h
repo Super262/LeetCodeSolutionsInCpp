25ea92f5b760a4c7222d89431a0146cf6278e2f8
fengwei <wuzhongfengwei@163.com>
2021-11-15 23:16:29 -0800

    Finished Problem 263.

---
+++ Problem0263.h
@@ -0,0 +1,29 @@
+//
+// Created by Fengwei Zhang on 11/15/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0263_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0263_H
+
+class Solution {
+public:
+    bool isUgly(int n) {
+        if (n == 0) {
+            return false;
+        }
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else if (n % 3 == 0) {
+                n /= 3;
+            } else if (n % 5 == 0) {
+                n /= 5;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0263_H
