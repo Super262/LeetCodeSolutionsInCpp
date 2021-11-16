83beea0d0a2c31a5c4d42c46730ab59321c58a6c
fengwei <wuzhongfengwei@163.com>
2021-11-16 09:37:27 -0800

    Finished Problem 274.

---
+++ Problem0274.h
@@ -0,0 +1,37 @@
+//
+// Created by Fengwei Zhang on 11/16/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0274_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0274_H
+
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+class Solution {
+public:
+    int hIndex(const vector<int> &citations) {
+        const int n = (int) citations.size();
+        int counter[n + 1];  // 统计每个引用次数对应的论文数
+        memset(counter, 0, sizeof counter);
+        for (int i = 0; i < n; ++i) {
+            if (citations[i] >= n) {  // 超过n，按n算，因为h不超过n
+                ++counter[n];
+            } else {
+                ++counter[citations[i]];
+            }
+        }
+        int total = 0;
+        for (int i = n; i >= 0; --i) {
+            total += counter[i];
+            if (total >= i) {  // 被引用次数为i～n的论文的数量不少于i，则h = i
+                return i;
+            }
+        }
+        return 0;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0274_H
