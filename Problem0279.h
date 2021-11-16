40a56a2483ea680354aa30b9a2e9d77513bc1a43
fengwei <wuzhongfengwei@163.com>
2021-11-16 10:56:20 -0800

    Finished Problem 279.

---
+++ Problem0279.h
@@ -0,0 +1,40 @@
+//
+// Created by Fengwei Zhang on 11/16/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0279_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0279_H
+
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    // 四平方和定理：https://www.acwing.com/solution/content/300/
+    int numSquares(int n) {
+        if (checkNum(n)) {
+            return 1;
+        }
+        for (int x = 1; x <= n / x; ++x) {
+            if (checkNum(n - x * x)) {
+                return 2;
+            }
+        }
+        while (n % 4 == 0) {
+            n /= 4;
+        }
+        if (n % 8 != 7) {
+            return 3;
+        }
+        return 4;
+    }
+
+private:
+    bool checkNum(const int &x) {
+        auto r = (int) sqrt(x);
+        return r * r == x;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0279_H
