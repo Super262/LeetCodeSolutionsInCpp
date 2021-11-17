951e3ab43375c33122da3afc523559366130c1ee
fengwei <wuzhongfengwei@163.com>
2021-11-16 18:47:50 -0800

    Finished Problem 284.

---
+++ Problem0284.h
@@ -0,0 +1,65 @@
+//
+// Created by Fengwei Zhang on 11/16/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0284_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0284_H
+
+#include <vector>
+
+using namespace std;
+
+class Iterator {
+    struct Data;
+    Data *data;
+public:
+    Iterator(const vector<int> &nums);
+
+    Iterator(const Iterator &iter);
+
+    // Returns the next element in the iteration.
+    int next();
+
+    // Returns true if the iteration has more elements.
+    bool hasNext() const;
+};
+
+
+class PeekingIterator : public Iterator {
+private:
+    int current;
+    bool has_next;
+
+public:
+    PeekingIterator(const vector<int> &nums) : Iterator(nums) {
+        // Initialize any member here.
+        // **DO NOT** save a copy of nums and manipulate it directly.
+        // You should only use the Iterator interface methods.
+        has_next = Iterator::hasNext();
+        if (has_next) {
+            current = Iterator::next();
+        }
+    }
+
+    // Returns the next element in the iteration without advancing the iterator.
+    int peek() {
+        return current;
+    }
+
+    // hasNext() and next() should behave the same as in the Iterator interface.
+    // Override them if needed.
+    int next() {
+        auto t = current;
+        has_next = Iterator::hasNext();
+        if (has_next) {
+            current = Iterator::next();
+        }
+        return t;
+    }
+
+    bool hasNext() const {
+        return has_next;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0284_H
