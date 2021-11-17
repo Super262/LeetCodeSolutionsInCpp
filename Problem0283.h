37c52ed8f376681d6d1a47301f58cb14b65b7c5c
fengwei <wuzhongfengwei@163.com>
2021-11-16 16:23:23 -0800

    Finished Problem 283.

---
+++ Problem0283.h
@@ -0,0 +1,28 @@
+//
+// Created by Fengwei Zhang on 11/16/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0283_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0283_H
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int> &nums) {
+        int k = 0;
+        for (int x: nums) {
+            if (x == 0) {
+                continue;
+            }
+            nums[k++] = x;
+        }
+        while (k < nums.size()) {
+            nums[k++] = 0;
+        }
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0283_H
