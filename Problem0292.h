c73ece7d7de3638a24d218dfdd665b0b5b79b4af
fengwei <wuzhongfengwei@163.com>
2021-11-16 23:23:59 -0800

    Finished Problem 292.

---
+++ Problem0292.h
@@ -0,0 +1,15 @@
+//
+// Created by Fengwei Zhang on 11/16/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0292_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0292_H
+
+class Solution {
+public:
+    bool canWinNim(const int &n) {
+        return n % 4 != 0;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0292_H
