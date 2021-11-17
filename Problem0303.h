f92c99c686ffea2256eb45726b535cb55ca269de
fengwei <wuzhongfengwei@163.com>
2021-11-17 09:30:47 -0800

    Finished Problem 303.

---
+++ Problem0303.h
@@ -0,0 +1,34 @@
+//
+// Created by Fengwei Zhang on 11/17/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0303_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0303_H
+
+#include <vector>
+
+using namespace std;
+
+class NumArray {
+private:
+    vector<int> prefix;
+
+public:
+    NumArray(const vector<int> &nums) {
+        prefix = vector<int>(nums.size() + 1, 0);
+        for (int i = 1; i <= nums.size(); ++i) {
+            prefix[i] = prefix[i - 1] + nums[i - 1];
+        }
+    }
+
+    int sumRange(const int &left, const int &right) {
+        return prefix[right + 1] - prefix[left];
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0303_H
