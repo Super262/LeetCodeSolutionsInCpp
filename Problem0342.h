6fe9ab8cd047d85442200178dc8a3c3fc2ad0f97
fengwei <wuzhongfengwei@163.com>
2021-11-24 12:05:36 -0800

    Finished Problem 342.

---
+++ Problem0342.h
@@ -0,0 +1,26 @@
+//
+// Created by Fengwei Zhang on 11/24/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0342_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0342_H
+
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isPowerOfFour(const int &n) {
+        if (n <= 0) {
+            return false;
+        }
+        auto r = (int) sqrt(n);  // 检查完全平方数
+        if (r * r != n) {
+            return false;
+        }
+        return (1 << 30) % r == 0; // 检查指数是否为2的幂
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0342_H
