b113bf7cdb4aeef0385b7d5ab6f51673b140b531
fengwei <wuzhongfengwei@163.com>
2021-11-24 14:17:01 -0800

    Finished Problem 345.

---
+++ Problem0345.h
@@ -0,0 +1,37 @@
+//
+// Created by Fengwei Zhang on 11/24/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0345_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0345_H
+
+#include <string>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    string reverseVowels(string s) {
+        unordered_set<char> towels{'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        int l = 0;
+        int r = (int) s.size() - 1;
+        while (l < r) {
+            if (towels.count(s[l]) && towels.count(s[r])) {
+                swap(s[l], s[r]);
+                ++l;
+                --r;
+                continue;
+            }
+            if (!towels.count(s[l])) {
+                ++l;
+            }
+            if (!towels.count(s[r])) {
+                --r;
+            }
+        }
+        return s;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0345_H
