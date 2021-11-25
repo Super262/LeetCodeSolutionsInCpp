b8a8bdbf9b00b8f2ac03cdd12d1107f9ede4f916
fengwei <wuzhongfengwei@163.com>
2021-11-25 00:48:29 -0800

    Finished Problem 371.

---
+++ Problem0371.h
@@ -0,0 +1,22 @@
+//
+// Created by Fengwei Zhang on 11/25/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0371_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0371_H
+
+class Solution {
+    // 1. "不进位加法"：a ^ b
+    // 2. 填充进位：避免使用"+"，实现 (a ^ b) + ((a & b) << 1)
+public:
+    int getSum(int a, int b) {
+        if (a == 0) {
+            return b;
+        }
+        auto sum = a ^ b;
+        auto carry = (unsigned int) (a & b) << 1;  // 转化为无符号类型，避免报错
+        return getSum((int) carry, sum);
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0371_H
