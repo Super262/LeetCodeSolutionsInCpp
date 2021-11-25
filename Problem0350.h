f905ab0b2538235467967261eae099db8a0ba972
fengwei <wuzhongfengwei@163.com>
2021-11-24 16:17:10 -0800

    Finished Problem 350.

--- Problem0350.h
+++ Problem0350.h
@@ -11,6 +11,11 @@
 using namespace std;
 
 class Solution {
+    // 1.若数组已经有序，我们可以使用双指针算法，保持 *p2 <= *p1
+    // 2.若nums1比nums2小很多，则将nums1全部放入哈希集合中的做法更好
+    // 3.若nums2很大，无法被加载到内存，怎么办？
+    //   如果nums1可以被读入内存，将nums1全部放入内存，逐个遍历nums2；
+    //   如果nums1不能被读入内存，则将num1、nums2外部排序（External Sorting），采用双指针算法。
 public:
     vector<int> intersect(const vector<int> &nums1, const vector<int> &nums2) {
         unordered_multiset<int> allNums(nums1.begin(), nums1.end());
