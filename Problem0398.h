156c3432ea76287552b8e125c3e83e4e1b17faa5
fengwei <wuzhongfengwei@163.com>
2021-11-26 10:15:50 -0800

    Finished Problem 398.

---
+++ Problem0398.h
@@ -0,0 +1,34 @@
+//
+// Created by Fengwei Zhang on 11/26/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0398_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0398_H
+
+#include <vector>
+#include <unordered_map>
+#include <cstdlib>
+
+using namespace std;
+
+class Solution {
+private:
+    unordered_map<int, vector<int>> num2Idx;
+public:
+    Solution(const vector<int> &nums) {
+        for (int i = 0; i < nums.size(); ++i) {
+            num2Idx[nums[i]].emplace_back(i);
+        }
+    }
+
+    int pick(const int &target) {
+        return num2Idx[target][random() % num2Idx[target].size()];
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * int param_1 = obj->pick(target);
+ */
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0398_H
