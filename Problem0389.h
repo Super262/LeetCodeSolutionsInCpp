686dd391ca0c57430e6cee3b2ee8542954cbec70
fengwei <wuzhongfengwei@163.com>
2021-11-25 18:02:22 -0800

    Reviewed Problem 389.

--- Problem0389.h
+++ Problem0389.h
@@ -6,26 +6,20 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0389_H
 
 #include <string>
-#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
     char findTheDifference(const string &s, const string &t) {
-        unordered_map<char, int> counter;
-        for (auto ch: t) {
-            ++counter[ch];
-        }
+        int x = 0;
         for (auto ch: s) {
-            --counter[ch];
+            x ^= ch;
         }
-        for (auto &item: counter) {
-            if (item.second) {
-                return item.first;
-            }
+        for (auto ch: t) {
+            x ^= ch;
         }
-        return '\0';
+        return (char) x;
     }
 };
 
