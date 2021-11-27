a05e961a1ca208f91e514644a094dfe31642500c
fengwei <wuzhongfengwei@163.com>
2021-11-27 09:08:20 -0800

    Finished Problem 416.

---
+++ Problem0416.h
@@ -0,0 +1,37 @@
+//
+// Created by Fengwei Zhang on 11/27/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0416_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0416_H
+
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+class Solution {
+    // 转化为背包问题：是否能装满容量为m / 2的背包
+public:
+    bool canPartition(const vector<int> &nums) {
+        const auto n = (int) nums.size();
+        int m = 0;
+        for (const auto &x: nums) {
+            m += x;
+        }
+        if (m % 2) {
+            return false;
+        }
+        bool dp[m + 1];
+        memset(dp, 0, sizeof dp);
+        dp[0] = true;
+        for (const auto &x: nums) {
+            for (auto j = m; j >= x; --j) {
+                dp[j] |= dp[j - x];
+            }
+        }
+        return dp[m / 2];
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0416_H
