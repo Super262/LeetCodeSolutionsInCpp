df999ddd735e195155a16eb026678a40d40b2aa6
fengwei <wuzhongfengwei@163.com>
2021-11-27 21:16:48 -0800

    Finished Problem 434.

---
+++ Problem0434.h
@@ -0,0 +1,36 @@
+//
+// Created by Fengwei Zhang on 11/27/21.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0434_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0434_H
+
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    int countSegments(const string &s) {
+        int res = 0;
+        int l = 0;
+        int r = 0;
+        while (r < s.size()) {
+            if (s[r] != ' ') {
+                ++r;
+                continue;
+            }
+            if (l < r) {
+                ++res;
+            }
+            l = r + 1;
+            r = l;
+        }
+        if (l < r) {
+            ++res;
+        }
+        return res;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0434_H
