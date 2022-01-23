34668073991bb2a86c2b9e943ea86eae800ef6fd
fengwei <virusformail@gmail.com>
2022-01-22 19:03:13 -0800

    Finished Problem 467.

---
+++ Problem0467.h
@@ -0,0 +1,35 @@
+//
+// Created by Fengwei Zhang on 1/22/22.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0467_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0467_H
+
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    int findSubstringInWraproundString(const string &p) {
+        int res = 0;
+        unordered_map<char, int> counter;
+        for (int i = 0; i < p.size();) {
+            auto j = i + 1;
+            while (j < p.size() && (p[j] == p[j - 1] + 1 || (p[j] == 'a' && p[j - 1] == 'z'))) {
+                ++j;
+            }
+            while (i < j) {
+                counter[p[i]] = max(counter[p[i]], j - i);
+                ++i;
+            }
+        }
+        for (auto &item: counter) {
+            res += item.second;
+        }
+        return res;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0467_H
