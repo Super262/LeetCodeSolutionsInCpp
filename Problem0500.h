faa7ca4142a716d9340a5775800c3211293573b6
fengwei <virusformail@gmail.com>
2022-01-24 15:52:00 -0800

    Finished Problem 500.

---
+++ Problem0500.h
@@ -0,0 +1,42 @@
+//
+// Created by Fengwei Zhang on 1/24/22.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0500_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0500_H
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> findWords(const vector<string> &words) {
+        string lines[3] = {"qwertyuiop", "asdfghjkl", "zxcvbnm"};
+        unordered_map<char, int> ch_to_idx;
+        for (int idx = 0; idx < 3; ++idx) {
+            for (const auto &ch: lines[idx]) {
+                ch_to_idx[ch] = idx;
+            }
+        }
+        vector<string> res;
+        for (const auto &w: words) {
+            auto t = ch_to_idx[(char) tolower(w[0])];
+            bool selected = true;
+            for (const auto &ch: w) {
+                if (ch_to_idx[(char) tolower(ch)] != t) {
+                    selected = false;
+                    break;
+                }
+            }
+            if (selected) {
+                res.emplace_back(w);
+            }
+        }
+        return res;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0500_H
