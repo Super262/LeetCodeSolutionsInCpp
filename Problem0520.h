ac04fcc8e92e8afc42f871dbcb621d889d4bb0ec
fengwei <virusformail@gmail.com>
2022-01-25 02:42:43 -0800

    Finished Problem 520.

---
+++ Problem0520.h
@@ -0,0 +1,34 @@
+//
+// Created by Fengwei Zhang on 1/25/22.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0520_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0520_H
+
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool detectCapitalUse(const string &word) {
+        int upper_cnt = 0;
+        for (const auto &x: word) {
+            if (isupper(x)) {
+                ++upper_cnt;
+            }
+        }
+        if (upper_cnt == 0) {
+            return true;
+        }
+        if (upper_cnt == word.size()) {
+            return true;
+        }
+        if (upper_cnt == 1 && isupper(word[0])) {
+            return true;
+        }
+        return false;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0520_H
