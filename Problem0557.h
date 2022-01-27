ddb0848d18d4ff3304abad5b2b18f151c0e1317b
fengwei <virusformail@gmail.com>
2022-01-26 17:48:33 -0800

    Finished Problem 557.

---
+++ Problem0557.h
@@ -0,0 +1,33 @@
+//
+// Created by Fengwei Zhang on 1/26/22.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0557_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0557_H
+
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        int l = 0, r = 0;
+        while (r < s.size()) {
+            if (s[r] != ' ') {
+                ++r;
+                continue;
+            }
+            reverse(s.begin() + l, s.begin() + r);
+            ++r;
+            l = r;
+        }
+        if (r >= l) {
+            reverse(s.begin() + l, s.begin() + r);
+        }
+        return s;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0557_H
