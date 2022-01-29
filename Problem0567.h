a5e67f16017084270a81d527a89ffcf766c75ec2
fengwei <virusformail@gmail.com>
2022-01-29 01:06:08 -0800

    Finished Problem 567.

---
+++ Problem0567.h
@@ -0,0 +1,57 @@
+//
+// Created by Fengwei Zhang on 1/29/22.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0567_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0567_H
+
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+    // 双指针经典题目，注意细节
+public:
+    bool checkInclusion(const string &s1, const string &s2) {
+        unordered_map<char, int> f1;
+        unordered_map<char, int> f2;
+        for (const auto &ch: s1) {
+            ++f1[ch];
+        }
+        // 注意：在修改f2[ch]前后，都要检查匹配情况！！
+        for (int l = 0, r = 0, counter = 0; r < s2.size(); ++r) {
+            if (checkFreq(s2[r], f1, f2)) {
+                --counter;
+            }
+            ++f2[s2[r]];
+            if (checkFreq(s2[r], f1, f2)) {
+                ++counter;
+            }
+            if (r - l + 1 > s1.size()) {
+                if (checkFreq(s2[l], f1, f2)) {
+                    --counter;
+                }
+                --f2[s2[l]];
+                if (checkFreq(s2[l], f1, f2)) {
+                    ++counter;
+                }
+                ++l;
+            }
+            if (counter == f1.size()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+private:
+    bool checkFreq(char ch, const unordered_map<char, int> &s1, const unordered_map<char, int> &s2) {
+        if (s1.count(ch) && s2.count(ch) && s1.find(ch)->second == s2.find(ch)->second) {
+            return true;
+        }
+        return false;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0567_H
