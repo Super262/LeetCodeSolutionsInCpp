32185dae1407e8e7fb2a3fd85dbfb36c9be8c850
fengwei <virusformail@gmail.com>
2022-01-30 03:12:26 -0800

    Reviewed Problem 13.

--- Problem0013.h
+++ Problem0013.h
@@ -5,26 +5,30 @@
 #ifndef LEETCODESOLUTIONSINCPP_PROBLEM0013_H
 #define LEETCODESOLUTIONSINCPP_PROBLEM0013_H
 
-class Problem0013 {
+#include <string>
+
+using namespace std;
+
+class Solution {
 public:
     int romanToInt(const string &s) {
-        int ch2Num[256];
-        ch2Num['I'] = 1;
-        ch2Num['V'] = 5;
-        ch2Num['X'] = 10;
-        ch2Num['L'] = 50;
-        ch2Num['C'] = 100;
-        ch2Num['D'] = 500;
-        ch2Num['M'] = 1000;
-        ch2Num['N'] = 1001;  // 边界值
+        int ch_to_num[256];
+        ch_to_num['I'] = 1;
+        ch_to_num['V'] = 5;
+        ch_to_num['X'] = 10;
+        ch_to_num['L'] = 50;
+        ch_to_num['C'] = 100;
+        ch_to_num['D'] = 500;
+        ch_to_num['M'] = 1000;
+        ch_to_num['N'] = 1001;  // 边界值
 
         int result = 0;
         char prev = 'N';
         for (auto ch: s) {
-            if (ch2Num[ch] > ch2Num[prev]) {  // 出现逆序，遇到特殊字符组合：IV......CM
-                result -= 2 * ch2Num[prev];
+            if (ch_to_num[ch] > ch_to_num[prev]) {  // 出现逆序，遇到特殊字符组合：IV......CM
+                result -= 2 * ch_to_num[prev];
             }
-            result += ch2Num[ch];
+            result += ch_to_num[ch];
             prev = ch;
         }
 
