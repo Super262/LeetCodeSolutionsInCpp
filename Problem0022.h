3349973479345812265d01942d4f69a41e27571d
fengwei <virusformail@gmail.com>
2022-01-30 11:53:13 -0800

    Reviewed Problem 22.

--- Problem0022.h
+++ Problem0022.h
@@ -10,31 +10,32 @@
 
 using namespace std;
 
-class Problem0022 {
-private:
-    // 合法的括号序列：leftCnt < n, rightCnt < n && leftCnt > rightCnt
-    void dfs(const int n, const int leftCnt, const int rightCnt, string &temp, vector<string> &result) {
-        if (leftCnt == n && rightCnt == n) {
-            result.emplace_back(string(temp));
-            return;
-        }
-        if (leftCnt < n) {
-            temp.push_back('(');
-            dfs(n, leftCnt + 1, rightCnt, temp, result);
-            temp.pop_back();
-        }
-        if (rightCnt < n && rightCnt < leftCnt) {
-            temp.push_back(')');
-            dfs(n, leftCnt, rightCnt + 1, temp, result);
-            temp.pop_back();
-        }
-    }
-
+class Solution {
+    // 合法的括号序列：left_cnt < n, right_cnt < n && left_cnt > right_cnt
+public:
     vector<string> generateParenthesis(const int n) {
         vector<string> result;
         string temp;
         dfs(n, 0, 0, temp, result);
         return result;
+    }
+
+private:
+    void dfs(const int n, const int left_cnt, const int right_cnt, string &temp, vector<string> &result) {
+        if (left_cnt == n && right_cnt == n) {
+            result.emplace_back(temp);
+            return;
+        }
+        if (left_cnt < n) {
+            temp.push_back('(');
+            dfs(n, left_cnt + 1, right_cnt, temp, result);
+            temp.pop_back();
+        }
+        if (right_cnt < n && right_cnt < left_cnt) {
+            temp.push_back(')');
+            dfs(n, left_cnt, right_cnt + 1, temp, result);
+            temp.pop_back();
+        }
     }
 };
 
