97de6e12014371271cfb108664b8ac6000b3389f
fengwei <virusformail@gmail.com>
2022-01-29 22:29:03 -0800

    Reviewed Problem 5.

--- Problem0005.h
+++ Problem0005.h
@@ -11,8 +11,9 @@
 
 using namespace std;
 
-class Problem0005 {
-private:
+class Solution {
+    // 区间DP，dp[i][j]：s[i:j]是否为回文字符串
+public:
     string longestPalindrome(const string &s) {
         bool dp[s.size()][s.size()];  // s[i, j]是回文串，dp[i][j] == true（i，j从0开始）
         memset(dp, 0, sizeof dp);
