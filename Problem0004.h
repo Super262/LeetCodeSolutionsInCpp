6e23bd873a3ffa4e4677df2ed680f77c8776ab2e
fengwei <virusformail@gmail.com>
2022-01-29 21:37:00 -0800

    Reviewed Problem 3.

--- Problem0004.h
+++ Problem0004.h
@@ -9,43 +9,44 @@
 
 using namespace std;
 
-class Problem0004 {
-private:
-    int findHelper(const vector<int> &nums1, const int s1, const vector<int> &nums2, const int s2, const int k) {
-        // k从1开始计数；nums1是长度较小的数组，nums2是长度较长的数组；s1，s2分别是数组的起始位置
-        if (nums1.size() - s1 > nums2.size() - s2) {  // nums1比nums2长
-            return findHelper(nums2, s2, nums1, s1, k);
-        }
-        if (k == 1) {
-            if (s1 == nums1.size()) {
-                return nums2[s2];
-            } else {
-                return min(nums1[s1], nums2[s2]);
-            }
-        }
-        if (s1 == nums1.size()) {
-            // nums1为空（无需测试nums2，前面已保证len(nums2) > len(num1）
-            return nums2[s2 + k - 1];
-        }
-        auto nextS1 = min(s1 + k / 2, (int) nums1.size());
-        auto nextS2 = min(s2 + k - k / 2, (int) nums2.size());
-        if (nums1[nextS1 - 1] > nums2[nextS2 - 1]) {
-            // 舍弃nums2的左半部
-            return findHelper(nums1, s1, nums2, nextS2, k - (nextS2 - s2));
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
+        auto total = (int) nums1.size() + (int) nums2.size();
+        if (total % 2) {
+            return helper(nums1, 0, nums2, 0, total / 2 + 1);
         } else {
-            // 舍弃nums1的左半部
-            return findHelper(nums1, nextS1, nums2, s2, k - (nextS1 - s1));
+            auto left_mid = helper(nums1, 0, nums2, 0, total / 2);
+            auto right_mid = helper(nums1, 0, nums2, 0, total / 2 + 1);
+            return (left_mid + right_mid) / 2.0;
         }
     }
 
-    double findMedianSortedArrays(vector<int> &nums1, vector<int> &nums2) {
-        auto totalLength = (int) nums1.size() + (int) nums2.size();
-        if (totalLength % 2) {
-            return findHelper(nums1, 0, nums2, 0, totalLength / 2 + 1);
+private:
+    int helper(const vector<int> &nums1, const int st1, const vector<int> &nums2, const int st2, const int k) {
+        // k从1开始计数；nums1是长度较小的数组，nums2是长度较长的数组；st1，s2分别是数组的起始位置
+        if (nums1.size() - st1 > nums2.size() - st2) {  // nums1比nums2长
+            return helper(nums2, st2, nums1, st1, k);
+        }
+        if (k == 1) {
+            if (st1 == nums1.size()) {
+                return nums2[st2];
+            } else {
+                return min(nums1[st1], nums2[st2]);
+            }
+        }
+        if (st1 == nums1.size()) {
+            // nums1为空（无需测试nums2，前面已保证len(nums2) > len(num1）
+            return nums2[st2 + k - 1];
+        }
+        auto next_st1 = min(st1 + k / 2, (int) nums1.size());
+        auto next_st2 = min(st2 + k - k / 2, (int) nums2.size());
+        if (nums1[next_st1 - 1] > nums2[next_st2 - 1]) {
+            // 舍弃nums2的左半部
+            return helper(nums1, st1, nums2, next_st2, k - (next_st2 - st2));
         } else {
-            auto leftMid = findHelper(nums1, 0, nums2, 0, totalLength / 2);
-            auto rightMid = findHelper(nums1, 0, nums2, 0, totalLength / 2 + 1);
-            return (leftMid + rightMid) / 2.0;
+            // 舍弃nums1的左半部
+            return helper(nums1, next_st1, nums2, st2, k - (next_st1 - st1));
         }
     }
 };
