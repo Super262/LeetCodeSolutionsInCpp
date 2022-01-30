045385a1a338ae6986695ba4cdc327c145f9f2b1
fengwei <virusformail@gmail.com>
2022-01-30 00:07:28 -0800

    Reviewed Problem 10.

--- Problem0010.h
+++ Problem0010.h
@@ -10,29 +10,29 @@
 
 using namespace std;
 
-class Problem0010 {
-private:
-    bool isMatch(string s, string p) {
+class Solution {
+    // dp[i][j]：s的前i个字符是否能匹配p的前j个字符（i, j >= 1）
+    // https://www.acwing.com/solution/content/557/
+public:
+    bool isMatch(const string &s, const string &p) {
         const int n = (int) s.size();
         const int m = (int) p.size();
-        bool f[n + 1][m + 1];
-        memset(f, 0, sizeof f);
-        s = ' ' + s;
-        p = ' ' + p;
-        f[0][0] = true;
+        bool dp[n + 1][m + 1];
+        memset(dp, 0, sizeof dp);
+        dp[0][0] = true;
         for (int i = 0; i <= n; ++i) {
             for (int j = 1; j <= m; ++j) {
-                if (j + 1 <= m && p[j + 1] == '*') {  // 将'*'和其前的字符当作一个整体对待
+                if (j + 1 <= m && p[j] == '*') {  // 将后面的'*'和当前字符当作一个整体
                     continue;
                 }
-                if (i && p[j] != '*') {
-                    f[i][j] = f[i - 1][j - 1] && (s[i] == p[j] || p[j] == '.');
-                } else if (p[j] == '*') { // 采用类似于完全背包问题的优化方案
-                    f[i][j] = f[i][j - 2] || (i && f[i - 1][j] && (s[i] == p[j - 1] || p[j - 1] == '.'));
+                if (i && p[j - 1] != '*') {  // 单字匹配
+                    dp[i][j] = dp[i - 1][j - 1] && (s[i - 1] == p[j - 1] || p[j - 1] == '.');
+                } else if (p[j - 1] == '*') { // 采用类似于完全背包问题的优化方案
+                    dp[i][j] = dp[i][j - 2] || (i && dp[i - 1][j] && (s[i - 1] == p[j - 2] || p[j - 2] == '.'));
                 }
             }
         }
-        return f[n][m];
+        return dp[n][m];
     }
 };
 
