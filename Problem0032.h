b4f021a3e7634b2f4caf3ed4595f76f53d626e3e
fengwei <virusformail@gmail.com>
2022-01-30 17:44:49 -0800

    Reviewed Problem 32.

--- Problem0032.h
+++ Problem0032.h
@@ -7,30 +7,31 @@
 
 #include <string>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
-class Problem0032 {
+class Solution {
     // 合法的括号序列：左括号和右括号数量相等；任意前缀中的左括号数量大于或等于右括号数量
     // 假设i是第一个不满足以上性质的位置，则合法的括号序列一定在i左侧
-private:
+public:
     int longestValidParentheses(const string &s) {
-        stack<int> leftIdx;
+        stack<int> left_idx;
         int result = 0;
-        for (int i = 0, startIdx = -1; i < (int) s.size(); ++i) {  // startIdx + 1指示合法序列的起点
+        for (int i = 0, st = 0; i < (int) s.size(); ++i) {  // st 指示合法序列的起点
             if (s[i] == '(') {
-                leftIdx.emplace(i);
+                left_idx.emplace(i);
+                continue;
+            }
+            if (left_idx.empty()) {
+                st = i + 1;
+                continue;
+            }
+            left_idx.pop();
+            if (left_idx.empty()) {
+                result = max(result, i - st + 1);
             } else {
-                if (leftIdx.empty()) {
-                    startIdx = i;
-                } else {
-                    leftIdx.pop();
-                    if (leftIdx.empty()) {
-                        result = max(result, i - startIdx);
-                    } else {
-                        result = max(result, i - leftIdx.top());
-                    }
-                }
+                result = max(result, i - left_idx.top());
             }
         }
         return result;
