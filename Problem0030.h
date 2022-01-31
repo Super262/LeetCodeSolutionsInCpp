8d421a40683aab77754171e1cc2b344c8315ee9e
fengwei <virusformail@gmail.com>
2022-01-30 16:20:17 -0800

    Reviewed Problem 30.

--- Problem0030.h
+++ Problem0030.h
@@ -7,42 +7,49 @@
 
 #include <vector>
 #include <string>
+#include <unordered_map>
 
 using namespace std;
 
-class Problem0030 {
+class Solution {
+    // 直接背诵，暴力枚举：https://www.acwing.com/solution/content/113/
+    // 由于数组中每个单词长度都是相同的，故可以按照单词的长度 len 来将 s 划分为 len 种候选单词序列。
+    // 举例来看 barfoothefoobarman，根据单词长度为 3，可以得到 3 种不同的划分，
+    // 分别是 bar foo the foo bar man、arf oot hef oob arm an 和 rfo oth efo oba rma n 三种。
+    // 对于以上的每一种划分来说，仅需要用哈希表就可以暴力的算出来，能否能由单词列表中的所有单词拼接而成。
 public:
     vector<int> findSubstring(const string &s, const vector<string> &words) {
-        vector<int> res;
         if (words.empty()) {
-            return res;
+            return {};
         }
-        unordered_map<string, int> wordFreq;
+        unordered_map<string, int> word_freq;
         for (const auto &item: words) {
-            ++wordFreq[item];
+            ++word_freq[item];
         }
         const int w = (int) words[0].size();
         const int m = (int) words.size();
-        for (int i = 0; i < w; ++i) {  // 枚举单词序列可能的起点位置：i % w
-            unordered_map<string, int> wd;
-            int cnt = 0;
-            for (int j = i; j + w <= (int) s.size(); j += w) {  // 新单词的起点
-                if (j - m * w >= i) {
-                    auto prevW = s.substr(j - m * w, w);
-                    --wd[prevW];
-                    if (wd[prevW] < wordFreq[prevW]) {
-                        --cnt;
+        vector<int> res;
+        unordered_map<string, int> cur_freq;
+        for (int i = 0; i < w; ++i) {  // 枚举单词序列可能的起点位置
+            int counter = 0;
+            for (auto j = i; j + w <= (int) s.size(); j += w) {  // 滑动窗口，枚举新单词的起点
+                if (j - m * w >= i) {  // 删除最早的单词
+                    auto prev_w = s.substr(j - m * w, w);
+                    --cur_freq[prev_w];
+                    if (word_freq.count(prev_w) && cur_freq[prev_w] < word_freq[prev_w]) {
+                        --counter;
                     }
                 }
-                auto nextW = s.substr(j, w);
-                ++wd[nextW];
-                if (wd[nextW] <= wordFreq[nextW]) {  // 词典中的单词出现
-                    ++cnt;
+                auto seg = s.substr(j, w);
+                ++cur_freq[seg];
+                if (word_freq.count(seg) && cur_freq[seg] <= word_freq[seg]) {  // 词典中的单词出现
+                    ++counter;
                 }
-                if (cnt == m) {
+                if (counter == m) {  // 匹配成功
                     res.emplace_back(j - (m - 1) * w);
                 }
             }
+            cur_freq.clear();
         }
         return res;
     }
