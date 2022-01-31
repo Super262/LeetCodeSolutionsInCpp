5ba0b365bef94db2f60c6ac5fad964d237f12575
fengwei <virusformail@gmail.com>
2022-01-30 18:36:29 -0800

    Reviewed Problem 35.

--- Problem0035.h
+++ Problem0035.h
@@ -9,20 +9,25 @@
 
 using namespace std;
 
-class Problem0035 {
+class Solution {
+    // 二分查找大于等于目标值的第一个位置
+    // 直接背诵，注意细节
 public:
-    int searchInsert(const vector<int> &nums, const int &target) {
-        int left = 0;
-        int right = (int) nums.size();
-        while (left < right) {
-            auto mid = left + (right - left) / 2;
+    int searchInsert(const vector<int> &nums, const int target) {
+        int l = 0;
+        int r = (int) nums.size() - 1;
+        while (l < r) {
+            auto mid = l + (r - l) / 2;
             if (nums[mid] >= target) {
-                right = mid;
+                r = mid;
             } else {
-                left = mid + 1;
+                l = mid + 1;
             }
         }
-        return left;
+        if (nums[r] >= target) {
+            return r;
+        }
+        return (int) nums.size();
     }
 };
 
