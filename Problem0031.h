ad47d392e05ddf72553b147b781ba2f76c7dc6c9
fengwei <virusformail@gmail.com>
2022-01-30 17:11:06 -0800

    Reviewed Problem 31.

--- Problem0031.h
+++ Problem0031.h
@@ -16,7 +16,7 @@ public:
             reverse(nums.begin(), nums.end());
             return;
         }
-        int k = (int) nums.size() - 2;
+        auto k = (int) nums.size() - 2;
         while (k >= 0 && nums[k] >= nums[k + 1]) {  // 找到递减序列前的元素
             --k;
         }
@@ -24,13 +24,13 @@ public:
             reverse(nums.begin(), nums.end());
             return;
         }
-        auto nextGreIdx = k + 1;
-        while (nextGreIdx < nums.size() && nums[nextGreIdx] > nums[k]) {
+        auto next_gre_idx = k + 1;
+        while (next_gre_idx < nums.size() && nums[next_gre_idx] > nums[k]) {
             // 在递减序列中搜索比nums[k]大的最小元素
-            ++nextGreIdx;
+            ++next_gre_idx;
         }
-        --nextGreIdx;
-        swap(nums[nextGreIdx], nums[k]);  // 交换
+        --next_gre_idx;
+        swap(nums[next_gre_idx], nums[k]);  // 交换
         reverse(nums.begin() + k + 1, nums.end()); // 反序原降序序列
     }
 };
