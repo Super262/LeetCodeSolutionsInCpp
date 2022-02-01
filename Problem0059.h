98149cfc69278a1382a6e0b3154f046a85a3ef46
fengwei <virusformail@gmail.com>
2022-02-01 06:31:39 -0800

    Reviewed Problem 59.

--- Problem0059.h
+++ Problem0059.h
@@ -11,11 +11,13 @@
 
 using namespace std;
 
-class Problem0059 {
+class Solution {
+    // 经典算法，直接背诵
 public:
     vector<vector<int>> generateMatrix(const int &n) {
+        static const int dx[4] = {0, 1, 0, -1};
+        static const int dy[4] = {1, 0, -1, 0};
         vector<vector<int>> result(n, vector<int>(n, 0));
-        int dx[] = {0, 1, 0, -1}, dy[] = {1, 0, -1, 0};
         bool visited[n][n];
         memset(visited, 0, sizeof visited);
         for (int x = 0, y = 0, d = 0, k = 1; k <= n * n; ++k) {
