2bebc250475ce54d77127a875b255927fc3dc420
fengwei <virusformail@gmail.com>
2022-02-01 00:21:13 -0800

    Reviewed Problem 42.

--- Problem0042.h
+++ Problem0042.h
@@ -10,20 +10,20 @@
 
 using namespace std;
 
-class Problem0042 {
-private:
+class Solution {
+public:
     int trap(const vector<int> &height) {
         stack<int> stk;  // 单调栈，体积递减
         int result = 0;
         for (int i = 0; i < (int) height.size(); ++i) {
-            int prevH = 0;
-            while (!stk.empty() && height[i] >= height[stk.top()]) {  // 高度分布：次高(top)，低(prevH)，最高（i）
-                result += (i - stk.top() - 1) * (height[stk.top()] - prevH);
-                prevH = height[stk.top()];
+            int prev_h = 0;
+            while (!stk.empty() && height[i] >= height[stk.top()]) {  // 高度分布：次高(top)，低(prev_h)，最高（i）
+                result += (i - stk.top() - 1) * (height[stk.top()] - prev_h);
+                prev_h = height[stk.top()];
                 stk.pop();
             }
-            if (!stk.empty()) {  // 高度分布：最高(top)，低(prevH)，次高（i）
-                result += (i - stk.top() - 1) * (height[i] - prevH);
+            if (!stk.empty()) {  // 高度分布：最高(top)，低(prev_h)，次高（i）
+                result += (i - stk.top() - 1) * (height[i] - prev_h);
             }
             stk.emplace(i);
         }
