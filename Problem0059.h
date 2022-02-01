f2f2c922f2710f88fd2e354acca8676bfd6722f0
fengwei <virusformail@gmail.com>
2022-02-01 06:50:21 -0800

    Reviewed Problem 59.

--- Problem0059.h
+++ Problem0059.h
@@ -18,14 +18,11 @@ public:
         static const int dx[4] = {0, 1, 0, -1};
         static const int dy[4] = {1, 0, -1, 0};
         vector<vector<int>> result(n, vector<int>(n, 0));
-        bool visited[n][n];
-        memset(visited, 0, sizeof visited);
         for (int x = 0, y = 0, d = 0, k = 1; k <= n * n; ++k) {
             result[x][y] = k;
-            visited[x][y] = true;
             auto nx = x + dx[d];
             auto ny = y + dy[d];
-            if (nx < 0 || nx >= n || ny < 0 || ny >= n || visited[nx][ny]) {
+            if (nx < 0 || nx >= n || ny < 0 || ny >= n || result[nx][ny]) {
                 d = (d + 1) % 4;
                 nx = x + dx[d];
                 ny = y + dy[d];
