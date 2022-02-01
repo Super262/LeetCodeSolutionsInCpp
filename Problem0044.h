08ab6bd80bfe7ccd3465a3374d9444a0d9c51fe4
fengwei <virusformail@gmail.com>
2022-02-01 01:45:00 -0800

    Reviewed Problem 44.

--- Problem0044.h
+++ Problem0044.h
@@ -10,23 +10,28 @@
 
 using namespace std;
 
-class Problem0044 {
+class Solution {
+    // dp[i][j]描述s的前i个字符和p的前j个字符的匹配情况（i,j >= 1）
     // 优化：借鉴完全背包问题的思想
-private:
-    bool isMatch(string s, string p) {
+public:
+    bool isMatch(const string &s, const string &p) {
         const int n = (int) s.size();
         const int m = (int) p.size();
-        s = ' ' + s;
-        p = ' ' + p;
-        bool dp[n + 1][m + 1];  // dp[i][j]描述s[1~i]和p[1~j]的匹配情况
+        bool dp[n + 1][m + 1];
         memset(dp, 0, sizeof dp);
-        dp[0][0] = true;  // 两个空串，匹配成功
-        for (int i = 0; i <= n; ++i) {  // i从0开始，因为dp[0][j]有意义
-            for (int j = 1; j <= m; ++j) {  // j从1开始，因为dp[i][0]无意义
-                if (p[j] == '*') {
-                    dp[i][j] = dp[i][j - 1] || (i > 0 && dp[i - 1][j]);
+        dp[0][0] = true;  // 初始化：两个空串，匹配成功
+        for (int i = 1; i <= m; ++i) {  // 初始化："*"可以匹配任意字符串
+            if (p[i - 1] != '*') {
+                break;
+            }
+            dp[0][i] = true;
+        }
+        for (int i = 1; i <= n; ++i) {
+            for (int j = 1; j <= m; ++j) {
+                if (p[j - 1] == '*') {
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
                 } else {
-                    dp[i][j] = (s[i] == p[j] || p[j] == '?') && i > 0 && dp[i - 1][j - 1];
+                    dp[i][j] = (s[i - 1] == p[j - 1] || p[j - 1] == '?') && dp[i - 1][j - 1];
                 }
             }
         }
