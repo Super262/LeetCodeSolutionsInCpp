a799ee537b3458be8ea6bac694911004341d1d3c
fengwei <virusformail@gmail.com>
2022-02-01 07:00:10 -0800

    Reviewed Problem 60.

--- Problem0060.h
+++ Problem0060.h
@@ -11,8 +11,8 @@
 
 using namespace std;
 
-class Problem0060 {
-private:
+class Solution {
+public:
     string getPermutation(int n, int k) {
         // 预计算n!（n个数字可组成的n位数有n!个）
         int factorial[n + 1];
@@ -29,7 +29,7 @@ private:
                 if (used[digit]) {  // 检测当前数字是否已经被使用过
                     continue;
                 }
-                if (k > factorial[n - i - 1]) {  // 检测k是否大于以当前数字开头的n位数的个数
+                if (k > factorial[n - i - 1]) {  // 检测k是否大于以当前数字开头的(n-i)位数的个数
                     k -= factorial[n - i - 1];
                 } else {
                     result[i] = (char) ('0' + digit);
