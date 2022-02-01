c998e6dfb1b33c31b59e9de92d764ab981cec929
fengwei <virusformail@gmail.com>
2022-02-01 03:03:27 -0800

    Reviewed Problem 49.

--- Problem0049.h
+++ Problem0049.h
@@ -6,21 +6,24 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0049_H
 
 #include <string>
+#include <algorithm>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
-class Problem0049 {
+class Solution {
     // 技巧：对字符串按字典序排序
-private:
+public:
     vector<vector<string>> groupAnagrams(vector<string> &strs) {
-        unordered_map <string, vector<string>> dict;
+        unordered_map<string, vector<string>> dict;
         vector<vector<string>> result;
         for (const auto &s: strs) {
             string key = s;
             sort(key.begin(), key.end());
             dict[key].emplace_back(s);
         }
+        result.reserve(dict.size());  // 提高效率
         for (auto &item: dict) {
             result.emplace_back(item.second);
         }
