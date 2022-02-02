ddcb25fe2d444e82d47cf90b9796a844269bc96c
fengwei <virusformail@gmail.com>
2022-02-01 23:24:59 -0800

    Reviewed Problem 76.

--- Problem0076.h
+++ Problem0076.h
@@ -11,8 +11,8 @@
 
 using namespace std;
 
-class Problem0076 {
-private:
+class Solution {
+public:
     string minWindow(const string &s, const string &t) {
         int hs[256], ht[256];
         memset(hs, 0, sizeof hs);  // 窗口的字符频率
@@ -20,24 +20,26 @@ private:
         for (auto ch: t) {  // 统计t的字符频率
             ++ht[ch];
         }
-        int validLength = 0;  // 当前窗口中的“有效长度”（包含所有类型的字符且相应字符频率不低于t的窗口长度）
-        string result;
+        int valid_length = 0;  // 当前窗口中的“有效长度”（包含所有类型的字符且相应字符频率不低于t的窗口长度）
+        int res_st = 0;  // 结果的起点
+        int res_len = 0;  // 结果的长度
         for (int l = 0, r = 0; r < (int) s.size(); ++r) {
             ++hs[s[r]];
             if (hs[s[r]] <= ht[s[r]]) {  // 没有冗余字符
-                ++validLength;
+                ++valid_length;
             }
             while (l <= r && hs[s[l]] > ht[s[l]]) {  // 删去冗余字符
                 --hs[s[l]];
                 ++l;
             }
-            if (validLength == (int) t.size()) {
-                if (result.empty() || r - l + 1 < result.size()) {
-                    result = s.substr(l, r - l + 1);
+            if (valid_length == (int) t.size()) {
+                if (!res_len || r - l + 1 < res_len) {
+                    res_len = r - l + 1;
+                    res_st = l;
                 }
             }
         }
-        return result;
+        return s.substr(res_st, res_len);
     }
 };
 
