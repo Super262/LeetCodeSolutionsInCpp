978dd5c2d13f35870f97cca7b168c5ea74ffbb10
fengwei <virusformail@gmail.com>
2022-02-01 19:15:32 -0800

    Reviewed Problem 70.

--- Problem0070.h
+++ Problem0070.h
@@ -5,15 +5,15 @@
 #ifndef LEETCODESOLUTIONSINCPP_PROBLEM0070_H
 #define LEETCODESOLUTIONSINCPP_PROBLEM0070_H
 
-class Problem0070 {
+class Solution {
 public:
-    int climbStairs(int n) {
-        int dp[n + 1];
+    int climbStairs(int n) {  // 滑动窗口优化
+        int dp[3];
         dp[1] = dp[0] = 1;
         for (int i = 2; i <= n; ++i) {
-            dp[i] = dp[i - 1] + dp[i - 2];
+            dp[i % 3] = dp[(i - 1) % 3] + dp[(i - 2) % 3];
         }
-        return dp[n];
+        return dp[n % 3];
     }
 };
 
