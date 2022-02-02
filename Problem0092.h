cf6733bb13c3d96998f75f621ee3f17f6d965cf2
fengwei <virusformail@gmail.com>
2022-02-02 13:02:23 -0800

    Reviewed Problem 92.

--- Problem0092.h
+++ Problem0092.h
@@ -23,21 +23,21 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-class Problem0092 {
-private:
-    ListNode *reverseBetween(ListNode *head, const int &left, const int &right) {
+class Solution {
+public:
+    ListNode *reverseBetween(ListNode *head, const int left, const int right) {
         if (!head || right - left + 1 < 2) {
             return head;
         }
         auto dummy = new ListNode(-1);
         dummy->next = head;
         auto a = dummy;
-        for (int i = 0; i < left - 1; ++i) {
+        for (int i = 0; i < left - 1; ++i) {  // 将a移动到left左边的点
             a = a->next;
         }
         auto b = a->next;
         auto c = b->next;
-        for (int i = 0; i < right - left; ++i) {
+        for (int i = 0; i < right - left; ++i) {  // 应反转(right-left)个指针
             auto t = c->next;
             c->next = b;
             b = c;
