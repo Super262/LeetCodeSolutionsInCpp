6d9235218f0fc9f689dbd87cdaf4d5eceeef5cc9
fengwei <virusformail@gmail.com>
2022-02-02 18:14:08 -0800

    Reviewed Problem 101.

--- Problem0101.h
+++ Problem0101.h
@@ -5,6 +5,10 @@
 #ifndef LEETCODESOLUTIONSINCPP_PROBLEM0101_H
 #define LEETCODESOLUTIONSINCPP_PROBLEM0101_H
 
+#include <queue>
+
+using namespace std;
+
 struct TreeNode {
     int val;
     TreeNode *left;
@@ -17,7 +21,41 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class Problem0101 {
+class Solution {
+    // 迭代法，直接背诵
+public:
+    bool isSymmetric(TreeNode *root) {
+        if (!root) {
+            return true;
+        }
+        queue<TreeNode *> q;
+        q.emplace(root);
+        q.emplace(root);
+        while (q.size() >= 2) {
+            auto *u = q.front();
+            q.pop();
+            auto *v = q.front();
+            q.pop();
+            if (!u && !v) {
+                continue;
+            }
+            if (!u || !v) {
+                return false;
+            }
+            if (u->val != v->val) {
+                return false;
+            }
+            q.emplace(u->left);
+            q.emplace(v->right);
+            q.emplace(u->right);
+            q.emplace(v->left);
+        }
+        return true;
+    }
+};
+
+/*class Solution {
+    // 递归法
 public:
     bool isSymmetric(TreeNode *root) {
         if (!root) {
@@ -35,6 +73,6 @@ public:
         }
         return dfs(left->left, right->right) && dfs(left->right, right->left);
     }
-};
+};*/
 
 #endif //LEETCODESOLUTIONSINCPP_PROBLEM0101_H
