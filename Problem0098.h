9a8e1f4a53395912939aa7161cde0c20a236b59a
fengwei <virusformail@gmail.com>
2022-02-02 16:19:15 -0800

    Reviewed Problem 98.

--- Problem0098.h
+++ Problem0098.h
@@ -12,22 +12,29 @@
 
 using namespace std;
 
-class Problem0098 {
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool isValidBST(const TreeNode *root) {
+        if (!root) {
+            return true;
+        }
+        return dfs(root)[0] == 1;
+    }
+
 private:
-    struct TreeNode {
-        int val;
-        TreeNode *left;
-        TreeNode *right;
-
-        TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-    };
-
     vector<int> dfs(const TreeNode *root) {
-
         // result[0]：是否为BST
         // result[1]：当前树最大值
         // result[2]：当前树最小值
@@ -51,13 +58,6 @@ private:
         }
 
         return result;
-    }
-
-    bool isValidBST(const TreeNode *root) {
-        if (!root) {
-            return true;
-        }
-        return dfs(root)[0] == 1;
     }
 };
 
