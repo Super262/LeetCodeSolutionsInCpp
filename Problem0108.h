37c01c7f7ce53d37587d6f9b1cc52df8fc83a500
fengwei <virusformail@gmail.com>
2022-02-03 15:13:23 -0800

    Reviewed Problem 108.

--- Problem0108.h
+++ Problem0108.h
@@ -9,34 +9,34 @@
 
 using namespace std;
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
 class Solution {
-private:
-    struct TreeNode {
-        int val;
-        TreeNode *left;
-        TreeNode *right;
-
-        TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-    };
-
 public:
+    TreeNode *sortedArrayToBST(const vector<int> &nums) {
+        return buildTree(nums, 0, (int) nums.size() - 1);
+    }
+
+private:
     TreeNode *buildTree(const vector<int> &nums, const int l, const int r) {
         if (l > r) {
-            return NULL;
+            return nullptr;
         }
         auto mid = l + (r - l) / 2;
         auto root = new TreeNode(nums[mid]);
         root->left = buildTree(nums, l, mid - 1);
         root->right = buildTree(nums, mid + 1, r);
         return root;
-    }
-
-    TreeNode *sortedArrayToBST(vector<int> &nums) {
-        return buildTree(nums, 0, (int) nums.size() - 1);
     }
 };
 
