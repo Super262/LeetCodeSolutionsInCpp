dd25cf7abf66990ac9540aaacd10a599619ed264
fengwei <virusformail@gmail.com>
2022-02-03 18:49:36 -0800

    Reviewed Problem 118.

--- Problem0118.h
+++ Problem0118.h
@@ -9,16 +9,17 @@
 
 using namespace std;
 
-class Problem0118 {
+class Solution {
 public:
-    vector<vector<int>> generate(const int &numRows) {
+    vector<vector<int>> generate(const int num_rows) {
         vector<vector<int>> dp;
-        for (int i = 0; i < numRows; ++i) {
+        dp.reserve(num_rows);
+        for (int i = 0; i < num_rows; ++i) {
             dp.emplace_back(vector<int>(i + 1, 0));
             dp[i][0] = 1;
             dp[i][dp[i].size() - 1] = 1;
         }
-        for (int i = 2; i < numRows; ++i) {
+        for (int i = 2; i < num_rows; ++i) {
             for (int j = 1; j < (int) dp[i].size() - 1; ++j) {
                 dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
             }
