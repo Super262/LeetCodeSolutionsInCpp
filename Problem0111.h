e5d66145ebb1f7dc8bae99fe7fc2f77b88942ca3
fengwei <virusformail@gmail.com>
2022-02-03 16:13:47 -0800

    Reviewed Problem 111.

--- Problem0111.h
+++ Problem0111.h
@@ -5,6 +5,11 @@
 #ifndef LEETCODESOLUTIONSINCPP_PROBLEM0111_H
 #define LEETCODESOLUTIONSINCPP_PROBLEM0111_H
 
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
 struct TreeNode {
     int val;
     TreeNode *left;
@@ -17,7 +22,37 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class Problem0111 {
+class Solution {
+    // 非递归解法
+public:
+    int minDepth(TreeNode *root) {
+        if (!root) {
+            return 0;
+        }
+        queue<pair<TreeNode *, int>> q;
+        q.emplace(root, 1);
+        while (!q.empty()) {
+            auto t = q.front();
+            q.pop();
+            auto d = t.second;
+            auto node = t.first;
+            if (!node->left && !node->right) {
+                return d;
+            }
+            if (node->left) {
+                q.emplace(node->left, d + 1);
+            }
+            if (node->right) {
+                q.emplace(node->right, d + 1);
+            }
+        }
+        return INT_MAX;
+    }
+};
+
+/*class Solution {
+    // 经典算法，直接背诵
+    // 递归解法
 public:
     int minDepth(TreeNode *root) {
         if (!root) {
@@ -34,6 +69,6 @@ public:
         }
         return minDepth(root->right) + 1;
     }
-};
+};*/
 
 #endif //LEETCODESOLUTIONSINCPP_PROBLEM0111_H
