ed2a240750375ee4dec715b35e212aac5384d793
fengwei <virusformail@gmail.com>
2022-02-04 11:03:19 -0800

    Reviewed Problem 140.

--- Problem0140.h
+++ Problem0140.h
@@ -12,15 +12,13 @@
 
 using namespace std;
 
-class Problem0140 {
+class Solution {
 public:
-    vector<string> wordBreak(const string &s, const vector<string> &wordDict) {
-        unordered_set<string> dict; // 优化查找速度：Trie替代哈希表（O(n) -> O (1)）
+    vector<string> wordBreak(const string &s, const vector<string> &words) {
+        // 优化查找速度：Trie替代哈希表（O(n) -> O (1)）
+        unordered_set<string> dict(words.begin(), words.end());
         const int n = (int) s.size();
         bool dp[n + 1];  // dp[i]：前i个字符是否可被分割（1 <= i<= n）
-        for (const auto &w: wordDict) {
-            dict.insert(w);
-        }
         memset(dp, 0, sizeof dp);
         dp[0] = true;
         for (int r = 1; r <= n; ++r) {
@@ -37,15 +35,16 @@ public:
         return result;
     }
 
-    void dfs(const int &idx,
+private:
+    void dfs(const int idx,
              const string &s,
              const unordered_set<string> &dict,
              bool dp[],
-             vector<string> &tempSegs,
+             vector<string> &temp_segs,
              vector<string> &result) {  // 搜索s[0:idx]的所有分割方案
         if (idx < 0) {
             string path;
-            for (auto seg: tempSegs) {
+            for (auto seg: temp_segs) {
                 seg += " " + path;
                 path = seg;
             }
@@ -58,9 +57,9 @@ public:
             if (!dict.count(current) || !dp[ed]) {
                 continue;
             }
-            tempSegs.emplace_back(current);
-            dfs(ed - 1, s, dict, dp, tempSegs, result);
-            tempSegs.pop_back();
+            temp_segs.emplace_back(current);
+            dfs(ed - 1, s, dict, dp, temp_segs, result);
+            temp_segs.pop_back();
         }
     }
 };
