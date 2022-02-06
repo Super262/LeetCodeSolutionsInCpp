14c8766bacda67f78c1cfee912e6affebfa0ab25
fengwei <virusformail@gmail.com>
2022-02-05 23:48:07 -0800

    Reviewed Problem 152.

--- Problem0152.h
+++ Problem0152.h
@@ -9,15 +9,18 @@
 
 using namespace std;
 
-class Problem0152 {
+class Solution {
+    // 经典DP，直接背诵
+    // dp[i][0]：所有以nums[i]结尾的子数组的乘积的最大值
+    // dp[i][1]：所有以nums[i]结尾的子数组的乘积的最小值
 public:
     int maxProduct(const vector<int> &nums) {
         const int n = (int) nums.size();
         // 滚动数组优化
-        int dp[2][2];  // dp[i][0]：所有以nums[i]结尾的子数组的乘积的最大值；dp[i][1]：所有以nums[i]结尾的子数组的乘积的最小值
+        int dp[2][2];
         dp[0][0] = nums[0];
         dp[0][1] = nums[0];
-        int result = dp[0][0];
+        auto res = dp[0][0];
         for (int i = 1; i < n; ++i) {
             if (nums[i] >= 0) {
                 dp[i % 2][0] = max(nums[i], dp[(i - 1) % 2][0] * nums[i]);
@@ -26,9 +29,9 @@ public:
                 dp[i % 2][0] = max(nums[i], dp[(i - 1) % 2][1] * nums[i]);
                 dp[i % 2][1] = min(nums[i], dp[(i - 1) % 2][0] * nums[i]);
             }
-            result = max(result, dp[i % 2][0]);
+            res = max(res, dp[i % 2][0]);
         }
-        return result;
+        return res;
     }
 };
 
