f461b00d53aea1e0542143971411bbeab3ecf92c
fengwei <virusformail@gmail.com>
2022-02-05 18:17:50 -0800

    Reviewed Problem 148.

--- Problem0148.h
+++ Problem0148.h
@@ -16,10 +16,11 @@ struct ListNode {
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-class Problem0148 {
+class Solution {
+    // 经典算法，直接背诵
+    // https://www.acwing.com/solution/content/408/
 public:
     ListNode *sortList(ListNode *head) {  // 归并排序思想
-        // https://www.acwing.com/solution/content/408/
         int n = 0;
         for (auto p = head; p; p = p->next) {
             ++n;
@@ -27,10 +28,10 @@ public:
         auto dummy = new ListNode(-1);
         dummy->next = head;
         for (int seg = 1; seg < n; seg *= 2) {  // 枚举分治段的长度
-            auto current = dummy;
+            head = dummy;
             for (int i = 1; i + seg <= n; i += 2 * seg) {
-                auto p = current->next;  // 左分治段指针
-                auto q = current->next;  // 右分治段指针
+                auto p = head->next;  // 左分治段指针
+                auto q = head->next;  // 右分治段指针
                 for (int k = 1; k <= seg; ++k) {
                     q = q->next;
                 }
@@ -38,29 +39,29 @@ public:
                 int qc = 0;  // 右计数器
                 while (pc < seg && qc < seg && p && q) {
                     if (p->val < q->val) {
-                        current->next = p;
+                        head->next = p;
                         p = p->next;
                         ++pc;
                     } else {
-                        current->next = q;
+                        head->next = q;
                         q = q->next;
                         ++qc;
                     }
-                    current = current->next;
+                    head = head->next;
                 }
                 while (pc < seg && p) {
-                    current->next = p;
+                    head->next = p;
                     p = p->next;
                     ++pc;
-                    current = current->next;
+                    head = head->next;
                 }
                 while (qc < seg && q) {
-                    current->next = q;
+                    head->next = q;
                     q = q->next;
                     ++qc;
-                    current = current->next;
+                    head = head->next;
                 }
-                current->next = q;  // 必须的步骤：接合链表
+                head->next = q;  // 必须的步骤：接合链表
             }
         }
         return dummy->next;
