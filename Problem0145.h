47b488bdff47cd46ac0b4fc65fc781bd4857652d
fengwei <virusformail@gmail.com>
2022-02-05 17:02:49 -0800

    Reviewed Problem 145.

--- Problem0145.h
+++ Problem0145.h
@@ -5,6 +5,7 @@
 #ifndef LEETCODESOLUTIONSINCPP_PROBLEM0145_H
 #define LEETCODESOLUTIONSINCPP_PROBLEM0145_H
 
+#include <vector>
 #include <stack>
 
 using namespace std;
@@ -21,10 +22,55 @@ struct TreeNode {
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class Problem0145 {
+class Solution {
+    // Morris遍历，空间O(1)
+    // 经典算法，直接背诵
 public:
     vector<int> postorderTraversal(TreeNode *root) {
-        // 巧妙做法：按照前序遍历，先右后左；再翻转结果
+        if (!root) {
+            return {};
+        }
+        vector<int> res;
+        TreeNode *p1 = root;
+        while (p1) {
+            auto p2 = p1->left;
+            if (p2) {
+                while (p2->right && p2->right != p1) {
+                    p2 = p2->right;
+                }
+                if (!p2->right) {
+                    p2->right = p1;
+                    p1 = p1->left;
+                    continue;
+                } else {
+                    p2->right = nullptr;
+                    addPath(res, p1->left);
+                }
+            }
+            p1 = p1->right;
+        }
+        addPath(res, root);
+        return res;
+    }
+
+private:
+    void addPath(vector<int> &vec, TreeNode *node) {
+        int count = 0;
+        while (node) {
+            ++count;
+            vec.emplace_back(node->val);
+            node = node->right;
+        }
+        reverse(vec.end() - count, vec.end());
+    }
+};
+
+/*class Solution {
+    // 经典算法，直接背诵
+    // 巧妙做法：按照前序遍历，先右后左，再翻转结果
+    // 空间O(n)
+public:
+    vector<int> postorderTraversal(TreeNode *root) {
         vector<int> res;
         stack<TreeNode *> stk;
         while (root || !stk.empty()) {
@@ -39,6 +85,6 @@ public:
         reverse(res.begin(), res.end());
         return res;
     }
-};
+};*/
 
 #endif //LEETCODESOLUTIONSINCPP_PROBLEM0145_H
