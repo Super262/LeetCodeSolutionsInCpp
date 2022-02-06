f30f16885bc575970b1829676cb642765da991f6
fengwei <virusformail@gmail.com>
2022-02-06 01:59:20 -0800

    Reviewed Problem 164.

--- Problem0164.h
+++ Problem0164.h
@@ -38,7 +38,7 @@ public:
             ranges[idx].max = max(x, ranges[idx].max);
             ranges[idx].min = min(x, ranges[idx].min);
         }
-        long long result = 0;  // 涉及到INT边界值，防止溢出
+        long long result = INT_MIN;  // 涉及到INT边界值，用long long防止溢出
         // 若结果为(b-a)，则a是当前桶的最大值、b是下一个桶的最小值，因为a、b在数组中是相邻元素
         for (int i = 0, pre_max = minimal; i < n - 1; ++i) {
             if (!ranges[i].used) {  // 跳过空桶
