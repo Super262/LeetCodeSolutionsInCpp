d55b3c7462b37fb32ac4fbe2722b5b0ba03ef131
fengwei <virusformail@gmail.com>
2022-02-06 01:16:51 -0800

    Reviewed Problem 164.

--- Problem0164.h
+++ Problem0164.h
@@ -9,7 +9,47 @@
 
 using namespace std;
 
-class Problem0164 {
+class Solution {
+    // https://www.acwing.com/solution/content/19237/
+    // 经典算法，直接背诵
+public:
+    int maximumGap(const vector<int> &nums) {
+        const int n = (int) nums.size();
+        if (n < 2) {
+            return 0;
+        }
+        int maximal = INT_MIN;
+        int minimal = INT_MAX;
+        for (const auto &x: nums) {
+            maximal = max(maximal, x);
+            minimal = min(minimal, x);
+        }
+        if (maximal == minimal) {  // 特殊情况：所用元素相等
+            return 0;
+        }
+        Range ranges[n - 1];  // n个元素间存在(n-1)个差值
+        const auto seg = (maximal - minimal + n - 2) / (n - 1);
+        for (const auto &x: nums) {
+            if (x == minimal) {
+                continue;
+            }
+            auto idx = (x - minimal - 1) / seg;
+            ranges[idx].used = true;
+            ranges[idx].max = max(x, ranges[idx].max);
+            ranges[idx].min = min(x, ranges[idx].min);
+        }
+        int result = 0;
+        // 若结果为(b-a)，则a是当前桶的最大值、b是下一个桶的最小值，因为a、b在数组中是相邻元素
+        for (int i = 0, pre_max = minimal; i < n - 1; ++i) {
+            if (!ranges[i].used) {
+                continue;
+            }
+            result = max(result, ranges[i].min - pre_max);
+            pre_max = ranges[i].max;
+        }
+        return result;
+    }
+
 private:
     struct Range {
         int max;
@@ -22,45 +62,6 @@ private:
             used = false;
         }
     };
-
-public:
-    // https://www.acwing.com/solution/content/19237/
-    // 直接背诵
-    int maximumGap(const vector<int> &nums) {
-        const int n = (int) nums.size();
-        if (n < 2) {
-            return 0;
-        }
-        int maximal = INT_MAX;
-        int minimal = INT_MIN;
-        for (const auto &x: nums) {
-            maximal = max(maximal, x);
-            minimal = min(minimal, x);
-        }
-        if (minimal == maximal) {
-            return 0;
-        }
-        Range ranges[n - 1];
-        const auto rangeLength = (maximal - minimal + n - 2) / (n - 1);
-        for (const auto &x: nums) {
-            if (x == minimal) {
-                continue;
-            }
-            auto idx = (x - minimal - 1) / rangeLength;
-            ranges[idx].used = true;
-            ranges[idx].min = min(ranges[idx].min, x);
-            ranges[idx].max = min(ranges[idx].max, x);
-        }
-        int result = 0;
-        for (int i = 0, preMax = minimal; i < n - 1; ++i) {
-            if (!ranges[i].used) {
-                continue;
-            }
-            result = max(result, ranges[i].min - preMax);
-            last = ranges[i].max;
-        }
-        return result;
-    }
 };
 
 #endif //LEETCODESOLUTIONSINCPP_PROBLEM0164_H
