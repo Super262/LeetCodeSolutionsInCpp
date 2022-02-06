a67421fb305e2000453e311bb0c673aafee9d7ef
fengwei <virusformail@gmail.com>
2022-02-05 17:28:52 -0800

    Reviewed Problem 146.

--- Problem0146.h
+++ Problem0146.h
@@ -12,6 +12,47 @@ using namespace std;
 class LRUCache {
     // https://www.acwing.com/solution/content/1013/
     // 双链表的价值：O(1)删除
+public:
+    LRUCache(const int c) {
+        size = 0;
+        capacity = c;
+        head = new Node(-1, -1);
+        tail = new Node(-1, -1);
+        head->next = tail;
+        tail->prev = head;
+    }
+
+    int get(const int key) {
+        if (!index.count(key)) {
+            return -1;
+        }
+        auto node = index[key];
+        detach(node);
+        addLast(node);
+        return node->value;
+    }
+
+    void put(const int key, const int value) {
+        if (index.count(key)) {
+            auto node = index[key];
+            detach(node);
+            node->value = value;
+            addLast(node);
+        } else {
+            if (size == capacity) {
+                auto node = head->next;
+                index.erase(node->key);
+                detach(node);
+                delete node;
+                --size;
+            }
+            ++size;
+            auto node = new Node(key, value);
+            addLast(node);
+            index[key] = node;
+        }
+    }
+
 private:
     struct Node {
         int key;
@@ -19,7 +60,7 @@ private:
         Node *next;
         Node *prev;
 
-        Node(const int &k, const int &v) {
+        Node(const int k, const int v) {
             key = k;
             value = v;
             prev = nullptr;
@@ -33,57 +74,16 @@ private:
     Node *tail;
     unordered_map<int, Node *> index;
 
-    void remove(Node *node) {
+    void detach(Node *node) {
         node->next->prev = node->prev;
         node->prev->next = node->next;
     }
 
-    void emplace_back(Node *node) {
+    void addLast(Node *node) {
         node->next = tail;
         node->prev = tail->prev;
         tail->prev->next = node;
         tail->prev = node;
-    }
-
-public:
-    LRUCache(const int &c) {
-        size = 0;
-        capacity = c;
-        head = new Node(-1, -1);
-        tail = new Node(-1, -1);
-        head->next = tail;
-        tail->prev = head;
-    }
-
-    int get(const int &key) {
-        if (!index.count(key)) {
-            return -1;
-        }
-        auto node = index[key];
-        remove(node);
-        emplace_back(node);
-        return node->value;
-    }
-
-    void put(const int &key, const int &value) {
-        if (index.count(key)) {
-            auto node = index[key];
-            remove(node);
-            node->value = value;
-            emplace_back(node);
-        } else {
-            if (size == capacity) {
-                auto node = head->next;
-                index.erase(node->key);
-                remove(node);
-                delete node;
-                --size;
-            }
-            ++size;
-            auto node = new Node(key, value);
-            emplace_back(node);
-            index[key] = node;
-        }
     }
 };
 
