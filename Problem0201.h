31637b9762aef07602dcf50f91193358d6b8cfb7
fengwei <virusformail@gmail.com>
2022-02-06 17:08:40 -0800

    Reviewed Problem 201.

--- Problem0201.h
+++ Problem0201.h
@@ -6,16 +6,16 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0201_H
 
 class Solution {
+    // https://www.acwing.com/solution/content/19514/
 public:
-    int rangeBitwiseAnd(const int &m, const int &n) {
-        // https://www.acwing.com/solution/content/19514/
+    int rangeBitwiseAnd(const int m, const int n) {
         int result = 0;
         for (int i = 30; i >= 0; --i) {
-            if ((m >> i & 1) != (n >> i & 1)) {
+            if (((m >> i) & 1) != ((n >> i) & 1)) {  // 高位不同，说明已达到某个数字的尽头
                 break;
             }
-            if ((n >> i & 1) == 1) {
-                result += (1 << i);
+            if (((n >> i) & 1) == 1) {
+                result += 1 << i;
             }
         }
         return result;
