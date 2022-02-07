87b208facee37805e775087ef76e62be258135fa
fengwei <virusformail@gmail.com>
2022-02-06 17:04:51 -0800

    Reviewed Problem 200.

--- Problem0200.h
+++ Problem0200.h
@@ -13,11 +13,9 @@ using namespace std;
 class Solution {
 public:
     int numIslands(vector<vector<char>> &grid) {
-        const int m = (int) grid.size();
-        const int n = (int) grid[0].size();
         int result = 0;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
+        for (int i = 0; i < (int) grid.size(); ++i) {
+            for (int j = 0; j < (int) grid[0].size(); ++j) {
                 if (grid[i][j] == '0') {
                     continue;
                 }
@@ -29,24 +27,22 @@ public:
     }
 
 private:
-    void bfs(const int &x, const int &y, vector<vector<char>> &grid) {
-        const int m = (int) grid.size();
-        const int n = (int) grid[0].size();
+    void bfs(const int rx, const int ry, vector<vector<char>> &grid) {
         const int dx[] = {0, 0, 1, -1};
         const int dy[] = {1, -1, 0, 0};
         queue<pair<int, int>> q;
-        q.emplace(pair<int, int>(x, y));
-        grid[x][y] = '0';
+        q.emplace(rx, ry);
+        grid[rx][ry] = '0';
         while (!q.empty()) {
             auto root = q.front();
             q.pop();
             for (int i = 0; i < 4; ++i) {
                 auto nx = root.first + dx[i];
                 auto ny = root.second + dy[i];
-                if (nx < 0 || nx >= m || ny < 0 || ny >= n || grid[nx][ny] == '0') {
+                if (nx < 0 || nx >= (int) grid.size() || ny < 0 || ny >= (int) grid[0].size() || grid[nx][ny] == '0') {
                     continue;
                 }
-                q.emplace(pair<int, int>(nx, ny));
+                q.emplace(nx, ny);
                 grid[nx][ny] = '0';
             }
         }
