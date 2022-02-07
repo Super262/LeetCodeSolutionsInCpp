43e7e2f4e7fdb7381c02d98a9fb8f9d25a9d3705
fengwei <virusformail@gmail.com>
2022-02-06 18:31:48 -0800

    Reviewed Problem 209.

--- Problem0209.h
+++ Problem0209.h
@@ -10,22 +10,26 @@
 using namespace std;
 
 class Solution {
+    // 经典算法，直接背诵：https://www.acwing.com/solution/content/268/
     // 牢记双指针算法的正确性（单调性）证明！
 public:
-    int minSubArrayLen(const int &target, const vector<int> &nums) {
-        const int n = (int) nums.size();
-        int result = n + 1;
+    int minSubArrayLen(const int target, const vector<int> &nums) {
+        const auto n = (int) nums.size();
+        auto res = n + 1;
         for (int l = 0, r = 0, temp = 0; r < n; ++r) {
             temp += nums[r];
             while (l <= r && temp >= target) {
-                result = min(result, r - l + 1);
-                temp -= nums[l++];
+                temp -= nums[l];
+                ++l;
+            }
+            if (l > 0) { // [l-1,r]满足题意
+                res = min(res, r - l + 2);
             }
         }
-        if (result == n + 1) {
-            result = 0;
+        if (res == n + 1) {
+            return 0;
         }
-        return result;
+        return res;
     }
 };
 
