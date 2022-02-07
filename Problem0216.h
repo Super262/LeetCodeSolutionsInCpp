2b2cee7451939db85370c5a907963951ad268b7a
fengwei <virusformail@gmail.com>
2022-02-06 22:28:09 -0800

    Reviewed Problem 216.

--- Problem0216.h
+++ Problem0216.h
@@ -9,33 +9,34 @@
 
 using namespace std;
 
-class Problem0216 {
-private:
-    void dfs(vector<vector<int>> &result,
-             vector<int> &temp,
-             const int currentNum,
-             const int n,
-             const int currentSum,
-             const int k) {
-        if (currentSum == n && temp.size() == k) {  // 序列和为n，包含的个数为k，满足要求
-            result.emplace_back(vector<int>(temp));
-            return;
-        }
-        if (currentNum == 9 || temp.size() == k) {  // 已搜索到最后一个数字或序列长度达到上限
-            return;
-        }
-        for (int i = currentNum + 1; i <= 9; ++i) {
-            temp.emplace_back(i);
-            dfs(result, temp, i, n, currentSum + i, k);
-            temp.pop_back();
-        }
-    }
-
+class Solution {
+public:
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<vector<int>> result;
         vector<int> temp;
         dfs(result, temp, 0, n, 0, k);
         return result;
+    }
+
+private:
+    void dfs(vector<vector<int>> &result,
+             vector<int> &temp,
+             const int num,
+             const int n,
+             const int sum,
+             const int k) {
+        if (sum == n && temp.size() == k) {  // 序列和为n，包含的个数为k，满足要求
+            result.emplace_back(temp);
+            return;
+        }
+        if (num == 9 || temp.size() == k) {  // 已搜索到最后一个数字或序列长度达到上限
+            return;
+        }
+        for (int i = num + 1; i <= 9; ++i) {
+            temp.emplace_back(i);
+            dfs(result, temp, i, n, sum + i, k);
+            temp.pop_back();
+        }
     }
 };
 
