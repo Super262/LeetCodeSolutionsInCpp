641289416c970a799496f0e62db6566ab7634cbf
fengwei <virusformail@gmail.com>
2022-02-06 21:35:39 -0800

    Reviewed Problem 213.

--- Problem0213.h
+++ Problem0213.h
@@ -10,9 +10,11 @@
 using namespace std;
 
 class Solution {
+    // 经典DP，必须掌握
+    // https://www.acwing.com/solution/content/19758/
+    // dp[i % 2][0]：盗取第i个时的最大获利；dp[i % 2][1]：不盗取第i个时的最大获利
 public:
     int rob(const vector<int> &nums) {
-        // https://www.acwing.com/solution/content/19758/
         if (nums.empty()) {
             return 0;
         }
@@ -22,19 +24,18 @@ public:
         if (nums.size() == 2) {
             return max(nums[0], nums[1]);
         }
-        const int n = (int) nums.size();
-        int result = 0;
         // 滚动数组优化
-        int dp[2][2];  // dp[i % 2][0]：盗取第i个时的最大获利；dp[i % 2][1]：不盗取第i个时的最大获利
+        int dp[2][2];
 
         // Choose the first one: [0, n - 2]
         dp[0][0] = 0;
         dp[0][1] = nums[0];
+        const int n = (int) nums.size();
         for (int i = 1; i < n - 1; ++i) {
             dp[i % 2][0] = max(dp[(i - 1) % 2][0], dp[(i - 1) % 2][1]);
             dp[i % 2][1] = dp[(i - 1) % 2][0] + nums[i];
         }
-        result = max(dp[(n - 2) % 2][0], dp[(n - 2) % 2][1]);
+        auto result = max(dp[(n - 2) % 2][0], dp[(n - 2) % 2][1]);
 
         // Do not choose the first one: [1, n - 1]
         dp[1][0] = 0;
