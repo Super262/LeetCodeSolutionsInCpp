84cfdd842285fcb00a75eca460b8cfd5eb7324cb
fengwei <virusformail@gmail.com>
2022-02-08 04:53:05 -0800

    Finished Problem 605.

---
+++ Problem0605.h
@@ -0,0 +1,30 @@
+//
+// Created by Fengwei Zhang on 2/8/22.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0605_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0605_H
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    // 经典算法，直接背诵：贪心思想
+    // https://www.acwing.com/solution/content/469/
+public:
+    bool canPlaceFlowers(vector<int> &flowerbed, int n) {
+        for (int i = 0; i < (int) flowerbed.size(); ++i) {
+            if (flowerbed[i]) {
+                continue;
+            }
+            if ((i == 0 || !flowerbed[i - 1]) && (i + 1 == (int) flowerbed.size() || !flowerbed[i + 1])) {
+                --n;
+                flowerbed[i] = 1;
+            }
+        }
+        return n <= 0;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0605_H
