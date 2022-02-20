126de4834789182f9ecd9b2cbdcd256ddb1a1797
fengwei <virusformail@gmail.com>
2022-02-20 10:34:49 -0800

    Finished Problem 701.

---
+++ Problem0701.h
@@ -0,0 +1,35 @@
+//
+// Created by Fengwei Zhang on 2/20/22.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0701_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0701_H
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode *insertIntoBST(TreeNode *root, int val) {
+        if (!root) {
+            return new TreeNode(val);
+        }
+        if (root->val < val) {
+            root->right = insertIntoBST(root->right, val);
+        } else {
+            root->left = insertIntoBST(root->left, val);
+        }
+        return root;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0701_H
