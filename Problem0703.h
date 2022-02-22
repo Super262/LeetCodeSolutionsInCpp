49a6a438feb96050dbcd7122c212cccd95a939bf
fengwei <virusformail@gmail.com>
2022-02-21 21:40:07 -0800

    Finished Problem 703.

---
+++ Problem0703.h
@@ -0,0 +1,43 @@
+//
+// Created by Fengwei Zhang on 2/21/22.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0703_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0703_H
+
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class KthLargest {
+private:
+    int limit;
+    priority_queue<int, vector<int>, greater<int>> heap;
+
+public:
+    KthLargest(int k, vector<int> &nums) {
+        limit = k;
+        for (const auto &x: nums) {
+            heap.emplace(x);
+            if (heap.size() > limit) {
+                heap.pop();
+            }
+        }
+    }
+
+    int add(int val) {
+        heap.emplace(val);
+        if (heap.size() > limit) {
+            heap.pop();
+        }
+        return heap.top();
+    }
+};
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest* obj = new KthLargest(k, nums);
+ * int param_1 = obj->add(val);
+ */
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0703_H
