e7209859f14abb25172a77a32b1f00bfe33f32b9
fengwei <virusformail@gmail.com>
2022-02-22 23:06:08 -0800

    Finished Problem 726.

--- Problem0726.h
+++ Problem0726.h
@@ -13,61 +13,62 @@
 
 using namespace std;
 
-class Problem0726 {
+class Solution {
     // map存储时，key是有序的
-private:
-    map<string, int> dfs(const string &formula, int &chIdx) {  // 不要忘记：chIdx是引用
-        map<string, int> result;
-        while (chIdx < (int) formula.size()) {
-            if (formula[chIdx] == ')') {  // 递归的结束条件
-                break;
-            }
-            if (formula[chIdx] == '(') {  // 子方程式，递归
-                ++chIdx;  // 跳过左括号
-                auto eleCnt = dfs(formula, chIdx);
-                ++chIdx;  // 跳过右括号
-                // 检测系数是否存在
-                auto i = chIdx;
-                while (i < (int) formula.size() && isdigit(formula[i])) {
-                    ++i;
-                }
-                int factor = 1;
-                if (i > chIdx) {
-                    factor = stoi(formula.substr(chIdx, i - chIdx));
-                    chIdx = i;
-                }
-                for (auto &item: eleCnt) {
-                    result[item.first] += item.second * factor;
-                }
-            } else {
-                auto i = chIdx + 1; // 跳过大写字母
-                while (i < (int) formula.size() && formula[i] >= 'a' && formula[i] <= 'z') {
-                    ++i;
-                }
-                auto key = formula.substr(chIdx, i - chIdx);
-                chIdx = i;
-                int factor = 1;
-                while (i < (int) formula.size() && isdigit(formula[i])) {
-                    ++i;
-                }
-                if (i > chIdx) {
-                    factor = stoi(formula.substr(chIdx, i - chIdx));
-                    chIdx = i;
-                }
-                result[key] += factor;
+public:
+    string countOfAtoms(const string &formula) {
+        int ch_idx = 0;
+        auto ele_cnt = dfs(formula, ch_idx);
+        string result;
+        for (auto &item: ele_cnt) {
+            result += item.first;
+            if (item.second > 1) {
+                result += to_string(item.second);
             }
         }
         return result;
     }
 
-    string countOfAtoms(const string &formula) {
-        int chIdx = 0;
-        auto eleCnt = dfs(formula, chIdx);
-        string result;
-        for (auto &item: eleCnt) {
-            result += item.first;
-            if (item.second > 1) {
-                result += to_string(item.second);
+private:
+    map<string, int> dfs(const string &formula, int &ch_idx) {  // 不要忘记：chIdx是引用
+        map<string, int> result;
+        while (ch_idx < (int) formula.size()) {
+            if (formula[ch_idx] == ')') {  // 递归的结束条件
+                break;
+            }
+            if (formula[ch_idx] == '(') {  // 子方程式，递归
+                ++ch_idx;  // 跳过左括号
+                auto ele_cnt = dfs(formula, ch_idx);
+                ++ch_idx;  // 跳过右括号
+                // 检测系数是否存在
+                auto i = ch_idx;
+                while (i < (int) formula.size() && isdigit(formula[i])) {
+                    ++i;
+                }
+                int factor = 1;
+                if (i > ch_idx) {
+                    factor = stoi(formula.substr(ch_idx, i - ch_idx));
+                    ch_idx = i;
+                }
+                for (auto &item: ele_cnt) {
+                    result[item.first] += item.second * factor;
+                }
+            } else {
+                auto i = ch_idx + 1; // 跳过大写字母
+                while (i < (int) formula.size() && formula[i] >= 'a' && formula[i] <= 'z') {
+                    ++i;
+                }
+                auto key = formula.substr(ch_idx, i - ch_idx);
+                ch_idx = i;
+                int factor = 1;
+                while (i < (int) formula.size() && isdigit(formula[i])) {
+                    ++i;
+                }
+                if (i > ch_idx) {
+                    factor = stoi(formula.substr(ch_idx, i - ch_idx));
+                    ch_idx = i;
+                }
+                result[key] += factor;
             }
         }
         return result;
