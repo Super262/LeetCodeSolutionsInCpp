09d1b61d78e45ad4abf64201d00ab558324e86a2
fengwei <virusformail@gmail.com>
2022-03-06 11:19:57 -0800

    Reviewed Problem 754.

--- Problem0754.h
+++ Problem0754.h
@@ -16,12 +16,12 @@ public:
         if (target == 0) {
             return 0;
         }
-        target = abs(target);
+        target = abs(target);  // target的正、负情况对结果没有影响
         auto k = (int) sqrt(2ll * target) - 1;
-        while ((k + 1ll) * k / 2 < target) {
+        while ((k + 1ll) * k / 2 < target) {  // 利用求和公式找到满足 s = k*(k+1)/2 >= target 的最小k
             ++k;
         }
-        auto d = (int) ((k + 1ll) * k / 2 - target);
+        auto d = (int) ((k + 1ll) * k / 2 - target);  // 设D=(a+b+c+...+x)，若更改a～x的符号为负号后满足题意，则target=s-2*D
         while (d % 2) {
             ++k;
             d += k;
