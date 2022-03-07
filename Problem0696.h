f0ee690315f879c5c293a71766f25ba0fd8c9fa8
fengwei <virusformail@gmail.com>
2022-03-07 01:45:48 -0800

    Reviewed Problem 696.

--- Problem0696.h
+++ Problem0696.h
@@ -13,11 +13,14 @@ class Solution {
     // 直接背诵：若s由k个连续的1和k个连续的0组成，s共包含k个满足题意的子串
 public:
     int countBinarySubstrings(const string &s) {
-        int res = 0, last = 0;
-        for (int i = 0; i < s.size(); i++) {
-            int j = i + 1;
-            while (j < s.size() && s[j] == s[i]) j++;
-            int cur = j - i;
+        int res = 0;
+        int last = 0;
+        for (int i = 0; i < (int) s.size(); ++i) {
+            auto j = i + 1;
+            while (j < (int) s.size() && s[j] == s[i]) {
+                ++j;
+            }
+            auto cur = j - i;
             i = j - 1;
             res += min(last, cur);
             last = cur;
