4ade6c589b0381dfeb2f49e27b943648d69b7a4d
fengwei <virusformail@gmail.com>
2022-03-07 11:16:48 -0800

    Reviewed Problem 673.

--- Problem0673.h
+++ Problem0673.h
@@ -14,26 +14,27 @@ class Solution {
 public:
     int findNumberOfLIS(const vector<int> &nums) {
         const int n = (int) nums.size();
-        int dp[n], g[n];  // dp[i]表示以nums[i]结尾的LIS的最大长度，g[i]表示以nums[i]结尾的LIS的个数
+        int f[n];  // f[i]表示以nums[i]结尾的LIS的最大长度
+        int g[n];  // g[i]表示以nums[i]结尾的LIS的个数
         int max_l = 0, max_cnt = 0;
         for (int i = 0; i < n; ++i) {
-            dp[i] = 1;
+            f[i] = 1;
             g[i] = 1;
             for (int j = 0; j < i; ++j) {
                 if (nums[j] >= nums[i]) {
                     continue;
                 }
-                if (dp[i] < dp[j] + 1) {
-                    dp[i] = dp[j] + 1;
+                if (f[i] < f[j] + 1) {
+                    f[i] = f[j] + 1;
                     g[i] = g[j];
-                } else if (dp[i] == dp[j] + 1) {
+                } else if (f[i] == f[j] + 1) {
                     g[i] += g[j];
                 }
             }
-            if (dp[i] > max_l) {
-                max_l = dp[i];
+            if (f[i] > max_l) {
+                max_l = f[i];
                 max_cnt = g[i];
-            } else if (dp[i] == max_l) {
+            } else if (f[i] == max_l) {
                 max_cnt += g[i];
             }
         }
