5dd8224285517232d43cd28b26435ab08fab412b
fengwei <virusformail@gmail.com>
2022-03-18 07:52:49 -0700

    Finished Problem 1009.

---
+++ Problem1009.h
@@ -0,0 +1,22 @@
+//
+// Created by Fengwei Zhang on 3/18/22.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM1009_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM1009_H
+
+class Solution {
+public:
+    int bitwiseComplement(int n) {
+        if (!n) {
+            return 1;
+        }
+        int ones_cnt = 0;
+        for (auto x = n; x > 0; x >>= 1) {
+            ++ones_cnt;
+        }
+        return (int) (~n & ((1ll << ones_cnt) - 1));
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM1009_H
