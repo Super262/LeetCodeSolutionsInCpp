0e08855ba8c4a5975f664067d04951286b707f7b
fengwei <virusformail@gmail.com>
2022-03-18 18:38:26 -0700

    Reviewed Problem 443.

--- Problem0443.h
+++ Problem0443.h
@@ -14,30 +14,30 @@ class Solution {
     // 双指针经典应用
 public:
     int compress(vector<char> &chars) {
-        int k = 0;
+        int i = 0;
         for (int l = 0; l < chars.size(); ++l) {
             auto r = l;
             while (r < chars.size() && chars[r] == chars[l]) {
                 ++r;
             }
             if (r - l > 0) {
-                chars[k] = chars[l];
-                ++k;
+                chars[i] = chars[l];
+                ++i;
             }
             if (r - l > 1) {
                 // 这里不要使用to_string方法：不满足空间为O(1)
                 auto len = r - l;
-                auto t = k;
+                auto t = i;
                 while (len) {
-                    chars[k] = (char) ('0' + (len % 10));
+                    chars[i] = (char) ('0' + (len % 10));
                     len /= 10;
-                    ++k;
+                    ++i;
                 }
-                reverse(chars.begin() + t, chars.begin() + k);
+                reverse(chars.begin() + t, chars.begin() + i);
             }
             l = r - 1;
         }
-        while (chars.size() > k) {
+        while (chars.size() > i) {
             chars.pop_back();
         }
         return (int) chars.size();
