f2aca31623355c6200422644e9db97f7a06e4637
fengwei <virusformail@gmail.com>
2022-03-20 14:55:15 -0700

    Reviewed Problem 312.

--- Problem0312.h
+++ Problem0312.h
@@ -11,29 +11,25 @@
 using namespace std;
 
 class Solution {
+    // dp[st][ed]：打碎 balls[st + 1 : ed - 1] 后的最大价值
 public:
     int maxCoins(const vector<int> &nums) {
-        // 背诵代码！
-        const int n = (int) nums.size();
-        int temp[n + 2];
-        temp[0] = 1;
-        temp[n + 1] = 1;
-        for (int i = 1; i <= n; ++i) {  // 拷贝原数据，开头和结尾补充1
-            temp[i] = nums[i - 1];
-        }
-        // 注意这里的方程：dp[st][ed]表示打碎ballons[st + 1 : ed - 1]的最大价值！
-        int dp[n + 2][n + 2];
-        memset(dp, 0, sizeof dp);
+        const auto n = (int) nums.size();
+        int balls[n + 2];
+        copy(nums.begin(), nums.end(), balls + 1);  // 拷贝原数据
+        balls[0] = 1;
+        balls[n + 1] = 1;  // 开头和结尾补充1
+        int f[n + 2][n + 2];
+        memset(f, 0, sizeof f);
         for (int len = 3; len <= n + 2; ++len) {
             for (int st = 0; st + len - 1 < n + 2; ++st) {
                 const auto ed = st + len - 1;
                 for (int mid = st + 1; mid < ed; ++mid) {
-                    dp[st][ed] = max(dp[st][ed], temp[st] * temp[mid] * temp[ed] + dp[st][mid] + dp[mid][ed]);
+                    f[st][ed] = max(f[st][ed], balls[st] * balls[mid] * balls[ed] + f[st][mid] + f[mid][ed]);
                 }
             }
         }
-
-        return dp[0][n + 1];
+        return f[0][n + 1];
     }
 };
 
