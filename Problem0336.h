f28b1ad1f83285792afb7e403b28465575956912
fengwei <virusformail@gmail.com>
2022-03-20 02:13:43 -0700

    Reviewed Problem 336.

--- Problem0336.h
+++ Problem0336.h
@@ -11,7 +11,36 @@
 
 using namespace std;
 
-class Problem0336 {
+class Solution {
+public:
+    vector<vector<int>> palindromePairs(const vector<string> &words) {
+        unordered_map<string, int> reversed_idx;  // 保存反转后的key的索引
+        for (int i = 0; i < (int) words.size(); ++i) {
+            reversed_idx[string(words[i].rbegin(), words[i].rend())] = i;
+        }
+        vector<vector<int>> result;
+        for (int i = 0; i < (int) words.size(); ++i) {
+            const auto &w = words[i];
+            for (int l = 0; l <= (int) w.size(); ++l) {
+                auto left_part = w.substr(0, l);
+                auto right_part = w.substr(l);
+                if (isPalindrome(right_part) && reversed_idx.count(left_part) && reversed_idx[left_part] != i) {
+                    // 尝试构建(left_part + right_part + reverse(left_part))为回文字符串
+                    // 注意：reverse(left_part)不能等于leftPart，因为题目要求由2个单词构建字符串
+                    result.push_back({i, reversed_idx[left_part]});
+                }
+                if (isPalindrome(left_part) && reversed_idx.count(right_part) && reversed_idx[right_part] != i &&
+                    w.size() != words[reversed_idx[right_part]].size()) {
+                    // 尝试构建(reverse(right_part) + left_part + right_part)为回文字符串
+                    // 注意：reverse(right_part)不能等于rightPart，因为题目要求由2个单词构建字符串
+                    // 同时：避免重复加入(reverse(w) + w)的情况（即leftPart == ""）
+                    result.push_back({reversed_idx[right_part], i});
+                }
+            }
+        }
+        return result;
+    }
+
 private:
     bool isPalindrome(const string &s) {
         int i = 0, j = (int) s.size() - 1;
@@ -23,35 +52,6 @@ private:
             --j;
         }
         return true;
-    }
-
-public:
-    vector<vector<int>> palindromePairs(vector<string> &words) {
-        unordered_map<string, int> reversedIdx;  // 保存反转后的key的索引
-        for (int i = 0; i < (int) words.size(); ++i) {
-            reversedIdx[string(words[i].rbegin(), words[i].rend())] = i;
-        }
-        vector<vector<int>> result;
-        for (int i = 0; i < (int) words.size(); ++i) {
-            const auto &w = words[i];
-            for (int l = 0; l <= (int) w.size(); ++l) {
-                auto leftPart = w.substr(0, l);
-                auto rightPart = w.substr(l);
-                if (isPalindrome(rightPart) && reversedIdx.count(leftPart) && reversedIdx[leftPart] != i) {
-                    // 尝试构建(leftPart + rightPart + reverse(leftPart))为回文字符串
-                    // 注意：reverse(leftPart)不能等于leftPart，因为题目要求由2个单词构建字符串
-                    result.push_back({i, reversedIdx[leftPart]});
-                }
-                if (isPalindrome(leftPart) && reversedIdx.count(rightPart) && reversedIdx[rightPart] != i &&
-                    w.size() != words[reversedIdx[rightPart]].size()) {
-                    // 尝试构建(reverse(rightPart) + leftPart + rightPart)为回文字符串
-                    // 注意：reverse(rightPart)不能等于rightPart，因为题目要求由2个单词构建字符串
-                    // 同时：避免重复加入(reverse(w) + w)的情况（即leftPart == ""）
-                    result.push_back({reversedIdx[rightPart], i});
-                }
-            }
-        }
-        return result;
     }
 };
 
