653ddd6ccce3d4c3fda37cc58bd6948bb8efba51
fengwei <virusformail@gmail.com>
2022-03-19 21:36:45 -0700

    Reviewed Problem 377.

--- Problem0377.h
+++ Problem0377.h
@@ -14,7 +14,7 @@ class Solution {
     // 类似于完全背包问题：https://www.acwing.com/solution/content/5005/
     // 如果此题用完全背包解答，则会将顺序不同的序列算作相同的组合
 public:
-    int combinationSum4(vector<int> &nums, const int &target) {
+    int combinationSum4(const vector<int> &nums, const int &target) {
         unsigned long long dp[target + 1];  // 防止溢出！
         memset(dp, 0, sizeof dp);
         dp[0] = 1;
