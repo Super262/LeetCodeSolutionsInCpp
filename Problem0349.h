000bbf858010873618fd27b01c40e5aa9cbb8059
fengwei <virusformail@gmail.com>
2022-03-20 01:04:17 -0700

    Reviewed Problem 349.

--- Problem0349.h
+++ Problem0349.h
@@ -14,13 +14,13 @@ class Solution {
 public:
     vector<int> intersection(const vector<int> &nums1, const vector<int> &nums2) {
         vector<int> result;
-        unordered_set<int> uniqueNums(nums1.begin(), nums1.end());
+        unordered_set<int> unique_nums(nums1.begin(), nums1.end());
         for (const auto &x: nums2) {
-            if (!uniqueNums.count(x)) {
+            if (!unique_nums.count(x)) {
                 continue;
             }
             result.push_back(x);
-            uniqueNums.erase(x);
+            unique_nums.erase(x);
         }
         return result;
     }
