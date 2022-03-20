4a3280aa9d41c3856f2fa3978b85e27b53e14f9b
fengwei <virusformail@gmail.com>
2022-03-20 01:34:56 -0700

    Reviewed Problem 341.

--- Problem0341.h
+++ Problem0341.h
@@ -5,43 +5,26 @@
 #ifndef LEETCODESOLUTIONSINCPP_PROBLEM0341_H
 #define LEETCODESOLUTIONSINCPP_PROBLEM0341_H
 
-/**
- * // This is the interface that allows for creating nested lists.
- * // You should not implement it, or speculate about its implementation
- * class NestedInteger {
- *   public:
- *     // Return true if this NestedInteger holds a single integer, rather than a nested list.
- *     bool isInteger() const;
- *
- *     // Return the single integer that this NestedInteger holds, if it holds a single integer
- *     // The result is undefined if this NestedInteger holds a nested list
- *     int getInteger() const;
- *
- *     // Return the nested list that this NestedInteger holds, if it holds a nested list
- *     // The result is undefined if this NestedInteger holds a single integer
- *     const vector<NestedInteger> &getList() const;
- * };
- */
-
 #include <vector>
 #include <queue>
 
 using namespace std;
 
+class NestedInteger {
+public:
+    // Return true if this NestedInteger holds a single integer, rather than a nested list.
+    bool isInteger() const;
+
+    // Return the single integer that this NestedInteger holds, if it holds a single integer
+    // The result is undefined if this NestedInteger holds a nested list
+    int getInteger() const;
+
+    // Return the nested list that this NestedInteger holds, if it holds a nested list
+    // The result is undefined if this NestedInteger holds a single integer
+    const vector<NestedInteger> &getList() const;
+};
+
 class NestedIterator {
-private:
-    queue<int> data;
-
-    void flatten(const vector<NestedInteger> &nestedList) {
-        for (const auto &ni: nestedList) {
-            if (ni.isInteger()) {
-                data.emplace(ni.getInteger());
-            } else {
-                flatten(ni.getList());
-            }
-        }
-    }
-
 public:
     NestedIterator(vector<NestedInteger> &nestedList) {
         flatten(nestedList);
@@ -55,6 +38,19 @@ public:
 
     bool hasNext() {
         return !data.empty();
+    }
+
+private:
+    queue<int> data;
+
+    void flatten(const vector<NestedInteger> &nestedList) {
+        for (const auto &ni: nestedList) {
+            if (ni.isInteger()) {
+                data.emplace(ni.getInteger());
+            } else {
+                flatten(ni.getList());
+            }
+        }
     }
 };
 
