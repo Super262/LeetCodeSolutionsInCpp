4482fa8e747f8e56201e753e29397d4b57dc457c
fengwei <virusformail@gmail.com>
2022-03-19 17:51:45 -0700

    Reviewed Problem 381.

--- Problem0381.h
+++ Problem0381.h
@@ -13,7 +13,7 @@ using namespace std;
 
 class RandomizedCollection {
 private:
-    unordered_map<int, unordered_set<int>> num2IdxSet;
+    unordered_map<int, unordered_set<int>> num_idx_set;
     vector<int> storage;
 
 public:
@@ -23,25 +23,25 @@ public:
 
     bool insert(const int &val) {
         storage.push_back(val);
-        num2IdxSet[val].insert((int) storage.size() - 1);
-        return num2IdxSet[val].size() == 1;
+        num_idx_set[val].insert((int) storage.size() - 1);
+        return num_idx_set[val].size() == 1;
     }
 
     bool remove(const int &x) {
-        if (!num2IdxSet.count(x) || num2IdxSet[x].empty()) {
+        if (!num_idx_set.count(x) || num_idx_set[x].empty()) {
             return false;
         }
-        auto px = *(num2IdxSet[x].begin());
+        auto px = *(num_idx_set[x].begin());
         auto py = (int) storage.size() - 1;
         auto y = storage[py];
         swap(storage[px], storage[py]);
         // 注意：对包含x的索引的集合，先执行插入py，再删除py，不能省略任何操作（因为可能 x == y）！
-        num2IdxSet[x].erase(px);
-        num2IdxSet[x].insert(py);
-        num2IdxSet[y].erase(py);
-        num2IdxSet[y].insert(px);
+        num_idx_set[x].erase(px);
+        num_idx_set[x].insert(py);
+        num_idx_set[y].erase(py);
+        num_idx_set[y].insert(px);
         storage.pop_back();
-        num2IdxSet[x].erase(py);
+        num_idx_set[x].erase(py);
         return true;
     }
 
