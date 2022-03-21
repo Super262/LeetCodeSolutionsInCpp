772567f2ebea4c550a88825d7b16a93af0d23c84
fengwei <virusformail@gmail.com>
2022-03-21 16:09:03 -0700

    Reviewed Problem 226.

--- Problem0226.h
+++ Problem0226.h
@@ -5,22 +5,24 @@
 #ifndef LEETCODESOLUTIONSINCPP_PROBLEM0226_H
 #define LEETCODESOLUTIONSINCPP_PROBLEM0226_H
 
+#include <algorithm>
+
 using namespace std;
 
-class Problem0226 {
-private:
-    struct TreeNode {
-        int val;
-        TreeNode *left;
-        TreeNode *right;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
 
-        TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
 
-        TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
 
-        TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-    };
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
 
+class Solution {
+public:
     TreeNode *invertTree(TreeNode *root) {
         if (!root) {
             return root;
