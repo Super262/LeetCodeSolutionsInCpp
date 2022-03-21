d6d75ae9048ba6fe88e06be9ef1d2a338f003a8f
fengwei <virusformail@gmail.com>
2022-03-21 16:28:39 -0700

    Reviewed Problem 224.

--- Problem0224.h
+++ Problem0224.h
@@ -15,35 +15,42 @@ class Solution {
 public:
     int calculate(const string &s) {
         stack<int> ops;
-        ops.push(1);
-
         int sign = 1;
-        int ans = 0;
-        auto n = (int) s.length();
+        auto n = (int) s.size();
         int i = 0;
+        int ans = 0;
+        ops.emplace(sign);
         while (i < n) {
             if (s[i] == ' ') {
-                i++;
-            } else if (s[i] == '+') {
-                sign = ops.top();
-                i++;
-            } else if (s[i] == '-') {
-                sign = -ops.top();
-                i++;
-            } else if (s[i] == '(') {
-                ops.push(sign);
-                i++;
-            } else if (s[i] == ')') {
-                ops.pop();
-                i++;
-            } else {
-                long num = 0;
-                while (i < n && s[i] >= '0' && s[i] <= '9') {
-                    num = num * 10 + s[i] - '0';
-                    i++;
-                }
-                ans += (int) (sign * num);
+                ++i;
+                continue;
             }
+            if (s[i] == '+') {
+                sign = ops.top();
+                ++i;
+                continue;
+            }
+            if (s[i] == '-') {
+                sign = -ops.top();
+                ++i;
+                continue;
+            }
+            if (s[i] == '(') {
+                ops.push(sign);
+                ++i;
+                continue;
+            }
+            if (s[i] == ')') {
+                ops.pop();
+                ++i;
+                continue;
+            }
+            long long num = 0;
+            while (i < n && isdigit(s[i])) {
+                num = num * 10 + s[i] - '0';
+                ++i;
+            }
+            ans += (int) (sign * num);
         }
         return ans;
     }
