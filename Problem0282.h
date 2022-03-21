daa796639720d2e77359546ab461ac6d1ce21920
fengwei <virusformail@gmail.com>
2022-03-20 18:30:15 -0700

    Reviewed Problem 282.

--- Problem0282.h
+++ Problem0282.h
@@ -11,46 +11,45 @@
 using namespace std;
 
 class Solution {
+    // 巧妙的常数优化：https://www.acwing.com/solution/content/332/
 public:
     vector<string> addOperators(const string &num, const int &target) {
-        // https://www.acwing.com/solution/content/332/
         vector<string> result;
         string path(100, '\0');
-        int a = 0, b = 1;
-        dfs(num, target, 0, a, b, path, 0, result);
+        dfs(num, target, 0, 0, 1, path, 0, result);
         return result;
     }
 
 private:
     void dfs(const string &num,
              const int &target,
-             const int &curIdx,
+             const int &cur_idx,
              const long long &a,
              const long long &b,
              string &path,
              const int &len,
              vector<string> &result) {
-        if (curIdx == num.size()) {
+        if (cur_idx == num.size()) {
             if (target == a) {
                 result.emplace_back(path.substr(0, len - 1));  // 需要排除最后的“+”
             }
             return;
         }
         long long c = 0;
-        for (int i = curIdx; i < (int) num.size(); ++i) {
+        for (auto i = cur_idx; i < (int) num.size(); ++i) {
             c = c * 10 + (num[i] - '0');
-            path[len + i - curIdx] = num[i];
-            path[len + i - curIdx + 1] = '+';
-            dfs(num, target, i + 1, a + b * c, 1, path, len + i - curIdx + 2, result);
+            path[len + i - cur_idx] = num[i];
+            path[len + i - cur_idx + 1] = '+';
+            dfs(num, target, i + 1, a + b * c, 1, path, len + i - cur_idx + 2, result);
             if (i + 1 < num.size()) {
-                path[len + i - curIdx + 1] = '-';
-                dfs(num, target, i + 1, a + b * c, -1, path, len + i - curIdx + 2, result);
+                path[len + i - cur_idx + 1] = '-';
+                dfs(num, target, i + 1, a + b * c, -1, path, len + i - cur_idx + 2, result);
             }
             if (i + 1 < num.size()) {
-                path[len + i - curIdx + 1] = '*';
-                dfs(num, target, i + 1, a, b * c, path, len + i - curIdx + 2, result);
+                path[len + i - cur_idx + 1] = '*';
+                dfs(num, target, i + 1, a, b * c, path, len + i - cur_idx + 2, result);
             }
-            if (num[curIdx] == '0') {
+            if (num[cur_idx] == '0') {
                 break;
             }
         }
