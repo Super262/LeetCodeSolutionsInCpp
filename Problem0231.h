766a85975f069579e1adfb3add9d9c6b8a442427
fengwei <virusformail@gmail.com>
2022-03-21 15:28:25 -0700

    Reviewed Problem 231.

--- Problem0231.h
+++ Problem0231.h
@@ -5,7 +5,7 @@
 #ifndef LEETCODESOLUTIONSINCPP_PROBLEM0231_H
 #define LEETCODESOLUTIONSINCPP_PROBLEM0231_H
 
-class Problem0231 {
+class Solution {
 private:
     bool isPowerOfTwo(int n) {
         return n > 0 && (n & -n) == n;
