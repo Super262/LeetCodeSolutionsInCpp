a8575a914d2aa5a90cb739a142e782e422c39379
fengwei <virusformail@gmail.com>
2022-03-21 16:44:36 -0700

    Reviewed Problem 221.

--- Problem0221.h
+++ Problem0221.h
@@ -12,24 +12,24 @@ using namespace std;
 
 class Solution {
     // 经典算法，必须掌握
-    // 动态规划问题：dp[i][j]是(i, j)区域内矩形的最大边长
+    // 动态规划问题：f[i][j]是(i, j)区域内矩形的最大边长
 public:
     int maximalSquare(const vector<vector<char>> &matrix) {
         const auto m = (int) matrix.size();
         const auto n = (int) matrix[0].size();
-        int dp[m + 1][n + 1];
-        memset(dp, 0, sizeof dp);
-        int result = 0;
+        int f[m + 1][n + 1];
+        memset(f, 0, sizeof f);
+        int ans = 0;
         for (int i = 1; i <= m; ++i) {
             for (int j = 1; j <= n; ++j) {
                 if (matrix[i - 1][j - 1] == '0') {
                     continue;
                 }
-                dp[i][j] = min(min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
-                result = max(result, dp[i][j] * dp[i][j]);
+                f[i][j] = min(min(f[i - 1][j], f[i][j - 1]), f[i - 1][j - 1]) + 1;
+                ans = max(ans, f[i][j] * f[i][j]);
             }
         }
-        return result;
+        return ans;
     }
 };
 
