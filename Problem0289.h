6fe095f3bb240d86994013c562b4e1175db4f227
fengwei <virusformail@gmail.com>
2022-03-20 17:33:15 -0700

    Reviewed Problem 289.

--- Problem0289.h
+++ Problem0289.h
@@ -10,11 +10,11 @@
 using namespace std;
 
 class Solution {
+    // 利用整数的其它2进制位记录未来状态，最后1位记录当前状态
+    // https://www.acwing.com/solution/content/291/
 public:
     void gameOfLife(vector<vector<int>> &board) {
-        // 利用整数的其它2进制位记录未来状态，最后1位记录当前状态
-        // https://www.acwing.com/solution/content/291/
-        const int m = (int) board.size();
+        const auto m = (int) board.size();
         if (m == 0) {
             return;
         }
@@ -24,7 +24,7 @@ public:
         }
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                int livingOnes = 0;
+                int living_ones = 0;
                 for (int di = -1; di <= 1; ++di) {
                     for (int dj = -1; dj <= 1; ++dj) {
                         auto ni = i + di;
@@ -35,17 +35,17 @@ public:
                         if (ni < 0 || ni >= m || nj < 0 || nj >= n) {
                             continue;
                         }
-                        livingOnes += (board[ni][nj] & 1);  // 最后1位表示状态
+                        living_ones += (board[ni][nj] & 1);  // 最后1位表示状态
                     }
                 }
                 if ((board[i][j] & 1) == 1) {
-                    if (livingOnes < 2 || livingOnes > 3) {
+                    if (living_ones < 2 || living_ones > 3) {
                         board[i][j] = 1;  // 未来是0，当前是1：01
                     } else {
                         board[i][j] = 3;  // 未来是1，当前是1：11
                     }
                 } else {
-                    if (livingOnes == 3) {
+                    if (living_ones == 3) {
                         board[i][j] = 2;  // 未来是1，当前是0：10
                     } else {
                         board[i][j] = 0;  // 未来是0，当前是0：00
