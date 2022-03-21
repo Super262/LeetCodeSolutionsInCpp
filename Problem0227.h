41eb55638d32714d9dcf80743174b0eebb413377
fengwei <virusformail@gmail.com>
2022-03-21 16:04:44 -0700

    Reviewed Problem 227.

--- Problem0227.h
+++ Problem0227.h
@@ -6,13 +6,50 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0227_H
 
 #include <stack>
+#include <string>
 #include <cctype>
 
 using namespace std;
 
-class Problem0227 {
+class Solution {
+public:
+    int calculate(const string &s) {
+        int priority[256];
+        priority['+'] = 1;
+        priority['-'] = 1;
+        priority['*'] = 2;
+        priority['/'] = 2;
+        stack<char> ops;
+        stack<int> nums;
+        for (int i = 0; i < s.size(); ++i) {
+            auto ch = s[i];
+            if (ch == ' ') {
+                continue;
+            }
+            if (isdigit(ch)) {
+                int x = 0;
+                int j = i;
+                while (j < s.size() && isdigit(s[j])) {
+                    x = x * 10 + (s[j] - '0');
+                    ++j;
+                }
+                nums.emplace(x);
+                i = j - 1;
+            } else {
+                while (!ops.empty() && priority[ops.top()] >= priority[ch]) {
+                    eval(nums, ops);
+                }
+                ops.emplace(ch);
+            }
+        }
+        while (!ops.empty()) {
+            eval(nums, ops);
+        }
+        return nums.top();
+    }
+
 private:
-    void evaluate(stack<int> &nums, stack<char> &ops) {
+    void eval(stack<int> &nums, stack<char> &ops) {
         auto b = nums.top();
         nums.pop();
         auto a = nums.top();
@@ -31,39 +68,6 @@ private:
         if (op == '-') {
             nums.emplace(a - b);
         }
-    }
-
-    int calculate(string s) {
-        stack<char> ops;
-        stack<int> nums;
-        int priority[256];
-        priority['+'] = priority['-'] = 1;
-        priority['*'] = priority['/'] = 2;
-        for (int i = 0; i < s.size(); ++i) {
-            auto ch = s[i];
-            if (ch == ' ') {
-                continue;
-            }
-            if (isdigit(ch)) {
-                int x = 0;
-                int j = i;
-                while (j < s.size() && isdigit(s[j])) {
-                    x = x * 10 + (s[j] - '0');
-                    ++j;
-                }
-                nums.emplace(x);
-                i = j - 1;
-            } else {
-                while (!ops.empty() && priority[ops.top()] >= priority[ch]) {
-                    evaluate(nums, ops);
-                }
-                ops.emplace(ch);
-            }
-        }
-        while (!ops.empty()) {
-            evaluate(nums, ops);
-        }
-        return nums.top();
     }
 };
 
