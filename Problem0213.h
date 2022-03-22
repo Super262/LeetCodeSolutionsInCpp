138beb85a7bc114edff51d4b7da3bba341740305
fengwei <virusformail@gmail.com>
2022-03-21 17:21:44 -0700

    Reviewed Problem 213.

--- Problem0213.h
+++ Problem0213.h
@@ -12,7 +12,7 @@ using namespace std;
 class Solution {
     // 经典DP，必须掌握
     // https://www.acwing.com/solution/content/19758/
-    // dp[i % 2][1]：盗取第i个时的最大获利；dp[i % 2][0]：不盗取第i个时的最大获利
+    // f[i % 2][1]：盗取第i个时的最大获利；f[i % 2][0]：不盗取第i个时的最大获利
 public:
     int rob(const vector<int> &nums) {
         if (nums.empty()) {
@@ -25,26 +25,26 @@ public:
             return max(nums[0], nums[1]);
         }
         // 滚动数组优化
-        int dp[2][2];
+        int f[2][2];
 
         // Choose the first one: [0, n - 2]
-        dp[0][0] = 0;
-        dp[0][1] = nums[0];
-        const int n = (int) nums.size();
+        f[0][0] = 0;
+        f[0][1] = nums[0];
+        const auto n = (int) nums.size();
         for (int i = 1; i < n - 1; ++i) {
-            dp[i % 2][0] = max(dp[(i - 1) % 2][0], dp[(i - 1) % 2][1]);
-            dp[i % 2][1] = dp[(i - 1) % 2][0] + nums[i];
+            f[i % 2][0] = max(f[(i - 1) % 2][0], f[(i - 1) % 2][1]);
+            f[i % 2][1] = f[(i - 1) % 2][0] + nums[i];
         }
-        auto result = max(dp[(n - 2) % 2][0], dp[(n - 2) % 2][1]);
+        auto result = max(f[(n - 2) % 2][0], f[(n - 2) % 2][1]);
 
         // Do not choose the first one: [1, n - 1]
-        dp[1][0] = 0;
-        dp[1][1] = nums[1];
+        f[1][0] = 0;
+        f[1][1] = nums[1];
         for (int i = 2; i < n; ++i) {
-            dp[i % 2][0] = max(dp[(i - 1) % 2][0], dp[(i - 1) % 2][1]);
-            dp[i % 2][1] = dp[(i - 1) % 2][0] + nums[i];
+            f[i % 2][0] = max(f[(i - 1) % 2][0], f[(i - 1) % 2][1]);
+            f[i % 2][1] = f[(i - 1) % 2][0] + nums[i];
         }
-        result = max(max(dp[(n - 1) % 2][0], dp[(n - 1) % 2][1]), result);
+        result = max(max(f[(n - 1) % 2][0], f[(n - 1) % 2][1]), result);
 
         return result;
     }
