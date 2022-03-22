258ded9633461bfa3068b4b9495f577733d7f574
fengwei <virusformail@gmail.com>
2022-03-22 08:13:20 -0700

    Reviewed Problem 122.

--- Problem0122.h
+++ Problem0122.h
@@ -10,8 +10,9 @@
 using namespace std;
 
 class Solution {
+    // 一段时间内的交易 == 多个单天的交易
 public:
-    int maxProfit(const vector<int> &prices) {  // 一段时间内的交易 == 多个单天的交易
+    int maxProfit(const vector<int> &prices) {
         int result = 0;
         for (int i = 1; i < (int) prices.size(); ++i) {
             if (prices[i] > prices[i - 1]) {
