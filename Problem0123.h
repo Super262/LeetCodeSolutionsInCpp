5e3016098a6b201824352f7b459cb98e7b5584f2
fengwei <virusformail@gmail.com>
2022-03-22 08:12:14 -0700

    Reviewed Problem 123.

--- Problem0123.h
+++ Problem0123.h
@@ -10,8 +10,9 @@
 using namespace std;
 
 class Solution {
+    // 前后缀分解：预处理前缀，枚举后缀
 public:
-    int maxProfit(const vector<int> &prices) {  // 前后缀分解：预处理前缀，枚举后缀
+    int maxProfit(const vector<int> &prices) {
         int f[prices.size()];
         auto pre_min = prices[0];
         // 1. 预处理f[i]：在第0～i天进行一次交易的最大获利
@@ -22,10 +23,10 @@ public:
         }
 
         int result = 0;
-        auto next_max = prices[prices.size() - 1];
+        auto next_max = prices.back();
         // 2. 反向扫描，寻找在第i天买入（一次交易）的最大获利
         result = max(result, f[prices.size() - 1]); // 边界情况: 不进行第2次交易
-        for (int i = (int) prices.size() - 2; i >= 1; --i) {
+        for (auto i = (int) prices.size() - 2; i >= 1; --i) {
             result = max(result, f[i - 1] + next_max - prices[i]);
             next_max = max(next_max, prices[i]);
         }
