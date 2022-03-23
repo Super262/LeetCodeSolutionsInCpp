aae19bd5f4943cbd91fcdf58689f9410810ff38b
fengwei <virusformail@gmail.com>
2022-03-23 14:00:02 -0700

    Reviewed Problem 5.

--- Problem0005.h
+++ Problem0005.h
@@ -15,11 +15,13 @@ class Solution {
     // 区间DP，dp[i][j]：s[i:j]是否为回文字符串
 public:
     string longestPalindrome(const string &s) {
-        bool dp[s.size()][s.size()];  // s[i, j]是回文串，dp[i][j] == true（i，j从0开始）
+        const auto n = (int) s.size();
+        bool dp[n][n];  // s[i, j]是回文串，dp[i][j] == true（i，j从0开始）
         memset(dp, 0, sizeof dp);
-        string result;
-        for (int length = 1; length <= (int) s.size(); ++length) {
-            for (int l = 0; l + length - 1 < (int) s.size(); ++l) {
+        int ans_idx = -1;
+        int ans_len = 0;
+        for (int length = 1; length <= n; ++length) {
+            for (int l = 0; l + length - 1 < n; ++l) {
                 auto r = l + length - 1;
                 if (length == 1) {
                     dp[l][r] = true;
@@ -28,12 +30,13 @@ public:
                 } else {
                     dp[l][r] = s[l] == s[r] && dp[l + 1][r - 1];
                 }
-                if (dp[l][r] && r - l + 1 > (int) result.size()) {
-                    result = s.substr(l, r - l + 1);
+                if (dp[l][r] && r - l + 1 > ans_len) {
+                    ans_idx = l;
+                    ans_len = r - l + 1;
                 }
             }
         }
-        return result;
+        return s.substr(ans_idx, ans_len);
     }
 };
 
