19972539c53c60d596fc890b149317591d4eabdb
fengwei <virusformail@gmail.com>
2022-03-23 08:42:35 -0700

    Reviewed Problem 63.

--- Problem0063.h
+++ Problem0063.h
@@ -13,38 +13,38 @@ using namespace std;
 
 class Solution {
 public:
-    int uniquePathsWithObstacles(const vector<vector<int>> &obstacleGrid) {
-        const int m = (int) obstacleGrid.size();
-        const int n = (int) obstacleGrid[0].size();
-        if (obstacleGrid[0][0] || obstacleGrid[m - 1][n - 1]) { // 注意边界情况（输入数据为[[1]]）
+    int uniquePathsWithObstacles(const vector<vector<int>> &obstacle_grid) {
+        const int m = (int) obstacle_grid.size();
+        const int n = (int) obstacle_grid[0].size();
+        if (obstacle_grid[0][0] || obstacle_grid[m - 1][n - 1]) { // 注意边界情况（输入数据为[[1]]）
             return 0;
         }
-        int dp[m][n];
-        dp[0][0] = 1;
+        int f[m][n];
+        f[0][0] = 1;
         for (int i = 1; i < m; ++i) {
-            if (obstacleGrid[i][0]) {
-                dp[i][0] = 0;
+            if (obstacle_grid[i][0]) {
+                f[i][0] = 0;
             } else {
-                dp[i][0] = dp[i - 1][0];
+                f[i][0] = f[i - 1][0];
             }
         }
         for (int i = 1; i < n; ++i) {
-            if (obstacleGrid[0][i]) {
-                dp[0][i] = 0;
+            if (obstacle_grid[0][i]) {
+                f[0][i] = 0;
             } else {
-                dp[0][i] = dp[0][i - 1];
+                f[0][i] = f[0][i - 1];
             }
         }
         for (int i = 1; i < m; ++i) {
             for (int j = 1; j < n; ++j) {
-                dp[i][j] = 0;
-                if (obstacleGrid[i][j]) {
+                f[i][j] = 0;
+                if (obstacle_grid[i][j]) {
                     continue;
                 }
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                f[i][j] = f[i - 1][j] + f[i][j - 1];
             }
         }
-        return dp[m - 1][n - 1];
+        return f[m - 1][n - 1];
     }
 };
 
