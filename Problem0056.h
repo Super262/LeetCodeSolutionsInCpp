a354f46c67b6ca107ed629eeb9ee7489049d74b7
fengwei <virusformail@gmail.com>
2022-03-23 09:09:12 -0700

    Reviewed Problem 56.

--- Problem0056.h
+++ Problem0056.h
@@ -14,16 +14,15 @@ using namespace std;
 class Solution {
     // 经典算法，直接背诵
 public:
-    // We can sort vectors!
     vector<vector<int>> merge(vector<vector<int>> &intervals) {
         sort(intervals.begin(), intervals.end());
         vector<vector<int>> result;
-        result.push_back(intervals[0]);
-        for (unsigned long long i = 1; i < intervals.size(); ++i) {
+        result.emplace_back(intervals[0]);
+        for (int i = 1; i < intervals.size(); ++i) {
             if (intervals[i][0] <= result.back()[1]) {
                 result.back()[1] = max(intervals[i][1], result.back()[1]);
             } else {
-                result.push_back(intervals[i]);
+                result.emplace_back(intervals[i]);
             }
         }
         return result;
