68bbc268114abc834a30cec9ce85af942a3ba054
fengwei <virusformail@gmail.com>
2022-03-23 00:14:43 -0700

    Reviewed Problem 67.

--- Problem0067.h
+++ Problem0067.h
@@ -11,23 +11,22 @@ using namespace std;
 
 class Solution {
 public:
-    string addBinary(string a, string b) {
-        reverse(a.begin(), a.end());
-        reverse(b.begin(), b.end());
+    string addBinary(const string &a, const string &b) {
         string res;
+        res.reserve(max(a.size(), b.size()));
         int t = 0;
-        for (int i = 0; i < a.size() || i < b.size(); ++i) {
-            if (i < a.size()) {
-                t += (int) (a[i] - '0');
+        for (int i = 1; i <= a.size() || i <= b.size(); ++i) {
+            if (i <= a.size()) {
+                t += (int) (a[a.size() - i] - '0');
             }
-            if (i < b.size()) {
-                t += (int) (b[i] - '0');
+            if (i <= b.size()) {
+                t += (int) (b[b.size() - i] - '0');
             }
-            res.push_back((char) (t % 2 + '0'));
+            res += (char) (t % 2 + '0');
             t /= 2;
         }
         if (t) {
-            res.push_back((char) (t % 2 + '0'));
+            res += (char) (t % 2 + '0');
         }
         reverse(res.begin(), res.end());
         return res;
