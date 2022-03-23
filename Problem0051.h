9e71336263b6890be0f3254555bb8be66507b080
fengwei <virusformail@gmail.com>
2022-03-23 09:21:47 -0700

    Reviewed Problem 51.

--- Problem0051.h
+++ Problem0051.h
@@ -14,37 +14,37 @@ using namespace std;
 class Solution {
 public:
     vector<vector<string>> solveNQueens(int n) {
-        vector<string> temp(n, string(n, '.'));
-        vector<vector<string>> result;
+        vector<string> cur(n, string(n, '.'));
+        vector<vector<string>> ans;
         bool col_used[n], dg_used[2 * n], udg_used[2 * n];
         memset(col_used, 0, sizeof col_used);
         memset(dg_used, 0, sizeof dg_used);
         memset(udg_used, 0, sizeof udg_used);
-        dfs(0, n, col_used, dg_used, udg_used, temp, result);
-        return result;
+        dfs(0, n, col_used, dg_used, udg_used, cur, ans);
+        return ans;
     }
 
 private:
-    void dfs(const int row_idx,
+    void dfs(const int row,
              const int n,
              bool col_used[],
              bool dg_used[],
              bool udg_used[],
-             vector<string> &temp,
-             vector<vector<string>> &result) {
-        if (row_idx == n) {
-            result.emplace_back(temp);
+             vector<string> &cur,
+             vector<vector<string>> &ans) {
+        if (row == n) {
+            ans.emplace_back(cur);
             return;
         }
-        for (int col_idx = 0; col_idx < n; ++col_idx) {
-            if (col_used[col_idx] || dg_used[row_idx + col_idx] || udg_used[row_idx - col_idx + n]) {
+        for (int col = 0; col < n; ++col) {
+            if (col_used[col] || dg_used[row + col] || udg_used[row - col + n]) {
                 continue;
             }
-            col_used[col_idx] = dg_used[row_idx + col_idx] = udg_used[row_idx - col_idx + n] = true;
-            temp[row_idx][col_idx] = 'Q';
-            dfs(row_idx + 1, n, col_used, dg_used, udg_used, temp, result);
-            temp[row_idx][col_idx] = '.';
-            col_used[col_idx] = dg_used[row_idx + col_idx] = udg_used[row_idx - col_idx + n] = false;
+            col_used[col] = dg_used[row + col] = udg_used[row - col + n] = true;
+            cur[row][col] = 'Q';
+            dfs(row + 1, n, col_used, dg_used, udg_used, cur, ans);
+            cur[row][col] = '.';
+            col_used[col] = dg_used[row + col] = udg_used[row - col + n] = false;
         }
     }
 };
