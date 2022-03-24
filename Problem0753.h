740a711767a2fe1378927a0bc89061d04cbb929e
fengwei <virusformail@gmail.com>
2022-03-23 18:39:38 -0700

    Reviewed Problem 753.

--- Problem0753.h
+++ Problem0753.h
@@ -17,39 +17,33 @@ class Solution {
 public:
     string crackSafe(int n, int k) {
         m = 1;
-        for (int i = 1; i <= n; i++)
+        for (int i = 1; i <= n; ++i) {
             m *= k;
-
+        }
         seen.resize(m, false);
-
         solve(0, k);
-
-        string ans = res + string(n - 1, '0');
-
-        reverse(ans.begin(), ans.end());
-        // 这里不翻转其实也可以通过，但翻转才是刚才遍历得到的欧拉回路。
-
+        auto ans = res + string(n - 1, '0');
+        reverse(ans.begin(), ans.end());  // 这里不翻转其实也可以通过，但翻转才是刚才遍历得到的欧拉回路。
         return ans;
     }
 
-    int m;
+private:
     vector<bool> seen;
     stack<int> st;
     string res;
+    int m;
 
     void solve(int u, int k) {
         for (int c = 0; c < k; c++) {
             int e = u + c;
-            if (seen[e])
+            if (seen[e]) {
                 continue;
-
+            }
             seen[e] = true;
-            st.push(c);
-
-            int v = e * k % m;
+            st.emplace(c);
+            auto v = e * k % m;
             solve(v, k);
-
-            res.push_back(st.top() + '0');
+            res.push_back((char) (st.top() + '0'));
             st.pop();
         }
     }
