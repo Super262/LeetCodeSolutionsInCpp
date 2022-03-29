817687b03e2e250b6913ac7f59b9dd25831cbc4b
fengwei <virusformail@gmail.com>
2022-03-29 16:16:52 -0700

    Reviewed Problem 788.

---
+++ Problem0788.h
@@ -0,0 +1,40 @@
+//
+// Created by Fengwei Zhang on 3/29/22.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0788_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0788_H
+
+class Solution {
+public:
+    int rotatedDigits(int n) {
+        int ans = 0;
+        for (int x = 1; x <= n; ++x) {
+            if (isGood(x)) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+
+private:
+    bool isGood(int x) {
+        if (!x) {
+            return false;
+        }
+        bool is_diff = false;
+        while (x) {
+            auto d = x % 10;
+            if (d == 3 || d == 4 || d == 7) {
+                return false;
+            }
+            if (d == 2 || d == 5 || d == 6 || d == 9) {
+                is_diff = true;
+            }
+            x /= 10;
+        }
+        return is_diff;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0788_H
