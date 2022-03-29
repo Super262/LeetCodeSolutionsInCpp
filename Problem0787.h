096311b859edb75abe3471afd72baf31d27e8460
fengwei <virusformail@gmail.com>
2022-03-29 15:44:12 -0700

    Reviewed Problem 787.

--- Problem0787.h
+++ Problem0787.h
@@ -14,16 +14,20 @@ using namespace std;
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>> &flights, int src, int dst, int k) {
+        const int INF = 0x3f3f3f3f;
         int dist[2][n + 1];
         memset(dist[0], 0x3f, sizeof dist[0]);
         dist[0][src] = 0;
         for (int i = 1; i <= k + 1; ++i) {
             memcpy(dist[i % 2], dist[(i - 1) % 2], sizeof dist[(i - 1) % 2]);
             for (auto &e: flights) {
+                if (dist[(i - 1) % 2][e[0]] == INF) {
+                    continue;
+                }
                 dist[i % 2][e[1]] = min(dist[i % 2][e[1]], dist[(i - 1) % 2][e[0]] + e[2]);
             }
         }
-        if (dist[(k + 1) % 2][dst] == 0x3f3f3f3f) {
+        if (dist[(k + 1) % 2][dst] == INF) {
             return -1;
         }
         return dist[(k + 1) % 2][dst];
