17c506b655315623b9e3e731edd8b428269fdb4f
fengwei <virusformail@gmail.com>
2022-03-31 14:59:08 -0700

    Finished Problem 812.

---
+++ Problem0812.h
@@ -0,0 +1,35 @@
+//
+// Created by Fengwei Zhang on 3/31/22.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0812_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0812_H
+
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+    // 使用向量的叉积来计算三角形面积
+public:
+    double largestTriangleArea(const vector<vector<int>> &points) {
+        double ans = 0;
+        for (const auto &a: points) {
+            for (const auto &b: points) {
+                for (const auto &c: points) {
+                    ans = max(ans,
+                              ((double) abs(crossProduct(b[0] - a[0], b[1] - a[1], c[0] - a[0], c[1] - a[1]))) / 2);
+                }
+            }
+        }
+        return ans;
+    }
+
+private:
+    int crossProduct(int x1, int y1, int x2, int y2) {
+        return x1 * y2 - x2 * y1;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0812_H
