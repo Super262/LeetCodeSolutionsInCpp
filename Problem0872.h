a2704146f2a2937a5ff29704cb08dbf3ae14d875
fengwei <virusformail@gmail.com>
2022-04-08 01:06:35 -0700

    Finished Problem 872.

---
+++ Problem0872.h
@@ -0,0 +1,51 @@
+//
+// Created by Fengwei Zhang on 4/7/22.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0872_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0872_H
+
+#include <vector>
+
+using namespace std;
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool leafSimilar(TreeNode *root1, TreeNode *root2) {
+        vector<int> a;
+        vector<int> b;
+        getLeaves(root1, a);
+        getLeaves(root2, b);
+        return a == b;
+    }
+
+private:
+    void getLeaves(TreeNode *root, vector<int> &ans) {
+        if (!root) {
+            return;
+        }
+        if (!root->left && !root->right) {
+            ans.emplace_back(root->val);
+            return;
+        }
+        getLeaves(root->left, ans);
+        getLeaves(root->right, ans);
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0872_H
