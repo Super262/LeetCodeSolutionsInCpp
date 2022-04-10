0c40730222d68905eea2c6345a699081649d03c8
fengwei <virusformail@gmail.com>
2022-04-09 23:21:09 -0700

    Finished Problem 890.

---
+++ Problem0890.h
@@ -0,0 +1,48 @@
+//
+// Created by Fengwei Zhang on 4/9/22.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0890_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0890_H
+
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<string> findAndReplacePattern(const vector<string> &words, const string &pattern) {
+        vector<string> ans;
+        for (const auto &s: words) {
+            if (!checkPattern(s, pattern)) {
+                continue;
+            }
+            ans.emplace_back(s);
+        }
+        return ans;
+    }
+
+private:
+    static bool checkPattern(const string &s, const string &p) {
+        if (s.size() != p.size()) {
+            return false;
+        }
+        unordered_map<char, char> s2p;
+        unordered_map<char, char> p2s;
+        for (int i = 0; i < s.size(); ++i) {
+            if (!s2p.count(s[i]) && !p2s.count(p[i])) {
+                s2p[s[i]] = p[i];
+                p2s[p[i]] = s[i];
+                continue;
+            }
+            if (!s2p.count(s[i]) || !p2s.count(p[i]) || s2p[s[i]] != p[i] || p2s[p[i]] != s[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0890_H
