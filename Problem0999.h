cf64c394ee73b5e2dc5dba26de904abcf9141224
fengwei <virusformail@gmail.com>
2022-04-20 14:52:11 -0700

    Finished Problem 999.

---
+++ Problem0999.h
@@ -0,0 +1,50 @@
+//
+// Created by Fengwei Zhang on 4/20/22.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0999_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0999_H
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int numRookCaptures(const vector<vector<char>> &board) {
+        const auto m = (int) board.size();
+        const auto n = (int) board[0].size();
+        int sx = -1;
+        int sy = -1;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (board[i][j] == 'R') {
+                    sx = i;
+                    sy = j;
+                    break;
+                }
+            }
+        }
+        const int dx[4] = {0, 1, 0, -1};
+        const int dy[4] = {1, 0, -1, 0};
+        int ans = 0;
+        for (int i = 0; i < 4; ++i) {
+            auto x = sx;
+            auto y = sy;
+            while (true) {
+                x += dx[i];
+                y += dy[i];
+                if (x < 0 || y < 0 || x >= m || y >= n || board[x][y] == 'B') {
+                    break;
+                }
+                if (board[x][y] == 'p') {
+                    ++ans;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0999_H
