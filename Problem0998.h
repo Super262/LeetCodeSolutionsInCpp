993fe0f8025d9a34e1d54fdb94456d7124b8953c
fengwei <virusformail@gmail.com>
2022-04-20 14:25:29 -0700

    Finished Problem 998.

---
+++ Problem0998.h
@@ -0,0 +1,40 @@
+//
+// Created by Fengwei Zhang on 4/20/22.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0998_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0998_H
+
+/**
+ * Definition for a binary tree node.
+ */
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+    // 若val小于根，在右子树插入新节点；若val大于根，val成为新的根，原树为新树的左子树
+public:
+    TreeNode *insertIntoMaxTree(TreeNode *root, int val) {
+        if (!root) {
+            return new TreeNode(val);
+        }
+        if (root->val <= val) {
+            auto u = new TreeNode(val);
+            u->left = root;
+            return u;
+        }
+        root->right = insertIntoMaxTree(root->right, val);
+        return root;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0998_H
