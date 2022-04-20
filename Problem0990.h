f7c901bfcc6e8cd3c7729f17312219e8745cab8e
fengwei <virusformail@gmail.com>
2022-04-20 00:30:47 -0700

    Finished Problem 990.

---
+++ Problem0990.h
@@ -0,0 +1,68 @@
+//
+// Created by Fengwei Zhang on 4/19/22.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM0990_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM0990_H
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+    // 并查集：将等式两端的集合合并，判断不等式两端的集合是否在同个集合
+public:
+    bool equationsPossible(const vector<string> &equations) {
+        const int m = 26;
+        int parent[m];
+        int set_size[m];
+        for (int i = 0; i < m; ++i) {
+            parent[i] = i;
+            set_size[i] = 1;
+        }
+        for (const auto &e: equations) {  // 合并等式两端
+            if (e[1] == '!') {
+                continue;
+            }
+            auto a = findRoot(e[0] - 'a', parent);
+            auto b = findRoot(e[3] - 'a', parent);
+            mergeSets(a, b, parent, set_size);
+        }
+        for (const auto &e: equations) {  // 判断不等式两端
+            if (e[1] == '=') {
+                continue;
+            }
+            auto a = findRoot(e[0] - 'a', parent);
+            auto b = findRoot(e[3] - 'a', parent);
+            if (a == b) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+private:
+    int findRoot(int x, int parent[]) {
+        if (parent[x] != x) {
+            parent[x] = findRoot(parent[x], parent);
+        }
+        return parent[x];
+    }
+
+    static int mergeSets(int a, int b, int parent[], int set_size[]) {
+        if (a == b) {
+            return -1;
+        }
+        if (set_size[a] > set_size[b]) {
+            parent[b] = a;
+            set_size[a] += set_size[b];
+            return a;
+        }
+        parent[a] = b;
+        set_size[b] += set_size[a];
+        return b;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM0990_H
