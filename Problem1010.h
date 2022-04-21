56a87d23f1d20a5ea2ec923684b07d802cb32e7f
fengwei <virusformail@gmail.com>
2022-04-21 13:26:35 -0700

    Finished Problem 1010.

---
+++ Problem1010.h
@@ -0,0 +1,29 @@
+//
+// Created by Fengwei Zhang on 4/21/22.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM1010_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM1010_H
+
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+class Solution {
+    // 双指针 + 哈希表，统计同余数字的个数
+public:
+    int numPairsDivisibleBy60(const vector<int> &time) {
+        int counter[60];
+        memset(counter, 0, sizeof counter);
+        int ans = 0;
+        for (const int &t: time) {
+            auto x = ((-t) % 60 + 60) % 60;
+            ans += counter[x];
+            counter[((t % 60) + 60) % 60]++;
+        }
+        return ans;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM1010_H
