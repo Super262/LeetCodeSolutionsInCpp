5acbee4182c3fbea1172e607e8f47c494a8820d6
fengwei <virusformail@gmail.com>
2022-04-21 13:42:40 -0700

    Finished Problem 1011.

---
+++ Problem1011.h
@@ -0,0 +1,48 @@
+//
+// Created by Fengwei Zhang on 4/21/22.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM1011_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM1011_H
+
+#include <vector>
+#include <numeric>
+
+using namespace std;
+
+class Solution {
+    // 二分查找
+public:
+    int shipWithinDays(const vector<int> &weights, int days) {
+        int l = 0;
+        auto r = accumulate(weights.begin(), weights.end(), 0);
+        while (l < r) {
+            auto mid = l + (r - l) / 2;
+            if (canShipInDays(weights, days, mid)) {
+                r = mid;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return r;
+    }
+
+private:
+    bool canShipInDays(const vector<int> &weights, int days, int cap) {
+        int ans = 1;
+        int cur = 0;
+        for (const auto &x: weights) {
+            if (x > cap) {
+                return false;
+            }
+            if (cur + x > cap) {
+                cur = 0;
+                ++ans;
+            }
+            cur += x;
+        }
+        return ans <= days;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM1011_H
