86f624c54772e575f4701628b576a06c27d85ca0
fengwei <virusformail@gmail.com>
2022-04-22 15:15:48 -0700

    Finished Problem 1023.

---
+++ Problem1023.h
@@ -0,0 +1,40 @@
+//
+// Created by Fengwei Zhang on 4/22/22.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM1023_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM1023_H
+
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Solution {
+    // 子序列模版题，跳过q中不和p中大写字母匹配的小写字母
+public:
+    vector<bool> camelMatch(const vector<string> &queries, const string &pattern) {
+        vector<bool> ans(queries.size());
+        for (int k = 0; k < (int) queries.size(); ++k) {
+            int i = 0;
+            int j = 0;
+            bool is_matched = true;
+            while (i < (int) queries[k].size()) {  // 遍历查询
+                if (j < (int) pattern.size() && queries[k][i] == pattern[j]) {  // 相等，直接跳过
+                    ++i;
+                    ++j;
+                    continue;
+                }
+                if (isupper(queries[k][i])) {  // 发现不相等的大写字母，非法
+                    is_matched = false;
+                    break;
+                }
+                ++i;
+            }
+            ans[k] = is_matched && j == (int) pattern.size();
+        }
+        return ans;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM1023_H
