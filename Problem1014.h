93d0d53bd397d2c0b2f03f4d4834b05366008667
fengwei <virusformail@gmail.com>
2022-04-21 21:25:51 -0700

    Finished Problem 1014.

---
+++ Problem1014.h
@@ -0,0 +1,26 @@
+//
+// Created by Fengwei Zhang on 4/21/22.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM1014_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM1014_H
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+    // 双指针问题：我们希望找到i、j，满足i<j，且(values[j]-j)+(values[i]+i)最大
+public:
+    int maxScoreSightseeingPair(const vector<int> &values) {
+        int ans = INT_MIN;
+        int max_prev = INT_MIN;
+        for (int i = 0; i < (int) values.size(); ++i) {
+            ans = max(ans, values[i] - i + max_prev);
+            max_prev = max(max_prev, values[i] + i);
+        }
+        return ans;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM1014_H
