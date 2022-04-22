db4abc2b7c477929e295eacf962fe87633d34345
fengwei <virusformail@gmail.com>
2022-04-22 16:25:16 -0700

    Finished Problem 1025.

---
+++ Problem1025.h
@@ -0,0 +1,19 @@
+//
+// Created by Fengwei Zhang on 4/22/22.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM1025_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM1025_H
+
+class Solution {
+    // 结论：奇数必败，偶数必胜（必胜态：后继状态中至少一个是必败态；必败态：后继状态无必败态）
+    // n=1，结论成立；设k<n时，结论成立，要证k=n时，结论也成立
+    // (1) k为奇数，若x被选中（x可整除k），则x也是奇数，那么(k-x)是偶数，k必败
+    // (2) k为偶数，显然k-1必败，因此k必胜
+public:
+    bool divisorGame(int n) {
+        return n % 2 == 0;
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM1025_H
