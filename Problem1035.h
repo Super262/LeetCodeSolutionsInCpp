fa7a981e702ec8ae1c2afd46dfce8217853f997a
fengwei <virusformail@gmail.com>
2022-04-23 11:06:44 -0700

    Finished Problem 1035.

---
+++ Problem1035.h
@@ -0,0 +1,33 @@
+//
+// Created by Fengwei Zhang on 4/23/22.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM1035_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM1035_H
+
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+class Solution {
+    // 可以证明，此问题等价于最长公共子序列问题
+public:
+    int maxUncrossedLines(const vector<int> &nums1, const vector<int> &nums2) {
+        const auto m = (int) nums1.size();
+        const auto n = (int) nums2.size();
+        int f[m + 1][n + 1];
+        memset(f, 0, sizeof f);
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                f[i][j] = max(f[i][j - 1], f[i - 1][j]);
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    f[i][j] = max(f[i][j], f[i - 1][j - 1] + 1);
+                }
+            }
+        }
+        return f[n][m];
+    }
+};
+
+#endif //LEETCODESOLUTIONSINCPP_PROBLEM1035_H
