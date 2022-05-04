3d1d769238ad0c6956eeeb5adda371971f8c29b4
fengwei <virusformail@gmail.com>
2022-05-03 18:57:37 -0700

    Reviewed Problem 5.

--- Problem0005.h
+++ Problem0005.h
@@ -16,23 +16,21 @@ class Solution {
 public:
     string longestPalindrome(const string &s) {
         const auto n = (int) s.size();
-        bool dp[n][n];  // s[i, j]是回文串，dp[i][j] == true（i，j从0开始）
-        memset(dp, 0, sizeof dp);
+        bool f[n][n];  // s[i, j]是回文串，f[i][j] == true（i，j从0开始）
+        memset(f, 0, sizeof f);
         int ans_idx = -1;
         int ans_len = 0;
         for (int length = 1; length <= n; ++length) {
             for (int l = 0; l + length - 1 < n; ++l) {
                 auto r = l + length - 1;
                 if (length == 1) {
-                    dp[l][r] = true;
-                } else if (length == 2) {
-                    dp[l][r] = s[l] == s[r];
-                } else {
-                    dp[l][r] = s[l] == s[r] && dp[l + 1][r - 1];
+                    f[l][r] = true;
+                } else if (s[l] == s[r]) {
+                    f[l][r] = length == 2 || f[l + 1][r - 1];
                 }
-                if (dp[l][r] && r - l + 1 > ans_len) {
-                    ans_idx = l;
+                if (f[l][r] && r - l + 1 > ans_len) {
                     ans_len = r - l + 1;
+                    ans_idx = l;
                 }
             }
         }
