570837a09d9f28e6da4f4cdc60fe85951b06c47d
fengwei <virusformail@gmail.com>
2022-05-09 18:04:58 -0700

    Reviewed Problem 19.

--- Problem0019.h
+++ Problem0019.h
@@ -11,25 +11,26 @@ class Solution {
     // 经典算法，直接背诵：找到第n个节点的前驱，删除第n个节点
 public:
     ListNode *removeNthFromEnd(ListNode *head, int n) {
-        if (!head) {
-            return head;
-        }
-        auto l = head; // the slow pointer
-        auto r = head; // the fast pointer
         ++n;  // Increase n to make "l" can get the prev of nth node
-        while (n > 0 && r) {  // The fast pointer moves
-            r = r->next;
+        auto l = head;  // the slow pointer
+        auto r = head;  // the fast pointer
+        while (n > 0 && r) {
             --n;
+            r = r->next;
         }
         if (n > 0) {  // n == sz, remove the head
-            head = head->next;
+            auto t = head->next;
+            delete head;
+            head = t;
             return head;
         }
         while (r) {  // Two pointers move together
-            r = r->next;
             l = l->next;
+            r = r->next;
         }
-        l->next = l->next->next;
+        auto t = l->next->next;
+        delete l->next;
+        l->next = t;
         return head;
     }
 };
