6db9e352bfb8b42e8d135591ebaa4fe6c305aed0
fengwei <virusformail@gmail.com>
2022-05-10 01:43:51 -0700

    Reviewed Problem 22.

--- Problem0022.h
+++ Problem0022.h
@@ -14,28 +14,28 @@ class Solution {
     // 合法的括号序列：left_cnt <= n, right_cnt <= n && left_cnt >= right_cnt
     // 直接DFS
 public:
-    vector<string> generateParenthesis(const int n) {
-        vector<string> result;
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
         string temp;
         temp.reserve(2 * n);
-        dfs(n, 0, 0, temp, result);
-        return result;
+        dfs(n, 0, 0, temp, ans);
+        return ans;
     }
 
 private:
-    void dfs(const int n, const int left_cnt, const int right_cnt, string &temp, vector<string> &result) {
+    void dfs(int n, int left_cnt, int right_cnt, string &temp, vector<string> &ans) {
         if (left_cnt == n && right_cnt == n) {
-            result.emplace_back(temp);
+            ans.emplace_back(temp);
             return;
         }
         if (left_cnt < n) {
-            temp.push_back('(');
-            dfs(n, left_cnt + 1, right_cnt, temp, result);
+            temp += '(';
+            dfs(n, left_cnt + 1, right_cnt, temp, ans);
             temp.pop_back();
         }
-        if (right_cnt < n && right_cnt < left_cnt) {
-            temp.push_back(')');
-            dfs(n, left_cnt, right_cnt + 1, temp, result);
+        if (left_cnt <= n && right_cnt < left_cnt) {
+            temp += ')';
+            dfs(n, left_cnt, right_cnt + 1, temp, ans);
             temp.pop_back();
         }
     }
