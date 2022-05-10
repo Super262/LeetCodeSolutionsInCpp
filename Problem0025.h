13edb2694c2a658591fb174ee3d2b388672d6134
fengwei <virusformail@gmail.com>
2022-05-10 02:52:17 -0700

    Reviewed Problem 25.

--- Problem0025.h
+++ Problem0025.h
@@ -5,34 +5,26 @@
 #ifndef LEETCODESOLUTIONSINCPP_PROBLEM0025_H
 #define LEETCODESOLUTIONSINCPP_PROBLEM0025_H
 
-struct ListNode {
-    int val;
-    ListNode *next;
-
-    ListNode() : val(0), next(nullptr) {}
-
-    ListNode(int x) : val(x), next(nullptr) {}
-
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
+#include "listnode.h"
 
 class Solution {
-    // 经典算法，直接背诵
+    // 经典算法，直接背诵：反序k个节点，需要反序(k-1)条链
+    // for循环的条件语句必须有2个分号
 public:
     ListNode *reverseKGroup(ListNode *head, int k) {
         auto dummy = new ListNode(-1);
         dummy->next = head;
-        for (auto p = dummy;;) { // for循环的条件语句必须有2个分号
+        for (auto p = dummy;;) {  // 反序[p+1:q]部分
             auto q = p;
             for (int i = 0; i < k && q; ++i) {
                 q = q->next;
             }
-            if (!q) {
+            if (!q) {  // 当前段长不足k，终止操作
                 break;
             }
             auto a = p->next;
             auto b = a->next;
-            for (int i = 0; i < k - 1; ++i) {  // 反转(k-1)条边
+            for (int i = 0; i < k - 1; ++i) {  // 反转(k-1)条链
                 auto t = b->next;
                 b->next = a;
                 a = b;
@@ -43,7 +35,9 @@ public:
             c->next = b;
             p = c;
         }
-        return dummy->next;
+        auto ans = dummy->next;
+        delete dummy;
+        return ans;
     }
 };
 
