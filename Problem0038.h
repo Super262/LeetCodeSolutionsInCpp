f4a5c30b63cf735feb44afab79cf4761890c24f4
fengwei <virusformail@gmail.com>
2022-05-10 17:37:43 -0700

    Reviewed Problem 38.

--- Problem0038.h
+++ Problem0038.h
@@ -10,11 +10,12 @@
 using namespace std;
 
 class Solution {
+    // 依次迭代，从1到n-1：统计当前s中各数字出现的次数，生成新的s
 public:
     string countAndSay(const int n) {
         string s = "1";
         for (int i = 1; i < n; ++i) {
-            string t;
+            string t;  // 生成新的s，描述当前的s
             for (int l = 0, r = l + 1; l < (int) s.size(); ++l) {
                 while (r < (int) s.size() && s[l] == s[r]) {
                     ++r;
