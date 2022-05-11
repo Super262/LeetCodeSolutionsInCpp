a0f00d0f37abca70e1b8f45b914f7ac1f09079dd
fengwei <virusformail@gmail.com>
2022-05-11 03:12:56 -0700

    Reviewed Problem 61.

--- Problem0061.h
+++ Problem0061.h
@@ -6,34 +6,24 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0061_H
 
 #include <algorithm>
+#include "listnode.h"
 
 using namespace std;
 
-struct ListNode {
-    int val;
-    ListNode *next;
-
-    ListNode() : val(0), next(nullptr) {}
-
-    ListNode(int x) : val(x), next(nullptr) {}
-
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-
 class Solution {
+    // 注意细节：k%n
 public:
     ListNode *rotateRight(ListNode *head, int k) {
         if (!head) {
             return head;
         }
 
-        int n = 0;  // 链表长度
-        ListNode *tail;
+        int n = 0;  // 求链表长度
+        ListNode *tail;  // 记录尾节点
         for (auto p = head; p; p = p->next) {
             tail = p;
             ++n;
         }
-
 
         k %= n;  // 关键一步，不要忘记！
         auto *prev = head;
