da52a2160dc279b4cc06fef8fe9be419508dd8e4
fengwei <virusformail@gmail.com>
2022-05-11 16:58:13 -0700

    Reviewed Problem 96.

--- Problem0096.h
+++ Problem0096.h
@@ -14,18 +14,18 @@ using namespace std;
 
 class Solution {
     // 1. 数学方法，卡特兰数：C(n + 1) = 2 * (2 * n + 1) / (n + 2) * C(n)。
-    // 2. 动态规划：dp[i] = dp[j - 1] * dp[i - j]（长度为i的BST种类数是左右子树的种类数的乘积）
+    // 2. 动态规划：f[i] = f[j - 1] * f[i - j]（长度为i的BST种类数是左右子树的种类数的乘积）
 public:
     int numTrees(int n) {
-        int dp[n + 1];
-        memset(dp, 0, sizeof dp);
-        dp[0] = 1;
+        int f[n + 1];
+        memset(f, 0, sizeof f);
+        f[0] = 1;
         for (int len = 1; len <= n; ++len) {
             for (int root = 1; root <= len; ++root) {
-                dp[len] += dp[root - 1] * dp[len - root];
+                f[len] += f[root - 1] * f[len - root];
             }
         }
-        return dp[n];
+        return f[n];
     }
 };
 
