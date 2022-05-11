3eea4ba68b107a1eb30a62e4ed8d91c323880520
fengwei <virusformail@gmail.com>
2022-05-11 16:29:08 -0700

    Reviewed Problem 93.

--- Problem0093.h
+++ Problem0093.h
@@ -13,6 +13,7 @@
 using namespace std;
 
 class Solution {
+    // 直接DFS：尝试当前段的长度为1~3，避免包含前导零的数字、大于255的数字
 public:
     vector<string> restoreIpAddresses(const string &s) {
         vector<string> result;
@@ -23,20 +24,16 @@ public:
 
 private:
     void dfs(const string &s, const int ch_idx, const int num_idx, string &temp, vector<string> &res) {
-        if (ch_idx == (int) s.size()) {
-            if (num_idx == 4) {
+        if (num_idx == 4) {
+            if (ch_idx == (int) s.size()) {
                 temp.pop_back();  // 弹出分隔符
                 res.emplace_back(string(temp));
             }
             return;
         }
-        if (num_idx == 4) {
-            return;
-        }
 
         auto input_size = (int) temp.size();
-
-        for (auto i = ch_idx, t = 0; i < (int) s.size(); ++i) {
+        for (int i = ch_idx, t = 0; i < (int) s.size(); ++i) {
             t = t * 10 + (s[i] - '0');
             if (i > ch_idx && s[ch_idx] == '0') {  // 每位地址可以是0，但不能是0开头的多位数
                 break;
