4adb984c2d53b3b6308a09677bc9c07396beb34c
fengwei <virusformail@gmail.com>
2022-05-12 15:40:19 -0700

    Reviewed Problem 113.

--- Problem0113.h
+++ Problem0113.h
@@ -8,23 +8,12 @@
 #include <vector>
 #include <queue>
 #include <unordered_map>
+#include "treenode.h"
 
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
 class Solution {
-    // BFS解法，必须掌握
+    // BFS解法，必须掌握：用Map保存搜索路径
 public:
     vector<vector<int>> pathSum(TreeNode *root, const int target) {
         if (!root) {
