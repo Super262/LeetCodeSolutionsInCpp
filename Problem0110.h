9d80fe37d6b81df766a4db62b8a4ebf882a15d16
fengwei <virusformail@gmail.com>
2022-05-12 03:25:08 -0700

    Reviewed Problem 110.

--- Problem0110.h
+++ Problem0110.h
@@ -6,20 +6,9 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0110_H
 
 #include <algorithm>
+#include "treenode.h"
 
 using namespace std;
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
 
 class Solution {
     // 经典算法，直接背诵
