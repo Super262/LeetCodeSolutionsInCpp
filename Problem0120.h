8ad9225c4a0ed31a647aebc5272bee911e009991
fengwei <virusformail@gmail.com>
2022-05-12 16:52:52 -0700

    Reviewed Problem 120.

--- Problem0120.h
+++ Problem0120.h
@@ -11,23 +11,25 @@
 using namespace std;
 
 class Solution {
+    // f[i][i]：从(0,0)到(i,j)的最小路径和
 public:
     int minimumTotal(const vector<vector<int>> &triangle) {
-        int dp[2][triangle.size()];
-        memset(dp, 0x3f, sizeof dp);
-        dp[0][0] = triangle[0][0];
-        for (int i = 1; i < (int) triangle.size(); ++i) {
-            dp[i % 2][0] = triangle[i][0] + dp[(i - 1) % 2][0];
+        const auto n = triangle.size();
+        int f[2][n];
+        memset(f, 0x3f, sizeof f);
+        f[0][0] = triangle[0][0];
+        for (int i = 1; i < n; ++i) {
+            f[i % 2][0] = triangle[i][0] + f[(i - 1) % 2][0];
             for (int j = 1; j < i; ++j) {
-                dp[i % 2][j] = triangle[i][j] + min(dp[(i - 1) % 2][j - 1], dp[(i - 1) % 2][j]);
+                f[i % 2][j] = triangle[i][j] + min(f[(i - 1) % 2][j - 1], f[(i - 1) % 2][j]);
             }
-            dp[i % 2][i] = triangle[i][i] + dp[(i - 1) % 2][i - 1];
+            f[i % 2][i] = triangle[i][i] + f[(i - 1) % 2][i - 1];
         }
-        int result = 0x3f3f3f3f;
-        for (int i = 0; i < (int) triangle.size(); ++i) {
-            result = min(result, dp[(triangle.size() - 1) % 2][i]);
+        int ans = 0x3f3f3f3f;
+        for (int i = 0; i < n; ++i) {
+            ans = min(ans, f[(n - 1) % 2][i]);
         }
-        return result;
+        return ans;
     }
 };
 
