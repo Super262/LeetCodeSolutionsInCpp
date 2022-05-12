b83d3a754450cb1fc040cea468960e9f69743f37
fengwei <virusformail@gmail.com>
2022-05-11 17:09:32 -0700

    Reviewed Problem 97.

--- Problem0097.h
+++ Problem0097.h
@@ -11,26 +11,28 @@
 using namespace std;
 
 class Solution {
-    // 经典算法，直接背诵
+    // f[i][j]：s1[0:i-1]和s2[0:j-1]组成s3[0:i+j-1]，最后的字符来自s1或s2
 public:
     bool isInterleave(const string &s1, const string &s2, const string &s3) {
         if (s3.size() != s1.size() + s2.size()) {
             return false;
         }
-        bool dp[s1.size() + 1][s2.size() + 1];
-        memset(dp, 0, sizeof dp);
-        dp[0][0] = true;
-        for (int i = 0; i <= (int) s1.size(); ++i) {
-            for (int j = 0; j <= (int) s2.size(); ++j) {
+        const auto m = (int) s1.size();
+        const auto n = (int) s2.size();
+        bool f[m + 1][n + 1];
+        memset(f, 0, sizeof f);
+        f[0][0] = true;
+        for (int i = 0; i <= m; ++i) {
+            for (int j = 0; j <= n; ++j) {
                 if (i && s1[i - 1] == s3[i + j - 1]) {
-                    dp[i][j] = dp[i][j] || dp[i - 1][j];
+                    f[i][j] = f[i][j] || f[i - 1][j];
                 }
                 if (j && s2[j - 1] == s3[i + j - 1]) {
-                    dp[i][j] = dp[i][j] || dp[i][j - 1];
+                    f[i][j] = f[i][j] || f[i][j - 1];
                 }
             }
         }
-        return dp[s1.size()][s2.size()];
+        return f[m][n];
     }
 };
 
