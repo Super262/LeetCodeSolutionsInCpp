b299203449ad627f2dd7d722cba9e4af025d3cbf
fengwei <virusformail@gmail.com>
2022-05-11 17:45:40 -0700

    Reviewed Problem 100.

--- Problem0100.h
+++ Problem0100.h
@@ -5,17 +5,7 @@
 #ifndef LEETCODESOLUTIONSINCPP_PROBLEM0100_H
 #define LEETCODESOLUTIONSINCPP_PROBLEM0100_H
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
+#include "treenode.h"
 
 class Solution {
 public:
