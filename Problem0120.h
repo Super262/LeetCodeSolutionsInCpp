ba2d5b3c0c17a1978705b398a664396d1787c9fc
fengwei <virusformail@gmail.com>
2022-05-12 16:54:59 -0700

    Reviewed Problem 120.

--- Problem0120.h
+++ Problem0120.h
@@ -19,11 +19,11 @@ public:
         memset(f, 0x3f, sizeof f);
         f[0][0] = triangle[0][0];
         for (int i = 1; i < n; ++i) {
-            f[i % 2][0] = triangle[i][0] + f[(i - 1) % 2][0];
+            f[i % 2][0] = triangle[i][0] + f[(i - 1) % 2][0];  // (i,0)只能从(i-1,0)转移过来
             for (int j = 1; j < i; ++j) {
                 f[i % 2][j] = triangle[i][j] + min(f[(i - 1) % 2][j - 1], f[(i - 1) % 2][j]);
             }
-            f[i % 2][i] = triangle[i][i] + f[(i - 1) % 2][i - 1];
+            f[i % 2][i] = triangle[i][i] + f[(i - 1) % 2][i - 1];  // (i,i)只能从(i-1,i-1)转移过来
         }
         int ans = 0x3f3f3f3f;
         for (int i = 0; i < n; ++i) {
