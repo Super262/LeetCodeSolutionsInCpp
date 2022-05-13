3ff82d2e82c3ab47a70a577c5f6900e8e9e08f6c
fengwei <virusformail@gmail.com>
2022-05-13 04:09:34 -0700

    Reviewed Problem 151.

--- Problem0151.h
+++ Problem0151.h
@@ -10,27 +10,31 @@
 using namespace std;
 
 class Solution {
+    // 去掉前导空格、后余空格，翻转每个单词
 public:
     string reverseWords(string s) {
-        int k = 0;
+        int st = 0;
         for (int i = 0; i < (int) s.size(); ++i) {
             if (s[i] == ' ') {
                 continue;
             }
             auto j = i;
-            auto t = k;
+            auto ed = st;
             while (j < (int) s.size() && s[j] != ' ') {
-                s[t++] = s[j++];
+                s[ed] = s[j];
+                ++ed;
+                ++j;
             }
-            reverse(s.begin() + k, s.begin() + t);
-            s[t++] = ' ';
-            k = t;
+            reverse(s.begin() + st, s.begin() + ed); // 翻转单词（s[st:ed-1]）
+            s[ed] = ' ';  // 添加分隔符
+            st = ed;
+            ++st;
             i = j;
         }
-        if (k) {
-            --k;
+        if (st) {
+            --st;
         }
-        s.erase(s.begin() + k, s.end());
+        s.erase(s.begin() + st, s.end());  // 去除后余的空格
         reverse(s.begin(), s.end());
         return s;
     }
