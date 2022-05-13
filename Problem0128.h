9bcdc8caa05fb368892ee27c6043f98723ebec8f
fengwei <virusformail@gmail.com>
2022-05-12 18:05:13 -0700

    Reviewed Problem 128.

--- Problem0128.h
+++ Problem0128.h
@@ -11,13 +11,14 @@
 using namespace std;
 
 class Solution {
-    // 巧妙的做法，要牢记：利用哈希表保存前缀，快速查找a的前驱a-1是否存在
+    // 巧妙的做法，要牢记：利用哈希表保存前缀，快速查找a的后继a+1是否存在
+    // 注意细节：完成搜索后，删除结果序列，避免重复查找
 public:
     int longestConsecutive(const vector<int> &nums) {
         unordered_set<int> nums_set(nums.begin(), nums.end());
         int res = 0;
         for (const auto &l: nums) {  // 搜索以l为起点的最长序列
-            if (!nums_set.count(l) || nums_set.count(l - 1)) {  // 存在前驱结点，跳过
+            if (!nums_set.count(l) || nums_set.count(l - 1)) {  // 存在前驱结点，跳过（l-1存在，则l不可能为起点）
                 continue;
             }
             auto r = l;
