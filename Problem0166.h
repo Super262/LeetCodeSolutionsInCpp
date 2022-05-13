4bbecf94ef0e5244ba036e6c10c17ab414f89ff8
fengwei <virusformail@gmail.com>
2022-05-13 14:58:51 -0700

    Reviewed Problem 166.

--- Problem0166.h
+++ Problem0166.h
@@ -11,36 +11,35 @@
 using namespace std;
 
 class Solution {
-    // 经典算法，直接背诵
-    // 模拟整数除法；哈希表判重
+    // 模拟整数除法，用哈希表判重，以确定循环节的起点和终点
 public:
     string fractionToDecimal(int numerator, int denominator) {
         long long x = numerator;  // 防止溢出（-2^31 / -1 会发生溢出）
         long long y = denominator;
-        if (x % y == 0) {
+        if (x % y == 0) {  // 无余数，直接返回整数
             return to_string(x / y);
         }
         unordered_map<int, int> remainder_idx;
-        string result;
-        if ((x < 0) ^ (y < 0)) {  // 经典操作，要牢记
-            result.push_back('-');
+        string ans;
+        if ((x < 0) ^ (y < 0)) {  // x、y的正负性不同（经典操作，要牢记）
+            ans.push_back('-');
         }
         x = abs(x);
         y = abs(y);
-        result += to_string(x / y);
-        result.push_back('.');
+        ans += to_string(x / y);  // 保存整数和小数点
+        ans.push_back('.');
         x %= y;
-        while (x) {
-            remainder_idx[(int) x] = (int) result.size();
+        while (x) {  // 模拟算数乘法
+            remainder_idx[(int) x] = (int) ans.size();
             x *= 10;
-            result.push_back((char) ('0' + x / y));
+            ans.push_back((char) ('0' + x / y));
             x %= y;
             if (remainder_idx.count((int) x)) {
-                result = result.substr(0, remainder_idx[(int) x]) + '(' + result.substr(remainder_idx[(int) x]) + ')';
+                ans = ans.substr(0, remainder_idx[(int) x]) + '(' + ans.substr(remainder_idx[(int) x]) + ')';
                 break;
             }
         }
-        return result;
+        return ans;
     }
 };
 
