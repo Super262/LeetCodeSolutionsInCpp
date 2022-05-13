a76e72f7e4d9b379b86c16ef0a1cea592a8f31c0
fengwei <virusformail@gmail.com>
2022-05-12 22:47:22 -0700

    Reviewed Problem 134.

--- Problem0134.h
+++ Problem0134.h
@@ -10,11 +10,11 @@
 using namespace std;
 
 class Solution {
-    // 经典算法，直接背诵：若ed不可达，[st, ed]所有点都不是合法起点
+    // 贪心算法，直接背诵：若ed不可达，[st, ed]所有点都不是合法起点
     // https://www.acwing.com/video/1499/
 public:
     int canCompleteCircuit(const vector<int> &gas, const vector<int> &cost) {
-        const int n = (int) gas.size();
+        const auto n = (int) gas.size();
         int st = 0;
         while (st < n) {  // 枚举起点
             int remainder = 0;  // 剩余油量
