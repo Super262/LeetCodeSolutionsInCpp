b453adeef35f40c1625c2bd29463db4b664259b0
fengwei <virusformail@gmail.com>
2022-05-13 02:22:41 -0700

    Reviewed Problem 144.

--- Problem0144.h
+++ Problem0144.h
@@ -7,20 +7,9 @@
 
 #include <stack>
 #include <vector>
+#include "treenode.h"
 
 using namespace std;
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
 
 class Solution {
     // Morris遍历，直接背诵
