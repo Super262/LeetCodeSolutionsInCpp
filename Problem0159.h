f94c50a2f0de254010dad3f9ace8b654f7094401
fengwei <virusformail@gmail.com>
2022-05-13 13:49:20 -0700

    Reviewed Problem 159.

--- Problem0159.h
+++ Problem0159.h
@@ -13,31 +13,29 @@ using namespace std;
 class Solution {
 public:
     int lengthOfLongestSubstringTwoDistinct(const string &s) {
-        const int n = (int) s.size();
-        int existed[256];  // 手写哈希表记录每类字符的频率，加快速度
-        int dictSize = 0;  // 当前字符种类别数
-        memset(existed, 0, sizeof existed);
-
+        const auto n = (int) s.size();
+        int freq[256];  // 数组替代哈希表记录每类字符的频率，加快速度
+        int counter = 0;  // 当前字符种类别数
+        memset(freq, 0, sizeof freq);
         int r = 0;
         int l = 0;
-        int result = 0;
+        int ans = 0;
         while (r < n) {  // 滑动窗口
-            while (l < r && existed[s[r]] == 0 && dictSize >= 2) {
-                --existed[s[l]];
-                if (existed[s[l]] == 0) {  // 某类字符不存在了
-                    --dictSize;
+            while (l < r && freq[s[r]] == 0 && counter >= 2) {
+                --freq[s[l]];
+                if (freq[s[l]] == 0) {  // 某类字符不存在了
+                    --counter;
                 }
                 ++l;
             }
-            if (!existed[s[r]]) {  // 新加入一类字符
-                ++dictSize;
+            if (!freq[s[r]]) {  // 新加入一类字符
+                ++counter;
             }
-            ++existed[s[r]];
-            result = max(result, r - l + 1);
+            ++freq[s[r]];
+            ans = max(ans, r - l + 1);
             ++r;
         }
-
-        return result;
+        return ans;
     }
 };
 
