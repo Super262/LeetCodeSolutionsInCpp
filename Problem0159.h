00d600c82cf5fd3c647a46463e13dd7fd01e8790
fengwei <virusformail@gmail.com>
2022-05-13 13:52:46 -0700

    Reviewed Problem 159.

--- Problem0159.h
+++ Problem0159.h
@@ -11,29 +11,27 @@
 using namespace std;
 
 class Solution {
+    // 滑动窗口，经典题目
 public:
     int lengthOfLongestSubstringTwoDistinct(const string &s) {
         const auto n = (int) s.size();
         int freq[256];  // 数组替代哈希表记录每类字符的频率，加快速度
         int counter = 0;  // 当前字符种类别数
         memset(freq, 0, sizeof freq);
-        int r = 0;
-        int l = 0;
         int ans = 0;
-        while (r < n) {  // 滑动窗口
-            while (l < r && freq[s[r]] == 0 && counter >= 2) {
+        for (int l = 0, r = 0; r < n; ++r) {  // 滑动窗口
+            if (!freq[s[r]]) {  // 新加入的字符
+                ++counter;
+            }
+            ++freq[s[r]];
+            while (l <= r && counter > 2) {
                 --freq[s[l]];
-                if (freq[s[l]] == 0) {  // 某类字符不存在了
+                if (!freq[s[l]]) {  // 某类字符不存在了
                     --counter;
                 }
                 ++l;
             }
-            if (!freq[s[r]]) {  // 新加入一类字符
-                ++counter;
-            }
-            ++freq[s[r]];
             ans = max(ans, r - l + 1);
-            ++r;
         }
         return ans;
     }
