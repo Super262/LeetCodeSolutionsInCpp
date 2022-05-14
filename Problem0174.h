1bf88a760d046d9b66b6071ed4cc3b5a9e04f00e
fengwei <virusformail@gmail.com>
2022-05-13 17:57:18 -0700

    Reviewed Problem 174.

--- Problem0174.h
+++ Problem0174.h
@@ -11,8 +11,7 @@
 using namespace std;
 
 class Solution {
-    // 经典DP，必须掌握
-    // 已知最终状态（能量大于0），反推初始值
+    // 经典DP，必须掌握：已知最终状态（能量大于0），反推初始值
 public:
     int calculateMinimumHP(const vector<vector<int>> &dun) {
         const auto m = (int) dun.size();
@@ -22,12 +21,12 @@ public:
             for (int j = n - 1; j >= 0; --j) {
                 f[i % 2][j] = 0x3f3f3f3f;
                 if (i == m - 1 && j == n - 1) {
-                    f[i % 2][j] = max(1, 1 - dun[i][j]);
+                    f[i % 2][j] = max(1, 1 - dun[i][j]);  // 终点，能量最少为1
                 } else {
-                    if (i + 1 < m) {
+                    if (i + 1 < m) {  // 能向下走
                         f[i % 2][j] = min(f[i % 2][j], f[(i + 1) % 2][j] - dun[i][j]);
                     }
-                    if (j + 1 < n) {
+                    if (j + 1 < n) {  // 能向右走
                         f[i % 2][j] = min(f[i % 2][j], f[i % 2][j + 1] - dun[i][j]);
                     }
                     f[i % 2][j] = max(1, f[i % 2][j]);
