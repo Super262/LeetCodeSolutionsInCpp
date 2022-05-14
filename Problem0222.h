21cdafeb0d15316693a4b2d65deda2e10c8f1124
fengwei <virusformail@gmail.com>
2022-05-14 11:47:10 -0700

    Reviewed Problem 222.

--- Problem0222.h
+++ Problem0222.h
@@ -5,19 +5,10 @@
 #ifndef LEETCODESOLUTIONSINCPP_PROBLEM0222_H
 #define LEETCODESOLUTIONSINCPP_PROBLEM0222_H
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
+#include "treenode.h"
 
 class Solution {
+    // 由于输入是完全二叉树，我们可以根据树高来计算节点数
     // 时间复杂度：O((logN)^2)
 public:
     int countNodes(TreeNode *root) {
@@ -28,15 +19,15 @@ public:
         auto rc = root;
         int lh = 1;
         int rh = 1;
-        while (lc->left) {
+        while (lc->left) {  // 统计最左侧树高
             lc = lc->left;
             ++lh;
         }
-        while (rc->right) {
+        while (rc->right) {  // 统计最右侧树高
             rc = rc->right;
             ++rh;
         }
-        if (lh == rh) {
+        if (lh == rh) {  // 满二叉树，并且只有最底层为叶子节点
             return (1 << lh) - 1;
         }
         return 1 + countNodes(root->left) + countNodes(root->right);
