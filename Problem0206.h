d9dec1cd4271ea1a68000d62ef5ea16db35cdebc
fengwei <virusformail@gmail.com>
2022-05-14 02:53:49 -0700

    Reviewed Problem 206.

--- Problem0206.h
+++ Problem0206.h
@@ -5,17 +5,7 @@
 #ifndef LEETCODESOLUTIONSINCPP_PROBLEM0206_H
 #define LEETCODESOLUTIONSINCPP_PROBLEM0206_H
 
-
-struct ListNode {
-    int val;
-    ListNode *next;
-
-    ListNode() : val(0), next(nullptr) {}
-
-    ListNode(int x) : val(x), next(nullptr) {}
-
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
+#include "listnode.h"
 
 class Solution {
 public:
