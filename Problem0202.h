981e83c7596763a073ccb63fd38e256796d81d93
fengwei <virusformail@gmail.com>
2022-05-14 02:24:31 -0700

    Reviewed Problem 202.

--- Problem0202.h
+++ Problem0202.h
@@ -6,29 +6,27 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0202_H
 
 class Solution {
-    // 经典算法，直接背诵
-    // 类似于142题（Linked List Cycle II）
-    // https://www.acwing.com/solution/content/19516/
+    // 经典算法，直接背诵：若两指针相遇时均为1，说明不存在环；若两指针相遇时均不为1，则表示存在环
+    // 类似于142题（Linked List Cycle II）：https://www.acwing.com/solution/content/19516/
 public:
     bool isHappy(const int n) {
-        // 注意：这里的快慢指针的起点不能相同
         auto slow = n;
-        auto fast = squareSum(slow);
-        while (fast != slow) {
+        auto fast = n;
+        do {
             slow = squareSum(slow);
             fast = squareSum(squareSum(fast));
-        }
+        } while (fast != slow);
         return fast == 1;
     }
 
 private:
     int squareSum(int x) {
-        int result = 0;
+        int ans = 0;
         while (x) {
-            result += (x % 10) * (x % 10);
+            ans += (x % 10) * (x % 10);
             x /= 10;
         }
-        return result;
+        return ans;
     }
 };
 
