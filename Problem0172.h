9ba1da718abc3aa11df179b03533f372c05ceef9
fengwei <virusformail@gmail.com>
2022-05-13 17:39:17 -0700

    Reviewed Problem 172.

--- Problem0172.h
+++ Problem0172.h
@@ -6,7 +6,8 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0172_H
 
 class Solution {
-    // 经典算法，直接背诵
+    // 直接背诵：设x = n!，若x尾部有k个"0"，说明x可以分解出k个"10"，即说明x可以分解出k个"2*5"
+    // x = n! = (2^a) * (5^b) * ... * (y^i)，答案为min(a, b)；由于5>2，b == min(a, b)
     // https://www.acwing.com/solution/content/19308/
 public:
     int trailingZeroes(int n) {
