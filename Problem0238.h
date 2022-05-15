3fcf2407a1d9432a417705314b999082b4131037
fengwei <virusformail@gmail.com>
2022-05-14 17:19:34 -0700

    Reviewed Problem 238.

--- Problem0238.h
+++ Problem0238.h
@@ -10,20 +10,22 @@
 using namespace std;
 
 class Solution {
+    // prefix[i]：[0:i-1]所有数的乘积；suffix[i]：[i+1:n-1]所有数的乘积；prefix[0] = suffix[n-1] = 1
+    // 因此，最终答案ans[i]=prefix[i]*suffix[i]
 public:
-    vector<int> productExceptSelf(vector<int> &nums) {
-        vector<int> result(nums.size(), 1);
+    vector<int> productExceptSelf(const vector<int> &nums) {
+        vector<int> ans(nums.size(), 1);
         int prefix = 1;
         for (int i = 1; i < (int) nums.size(); ++i) {
             prefix *= nums[i - 1];
-            result[i] *= prefix;
+            ans[i] *= prefix;
         }
         int suffix = 1;
         for (int i = (int) nums.size() - 2; i >= 0; --i) {
             suffix *= nums[i + 1];
-            result[i] *= suffix;
+            ans[i] *= suffix;
         }
-        return result;
+        return ans;
     }
 };
 
