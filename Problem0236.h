e185b4b20c850126f21cabbd11e587722266f122
fengwei <virusformail@gmail.com>
2022-05-14 17:11:02 -0700

    Reviewed Problem 236.

--- Problem0236.h
+++ Problem0236.h
@@ -5,35 +5,27 @@
 #ifndef LEETCODESOLUTIONSINCPP_PROBLEM0236_H
 #define LEETCODESOLUTIONSINCPP_PROBLEM0236_H
 
-#include <iostream>
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
+#include "treenode.h"
 
 class Solution {
-    // 简化版LCA算法
+    // 简化版LCA算法，类似后序遍历
 public:
     TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
         if (!root || !p || !q) {
-            return NULL;
+            return nullptr;
         }
         if (p == root || q == root) {
             return root;
         }
-        auto left_res = lowestCommonAncestor(root->left, p, q);  // 向左查找包含p或q的子树
-        auto right_res = lowestCommonAncestor(root->right, p, q);  // 向右查找包含p或q的子树
-        if (left_res && right_res) {
+        auto left_anc = lowestCommonAncestor(root->left, p, q);  // 向左查找包含p或q的子树
+        auto right_anc = lowestCommonAncestor(root->right, p, q);  // 向右查找包含p或q的子树
+        if (left_anc && right_anc) {
             return root;
         }
-        if (left_res) {
-            return left_res;
+        if (left_anc) {
+            return left_anc;
         }
-        return right_res;
+        return right_anc;
     }
 };
 
