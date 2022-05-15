c113c7da22bd6f7a10b145f2ea8ae80dd9558b90
fengwei <virusformail@gmail.com>
2022-05-15 13:48:28 -0700

    Reviewed Problem 244.

--- Problem0244.h
+++ Problem0244.h
@@ -12,35 +12,32 @@
 using namespace std;
 
 class WordDistance {
-private:
-    unordered_map<string, vector<int>> locations;
-
+    // locations预存每个单词出现的位置序列（相同单词重复出现）
+    // 贪心算法：利用locations[w]的天然有序性
 public:
     WordDistance(const vector<string> &dict) {
-        // 预存每个单词出现的位置
         for (int i = 0; i < (int) dict.size(); ++i) {
-            if (!locations.count(dict[i])) {
-                locations[dict[i]] = vector<int>();
-            }
             locations[dict[i]].emplace_back(i);
         }
     }
 
     int shortest(const string &w1, const string &w2) {
-        int result = INT_MAX;
-        // 巧妙解法，利用locations的天然有序性
+        int ans = INT_MAX;
         int p1 = 0;
         int p2 = 0;
         while (p1 < (int) locations[w1].size() && p2 < (int) locations[w2].size()) {
-            result = min(result, abs(locations[w1][p1] - locations[w2][p2]));
-            if (locations[w1][p1] > locations[w2][p2]) {
+            ans = min(ans, abs(locations[w1][p1] - locations[w2][p2]));
+            if (locations[w1][p1] > locations[w2][p2]) {  // p2追近p1
                 ++p2;
-            } else {
+            } else {  // p1追近p2
                 ++p1;
             }
         }
-        return result;
+        return ans;
     }
+
+private:
+    unordered_map<string, vector<int>> locations;
 };
 
 
