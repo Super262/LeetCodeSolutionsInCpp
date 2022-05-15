4cac48cf1da4ac63838f63cde1277eae1446bc1d
fengwei <virusformail@gmail.com>
2022-05-15 14:18:02 -0700

    Reviewed Problem 246.

--- Problem0246.h
+++ Problem0246.h
@@ -10,37 +10,35 @@
 using namespace std;
 
 class Solution {
+    // 判断num是否中心对称：双指针，从边界向中心检查
+    // 根据常识，"2"、"3"、"4"、"5"、"7"是不合要求的
 public:
     bool isStrobogrammatic(const string &num) {
         int l = 0;
-        int r = (int) num.size() - 1;
-        while (l <= r) {
-            if (num[l] == '2' || num[l] == '3' || num[l] == '4' || num[l] == '5' || num[l] == '7') {
-                return false;
-            }
-            if (num[r] == '2' || num[r] == '3' || num[r] == '4' || num[r] == '5' || num[r] == '7') {
-                return false;
-            }
-            if (num[l] == '0') {
+        auto r = (int) num.size() - 1;
+        while (l <= r) {  // l<=r，因为num最小长度为1
+            if (num[l] == '0') {  // "0" 对应 "0"
                 if (num[r] != '0') {
                     return false;
                 }
-            } else if (num[l] == '1') {
+            } else if (num[l] == '1') {  // "1" 对应 "1"
                 if (num[r] != '1') {
                     return false;
                 }
-            } else if (num[l] == '6') {
+            } else if (num[l] == '6') {  // "6" 对应 "9"
                 if (num[r] != '9') {
                     return false;
                 }
-            } else if (num[l] == '8') {
+            } else if (num[l] == '8') {  // "8" 对应 "8"
                 if (num[r] != '8') {
                     return false;
                 }
-            } else {
+            } else if (num[l] == '9') { // "9" 对应 "6"
                 if (num[r] != '6') {
                     return false;
                 }
+            } else {  // 出现"2"、"3"、"4"、"5"、"7"
+                return false;
             }
             ++l;
             --r;
