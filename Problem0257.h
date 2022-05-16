75dc72351bd8b562edb416841c11f48548facc33
fengwei <virusformail@gmail.com>
2022-05-16 04:16:09 -0700

    Reviewed Problem 257.

--- Problem0257.h
+++ Problem0257.h
@@ -7,25 +7,14 @@
 
 #include <vector>
 #include <string>
+#include "treenode.h"
 
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
 class Solution {
+    // 直接DFS，时空复杂度：O(n^2)
 public:
     vector<string> binaryTreePaths(TreeNode *root) {
-        // 时空复杂度：O(n^2)
         vector<int> path;
         vector<string> result;
         dfs(root, path, result);
