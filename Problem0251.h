cc3bac4236cbf68cdcc9693610c9401da56ad0c7
fengwei <virusformail@gmail.com>
2022-05-15 17:09:20 -0700

    Reviewed Problem 251.

--- Problem0251.h
+++ Problem0251.h
@@ -10,42 +10,38 @@
 using namespace std;
 
 class Vector2D {
+    // 注意细节，列迭代器走到尾部后，更新行迭代器，直至数组尾部
 private:
-    vector<vector<int>>::iterator rowIt;
-    vector<int>::iterator colIt;
-    vector<vector<int>>::iterator vecEnd;
+    vector<vector<int>>::iterator row_it;  // 当前行
+    vector<int>::iterator col_it;  // 当前列
+    vector<vector<int>>::iterator grid_end;  // 矩阵尾部
 
 public:
     Vector2D(vector<vector<int>> &vec) {
-        vec.emplace(vec.begin(), vector<int>({-1}));  // 添加一个无效的头节点
-        rowIt = vec.begin();
-        colIt = rowIt->begin();
-        vecEnd = vec.end();
+        grid_end = vec.end();
+        row_it = vec.begin();
+        while (row_it != grid_end && row_it->empty()) {  // 跳过空行，寻找第一个有效元素
+            row_it++;
+        }
+        if (row_it != grid_end) {
+            col_it = row_it->begin();
+        }
     }
 
     int next() {
-        ++colIt;
-        while (colIt == rowIt->end()) {
-            ++rowIt;
-            colIt = rowIt->begin();
+        auto ans = *col_it;
+        col_it++;
+        while (row_it != grid_end && col_it == row_it->end()) {  // 寻找下一个有效元素
+            row_it++;
+            if (row_it != grid_end) {
+                col_it = row_it->begin();
+            }
         }
-        return *colIt;
+        return ans;
     }
 
     bool hasNext() {
-        auto tempCol = colIt;
-        auto tempRow = rowIt;
-        ++tempCol;
-        while (tempRow != vecEnd && tempCol == tempRow->end()) {
-            ++tempRow;
-            if (tempRow != vecEnd) {
-                tempCol = tempRow->begin();
-            }
-        }
-        if (tempRow != vecEnd && tempCol != tempRow->end()) {
-            return true;
-        }
-        return false;
+        return row_it != grid_end;
     }
 };
 
