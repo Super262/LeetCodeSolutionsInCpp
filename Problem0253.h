ac341ee991e4e35b5e96df5e07b5f60d43e73ef6
fengwei <virusformail@gmail.com>
2022-05-15 17:33:12 -0700

    Reviewed Problem 253.

--- Problem0253.h
+++ Problem0253.h
@@ -12,6 +12,7 @@
 using namespace std;
 
 class Solution {
+    // 贪心算法，区间分组问题
 public:
     int minMeetingRooms(vector<vector<int>> &intervals) {
         sort(intervals.begin(), intervals.end());
@@ -21,9 +22,9 @@ public:
                 heap.emplace(x[1]);
                 continue;
             }
-            if (heap.top() > x[0]) {
+            if (heap.top() > x[0]) {  // 发生重叠，添加新会议室
                 heap.emplace(x[1]);
-            } else {
+            } else {  // 无重叠
                 heap.pop();
                 heap.emplace(x[1]);
             }
