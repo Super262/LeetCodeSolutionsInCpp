d73ea0c0089cb9b51d34b9ff406b5808ed762d3f
fengwei <virusformail@gmail.com>
2022-05-16 17:20:46 -0700

    Finished Problem 260.

--- Problem0260.h
+++ Problem0260.h
@@ -10,16 +10,17 @@
 using namespace std;
 
 class Solution {
+    // 若a!=b，则a^b至少有1位为1；nums中只有两个只出现一次的数字a、b，因此nums中所有数字依次异或后的结果all_xor=a^b
+    // 我们可以在all_xor中找到非零的位置i，根据i=0/1分别生成a/b；时间O(n)，空间O(1)
     // https://www.acwing.com/solution/content/237/
 public:
     vector<int> singleNumber(const vector<int> &nums) {
-        // 时间O(n)，空间O(1)
         int all_xor = 0;
         for (const auto &x: nums) {
             all_xor ^= x;
         }
         int k = 0;
-        while ((all_xor & 1) == 0) {
+        while ((all_xor & 1) == 0) {  // 查找非零位置
             all_xor >>= 1;
             ++k;
         }
