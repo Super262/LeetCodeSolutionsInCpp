4c193ebd0a5511e33f811294238f93f16f772349
fengwei <virusformail@gmail.com>
2022-05-16 18:42:41 -0700

    Reviewed Problem 264.

--- Problem0264.h
+++ Problem0264.h
@@ -10,19 +10,21 @@
 using namespace std;
 
 class Solution {
-    // 多路归并：https://www.acwing.com/solution/content/20166/
+    // 类似多路归并：每次选择一个最小值；第2个丑数的候选集是{2}，{3}，{5}，当某个数x被加入结果集，就在源候选集加入一个新数字
+    // https://www.acwing.com/solution/content/20166/
 public:
     int nthUglyNumber(int n) {
-        vector<int> result(1, 1);  // 初始时只包括1
+        vector<int> ans(1, 1);  // 初始时只包括1
         int p1 = 0;
         int p2 = 0;
         int p3 = 0;
-        while (result.size() < n) { // 注意：构造新丑数时，只能基于已有的丑数
-            auto r1 = 2 * result[p1];
-            auto r2 = 3 * result[p2];
-            auto r3 = 5 * result[p3];
+        ans.reserve(n);
+        while ((int) ans.size() < n) { // 注意：构造新丑数时，只能基于已有的丑数
+            auto r1 = 2 * ans[p1];
+            auto r2 = 3 * ans[p2];
+            auto r3 = 5 * ans[p3];
             auto minimal = min(r1, min(r2, r3));
-            result.emplace_back(minimal);
+            ans.emplace_back(minimal);
             if (minimal == r1) {
                 ++p1;
             }
@@ -33,7 +35,7 @@ public:
                 ++p3;
             }
         }
-        return result[n - 1];
+        return ans.back();
     }
 };
 
