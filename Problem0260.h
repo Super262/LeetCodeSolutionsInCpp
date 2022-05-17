72362063c4cd530e7172c63c128a08e345654f21
fengwei <virusformail@gmail.com>
2022-05-16 17:32:22 -0700

    Reviewed Problem 260.

--- Problem0260.h
+++ Problem0260.h
@@ -11,7 +11,7 @@ using namespace std;
 
 class Solution {
     // 若a!=b，则a^b至少有1位为1；nums中只有两个只出现一次的数字a、b，因此nums中所有数字依次异或后的结果all_xor=a^b
-    // 我们可以在all_xor中找到非零的位置i，根据i=0/1分别生成a/b；时间O(n)，空间O(1)
+    // 我们可以在all_xor中找到非零的位置i，根据i=0/1分别生成a/b（s1是所有第i位为1的二进制数的异或和，s0同理）；时间O(n)，空间O(1)
     // https://www.acwing.com/solution/content/237/
 public:
     vector<int> singleNumber(const vector<int> &nums) {
@@ -20,24 +20,21 @@ public:
             all_xor ^= x;
         }
         int k = 0;
-        while ((all_xor & 1) == 0) {  // 查找非零位置
+        while ((all_xor & 1) == 0) {
             all_xor >>= 1;
             ++k;
         }
-        return {findKthEquals(nums, k, 0), findKthEquals(nums, k, 1)};
-    }
-
-private:
-    int findKthEquals(const vector<int> &nums, const int &k, const int &val) {
-        int result = 0;
+        int s0 = 0;
+        int s1 = 0;
         for (const auto &x: nums) {
-            if (((x >> k) & 1) == val) {
-                result ^= x;
+            if ((x >> k) & 1) {
+                s1 ^= x;
+            } else {
+                s0 ^= x;
             }
         }
-        return result;
+        return {s0, s1};
     }
-
 };
 
 #endif //LEETCODESOLUTIONSINCPP_PROBLEM0260_H
