5a2f979a4925d365f28db87cb9b36f3870fb26ce
fengwei <virusformail@gmail.com>
2022-05-17 06:43:15 -0700

    Reviewed Problem 269.

--- Problem0269.h
+++ Problem0269.h
@@ -7,97 +7,83 @@
 
 #include <cstring>
 #include <string>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
-class Problem0269 {
+class Solution {
+    // 拓扑排序：依据升序排列的单词序列建立有向图，边 (a, b) 表示 a < b；输出拓扑序列，表示所有种类字符的升序序列
 public:
-    void addEdge(int headIdx[], int nextIdx[], int vertexValue[], const int a, const int b, int &idx) {
-        vertexValue[idx] = b;
-        nextIdx[idx] = headIdx[a];
-        headIdx[a] = idx;
-        ++idx;
-    }
-
-    string topoSort(const int headIdx[],
-                    const int nextIdx[],
-                    const int vertexValue[],
-                    const bool nodeExisted[],
-                    int inDegree[]) {
-        int q[28];
-        int hh = 0, tt = -1;
-        for (int i = 1; i <= 26; ++i) {
-            if (nodeExisted[i] && inDegree[i] == 0) {
-                q[++tt] = i;
-            }
-        }
-        string result;
-        while (hh <= tt) {
-            auto root = q[hh++];
-            result.push_back((char) ('a' + root - 1));
-            for (int idx = headIdx[root]; idx != -1; idx = nextIdx[idx]) {
-                auto childV = vertexValue[idx];
-                --inDegree[childV];
-                if (inDegree[childV] == 0) {
-                    q[++tt] = childV;
-                }
-            }
-        }
-        return result;
-    }
-
-    string alienOrder(vector<string> &words) {
-        int headIdx[27];
-        int nextIdx[words.size() * 2 + 1];
-        int vertexValue[words.size() * 2 + 1];
-        memset(headIdx, -1, sizeof headIdx);  // -1为索引的无效值
-        memset(nextIdx, -1, sizeof nextIdx);
-        memset(vertexValue, 0, sizeof vertexValue);  // 0为顶点编号的无效值
-
-        // Generate nodes
-        int nodesCount = 0;
-        bool nodeExisted[27];
-        memset(nodeExisted, 0, sizeof nodeExisted);
-        for (const auto &s: words) {
-            for (auto ch: s) {
-                if (nodeExisted[ch - 'a' + 1]) {
+    string alienOrder(const vector<string> &words) {
+        int n = 0;  // 字符种类数
+        bool existed[K];  // existed[ch]：字符ch是否存在
+        memset(existed, 0, sizeof existed);
+        for (const auto &w: words) {
+            for (const auto &ch: w) {
+                auto i = ch - 'a';
+                if (existed[i]) {
                     continue;
                 }
-                nodeExisted[ch - 'a' + 1] = true;
-                ++nodesCount;
+                existed[i] = true;
+                ++n;
             }
         }
-
-        // Build the map.
-        int idx = 0;
-        int inDegree[27];
-        memset(inDegree, 0, sizeof inDegree);
-        for (int i = 1; i < words.size(); ++i) {
+        vector<int> graph[K];  // 有向图
+        int in_degree[K];
+        memset(in_degree, 0, sizeof in_degree);
+        for (int i = 1; i < (int) words.size(); ++i) {
             const auto &s1 = words[i - 1];
             const auto &s2 = words[i];
-            if (s1.size() > s2.size() && s1.find(s2) == 0) {   // words的顺序无效：s1比s2长，且s1完全包含s2
+            if (s1.size() > s2.size() && s1.find(s2) == 0) {  // s1包含s2，且s1比s2长，说明 s1 > s2，非法
                 return "";
             }
-            int j;
-            for (j = 0; j < (int) min(s1.size(), s2.size()); ++j) {
-                if (s1[j] == s2[j]) {
-                    continue;
+            int j = 0;
+            while (j < (int) min(s1.size(), s2.size())) {  // 找到s1、s2中首个不同字符
+                if (s1[j] != s2[j]) {
+                    break;
                 }
-                break;
+                ++j;
             }
-            if (j == (int) min(s1.size(), s2.size())) {  // s1和s2没有不相同的元素，跳过
+            if (j == (int) min(s1.size(), s2.size())) {  // s2包含s1，且s2不短于s1，说明s1、s2不存在不同的字符
                 continue;
             }
-            addEdge(headIdx, nextIdx, vertexValue, s1[j] - 'a' + 1, s2[j] - 'a' + 1, idx);
-            ++inDegree[s2[j] - 'a' + 1];
+            auto a = s1[j] - 'a';
+            auto b = s2[j] - 'a';
+            graph[a].emplace_back(b);  // 添加边 (a, b)
+            ++in_degree[b];
         }
-
-        // Topological Order
-        string result = topoSort(headIdx, nextIdx, vertexValue, nodeExisted, inDegree);
-        if (result.size() < nodesCount) {
+        auto ans = topoSort(graph, n, existed, in_degree);
+        if ((int) ans.size() < n) {  // 拓扑序列长度小于n，说明拓扑序不存在
             return "";
         }
-        return result;
+        return ans;
+    }
+
+private:
+    const int K = 26;
+
+    string topoSort(const vector<int> graph[], int n, const bool existed[], int in_degree[]) {
+        queue<int> q;
+        for (int v = 0; v < K; ++v) {
+            if (existed[v] && !in_degree[v]) {
+                q.emplace(v);
+            }
+        }
+        string ans;
+        ans.reserve(n);
+        while (!q.empty()) {
+            auto u = q.front();
+            q.pop();
+            ans += (char) (u + 'a');
+            for (const auto &v: graph[u]) {
+                --in_degree[v];
+                if (!in_degree[v]) {
+                    q.emplace(v);
+                }
+            }
+        }
+        return ans;
     }
 };
 
