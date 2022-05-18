dc101fce03a5b982429f87aa63dd10e55c7ddd8e
fengwei <virusformail@gmail.com>
2022-05-18 08:52:54 -0700

    Reviewed Problem 282.

--- Problem0282.h
+++ Problem0282.h
@@ -11,48 +11,52 @@
 using namespace std;
 
 class Solution {
-    // 巧妙的常数优化：https://www.acwing.com/solution/content/332/
+    // 巧妙的常数优化：我们维护不变式a+b*()，括号中的数是我们当前枚举的数字
+    // 若下个运算符是"+"，那么 a+b*(c)+()=(a+b*c)+1*()
+    // 若下个运算符是"-"，那么 a+b*(c)-()=(a+b*c)+(-1)*()
+    // 若下个运算符是"+"，那么 a+b*(c)*()=a+(b*c)*()
+    // 最终等式为a+1*()，a即是最终结果
+    // https://www.acwing.com/solution/content/332/
+    // 细节：DFS后要注意恢复现场，撤销当前阶段所有操作，要反复检查撤退的步数，确保正确
 public:
-    vector<string> addOperators(const string &num, const int &target) {
-        vector<string> result;
-        string path(100, '\0');
-        dfs(num, target, 0, 0, 1, path, 0, result);
-        return result;
+    vector<string> addOperators(const string &num, int target) {
+        vector<string> ans;
+        string path;
+        dfs(num, 0, 0, 1, target, path, ans);
+        return ans;
     }
 
 private:
-    void dfs(const string &num,
-             const int &target,
-             const int &u,
-             const long long &a,
-             const long long &b,
-             string &path,
-             const int &len,
-             vector<string> &result) {
-        if (u == num.size()) {
-            if (target == a) {
-                result.emplace_back(path.substr(0, len - 1));  // 需要排除最后的“+”
+    void dfs(const string &num, int u, long long a, long long b, int target, string &path, vector<string> &ans) {
+        if (u >= (int) num.size()) {
+            if (a == target) {
+                ans.emplace_back(path);
+                ans.back().pop_back();  // 弹出末尾的'+'
             }
             return;
         }
         long long c = 0;
         for (auto i = u; i < (int) num.size(); ++i) {
             c = c * 10 + (num[i] - '0');
-            path[len + i - u] = num[i];
-            // 我们默认尝试添加"+"；这条递归路径是指向可行解的路径，因为a的值会被更新、u会变为num.size()
-            path[len + i - u + 1] = '+';
-            dfs(num, target, i + 1, a + b * c, 1, path, len + i - u + 2, result);
-            if (i + 1 < num.size()) {
-                path[len + i - u + 1] = '-';
-                dfs(num, target, i + 1, a + b * c, -1, path, len + i - u + 2, result);
+            path += num[i];
+            path += '+';
+            dfs(num, i + 1, a + b * c, 1, target, path, ans);
+            path.pop_back();
+            if (i + 1 < (int) num.size()) {
+                path += '-';
+                dfs(num, i + 1, a + b * c, -1, target, path, ans);
+                path.pop_back();
+                path += '*';
+                dfs(num, i + 1, a, b * c, target, path, ans);
+                path.pop_back();
             }
-            if (i + 1 < num.size()) {
-                path[len + i - u + 1] = '*';
-                dfs(num, target, i + 1, a, b * c, path, len + i - u + 2, result);
+            if (num[u] == '0') {  // 发现前导零，终止
+                path.pop_back();  // 弹出'0'
+                return;
             }
-            if (num[u] == '0') {
-                break;
-            }
+        }
+        for (auto i = u; i < (int) num.size(); ++i) {  // 弹出当前末尾的数字
+            path.pop_back();
         }
     }
 };
