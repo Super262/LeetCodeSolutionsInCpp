eff398f22b4cef8d4ac47990cba0d02081622d5b
fengwei <virusformail@gmail.com>
2022-05-20 04:39:56 -0700

    Reviewed Problem 300.

--- Problem0300.h
+++ Problem0300.h
@@ -11,28 +11,29 @@
 using namespace std;
 
 class Solution {
+    // 贪心算法，二分查找实现，参考AcWing 896
 public:
     int lengthOfLIS(const vector<int> &nums) {
-        const int n = (int) nums.size();
-        int lisTail[n + 1];
-        memset(lisTail, 0x3f, sizeof lisTail);
-        int result = 0;
+        const auto n = (int) nums.size();
+        int lis_tail[n + 1];
+        memset(lis_tail, 0x3f, sizeof lis_tail);
+        int ans = 0;
         for (int i = 0; i < n; ++i) {
             const auto &t = nums[i];
             int l = 0;
-            int r = result;
+            int r = ans;
             while (l < r) {
                 auto mid = l + (r - l + 1) / 2;
-                if (lisTail[mid] >= t) {
+                if (lis_tail[mid] >= t) {
                     r = mid - 1;
                 } else {
                     l = mid;
                 }
             }
-            lisTail[l + 1] = min(lisTail[l + 1], t);
-            result = max(result, l + 1);
+            lis_tail[l + 1] = min(lis_tail[l + 1], t);
+            ans = max(ans, l + 1);
         }
-        return result;
+        return ans;
     }
 };
 
