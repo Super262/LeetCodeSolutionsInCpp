f1be8492e6e6fbf03bd8c6e29b65a3ae7a0cac1b
fengwei <virusformail@gmail.com>
2022-05-20 04:02:49 -0700

    Reviewed Problem 297.

--- Problem0297.h
+++ Problem0297.h
@@ -6,61 +6,56 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0297_H
 
 #include <string>
+#include "treenode.h"
 
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
 class Codec {
+    // 编码、解码都采用类似先序遍历的思路
+    // 编码：","分隔节点值，"#"指示空节点；解码：若当前为空节点（"#"），指针后移2位（跳过当前节点和分隔符）
 public:
 
     // Encodes a tree to a single string.
     string serialize(TreeNode *root) {
         string result;
-        dfs_ser(root, result);
+        dfs_encode(root, result);
         return result;
     }
 
     // Decodes your encoded data to tree.
     TreeNode *deserialize(const string &data) {
         int idx = 0;
-        return dfs_des(data, idx);
+        return dfs_decode(data, idx);
     }
 
 private:
-    void dfs_ser(TreeNode *root, string &result) {
+    void dfs_encode(TreeNode *root, string &result) {
         if (!root) {
             result += "#,";
             return;
         }
         result += to_string(root->val);
         result += ",";
-        dfs_ser(root->left, result);
-        dfs_ser(root->right, result);
+        dfs_encode(root->left, result);
+        dfs_encode(root->right, result);
     }
 
-    TreeNode *dfs_des(const string &data, int &idx) {
-        if (idx >= data.size()) {
-            return NULL;
+    TreeNode *dfs_decode(const string &data, int &idx) {
+        if (idx >= (int) data.size()) {
+            return nullptr;
         }
         if (data[idx] == '#') {
-            idx += 2;
-            return NULL;
+            idx += 2;  // 跳过当前节点和分隔符
+            return nullptr;
         }
-        int k = idx;
+        auto k = idx;
         while (data[k] != ',') {
             ++k;
         }
         auto root = new TreeNode(stoi(data.substr(idx, k - idx)));
-        idx = k + 1;
-        root->left = dfs_des(data, idx);
-        root->right = dfs_des(data, idx);
+        idx = k + 1;  // 跳过分隔符
+        root->left = dfs_decode(data, idx);
+        root->right = dfs_decode(data, idx);
         return root;
     }
 };
