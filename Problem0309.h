416a6a2185b2342b37421dfbd580877c8026c275
fengwei <virusformail@gmail.com>
2022-05-21 03:49:51 -0700

    Reviewed Problem 309.

--- Problem0309.h
+++ Problem0309.h
@@ -19,9 +19,9 @@ public:
             return 0;
         }
         int f[3][3];
-        memset(f, -0x3f, sizeof f);
         f[0][0] = 0;
         f[0][1] = -prices[0];
+        f[0][2] = -0x3f3f3f3f;
         for (int i = 1; i < n; ++i) {
             f[i % 3][0] = max(f[(i - 1) % 3][0], f[(i - 1) % 3][2]);
             f[i % 3][1] = max(f[(i - 1) % 3][1], f[(i - 1) % 3][0] - prices[i]);
