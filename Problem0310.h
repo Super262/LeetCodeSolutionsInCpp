0ee5b4b24f69a569530694d4981c1253e149f090
fengwei <virusformail@gmail.com>
2022-05-21 16:17:07 -0700

    Reviewed Problem 310.

--- Problem0310.h
+++ Problem0310.h
@@ -15,78 +15,83 @@ class Solution {
     // 设v是搜索路径上的某个节点，u是v的前驱节点：d1[v]是以v为根的子树的高度最大值，d2[v]是以v为根的子树的高度次大值
     // up[v]是节点v向上的最大距离，next1[v]是v达到最大距离的路径上的后继，next2[v]是v达到次大距离的路径上的后继
 public:
-    vector<int> findMinHeightTrees(const int &n, const vector<vector<int>> &edges) {
-        vector<vector<int>> graph(n, vector<int>());
+    vector<int> findMinHeightTrees(int n, const vector<vector<int>> &edges) {
+        vector<int> graph[n];
         for (const auto &e: edges) {
             graph[e[0]].emplace_back(e[1]);
             graph[e[1]].emplace_back(e[0]);
         }
-        vector<int> d1(n, 0);
-        vector<int> d2(n, 0);
-        vector<int> next1(n, 0);
-        vector<int> next2(n, 0);
-        vector<int> up(n, 0);
+        int d1[n];
+        int d2[n];
+        int next1[n];
+        int next2[n];
+        int up[n];
+        memset(d1, 0, sizeof d1);
+        memset(d2, 0, sizeof d2);
+        memset(up, 0, sizeof up);
+        memset(next1, -1, sizeof next1);
+        memset(next2, -1, sizeof next2);
         dfs1(0, -1, graph, d1, next1, d2, next2);
         dfs2(0, -1, graph, up, d1, next1, d2, next2);
-        int min_h = n + 1;
-        for (int i = 0; i < n; ++i) {
-            min_h = min(min_h, max(d1[i], up[i]));
+        auto minh = n + 1;
+        for (int v = 0; v < n; ++v) {
+            minh = min(minh, max(up[v], d1[v]));
         }
-        vector<int> result;
-        for (int i = 0; i < n; ++i) {
-            if (min_h == max(d1[i], up[i])) {
-                result.emplace_back(i);
+        vector<int> ans;
+        for (int v = 0; v < n; ++v) {
+            if (minh == max(d1[v], up[v])) {
+                ans.emplace_back(v);
             }
         }
-        return result;
+        return ans;
     }
 
 private:
-    void dfs1(const int root,
-              const int father,
-              const vector<vector<int>> &graph,
-              vector<int> &d1,
-              vector<int> &next1,
-              vector<int> &d2,
-              vector<int> &next2) {
+    void dfs1(int u,
+              int father,
+              vector<int> graph[],
+              int d1[],
+              int next1[],
+              int d2[],
+              int next2[]) {
         // 求解以root为根时，各个子树高度的最大值d1和次大值d2，并分别记录后继next1和next2
-        for (const auto &x: graph[root]) {
-            if (x == father) {
+        for (const auto &v: graph[u]) {
+            if (v == father) {
                 continue;
             }
-            dfs1(x, root, graph, d1, next1, d2, next2);
-            auto d = d1[x] + 1;
-            if (d >= d1[root]) {
-                d2[root] = d1[root];
-                d1[root] = d;
-                next2[root] = next1[root];
-                next1[root] = x;
-            } else if (d > d2[root]) {
-                d2[root] = d;
-                next2[root] = x;
+            dfs1(v, u, graph, d1, next1, d2, next2);
+            auto d = d1[v] + 1;
+            if (d >= d1[u]) {
+                d2[u] = d1[u];
+                next2[u] = next1[u];
+                d1[u] = d;
+                next1[u] = v;
+            } else if (d > d2[u]) {
+                d2[u] = d;
+                next2[u] = v;
             }
         }
     }
 
-    void dfs2(const int root,
-              const int father,
-              const vector<vector<int>> &graph,
-              vector<int> &up,
-              vector<int> &d1,
-              vector<int> &next1,
-              vector<int> &d2,
-              vector<int> &next2) {
+    void dfs2(int u,
+              int father,
+              vector<int> graph[],
+              int up[],
+              int d1[],
+              int next1[],
+              int d2[],
+              int next2[]) {
         // 求解以root为根时，向上延伸的最大长度
-        for (const auto &x: graph[root]) {
-            if (x == father) {
+        for (const auto &v: graph[u]) {
+            if (v == father) {
                 continue;
             }
-            if (x == next1[root]) { // root的向下最大路径经过x
-                up[x] = max(up[root], d2[root]) + 1; // 选择其它路径
+            if (next1[u] == v) {  // root的向下最大路径经过x，选择次大路径
+                up[v] = max(up[u], d2[u]) + 1;
             } else {
-                up[x] = max(up[root], d1[root]) + 1;
+                up[v] = max(up[u], d1[u]) + 1;
             }
-            dfs2(x, root, graph, up, d1, next1, d2, next2);
+            dfs2(v, u, graph, up, d1, next1, d2, next2);
         }
     }
 };
