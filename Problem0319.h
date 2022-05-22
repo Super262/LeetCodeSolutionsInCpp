2f8e41d4ed6c686705ae3167d8bd8ad717894f29
fengwei <virusformail@gmail.com>
2022-05-22 07:19:44 -0700

    Reviewed Problem 319.

--- Problem0319.h
+++ Problem0319.h
@@ -11,11 +11,11 @@ using namespace std;
 
 class Solution {
     // 灯泡i被按下的次数：i的约数个数
-    // 数学性质：完全平方数的约数个数是奇数（充要条件
-    // https://www.acwing.com/solution/content/346/
+    // 数学性质：完全平方数的约数个数是奇数（充要条件）
+    // https://leetcode.cn/problems/bulb-switcher/solution/deng-pao-kai-guan-by-leetcode-solution-rrgp/
 public:
     int bulbSwitch(const int &n) {
-        return (int) sqrt(n);
+        return (int) sqrt(n + 0.5);
     }
 };
 
