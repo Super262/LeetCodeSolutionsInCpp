fa53a009e9b7eec3e8511e95522f61fb2148f4d2
fengwei <virusformail@gmail.com>
2022-05-23 11:27:51 -0700

    Reviewed Problem 337.

--- Problem0337.h
+++ Problem0337.h
@@ -6,20 +6,9 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0337_H
 
 #include <vector>
+#include "treenode.h"
 
 using namespace std;
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
 
 class Solution {
 public:
