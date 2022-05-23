98e5cc5445d5b42fbc7a411515057cf002532355
fengwei <virusformail@gmail.com>
2022-05-22 17:27:16 -0700

    Reviewed Problem 325.

--- Problem0325.h
+++ Problem0325.h
@@ -11,9 +11,11 @@
 using namespace std;
 
 class Solution {
+    // 设prefix[i]是前i个数的和（i>=1），我们要找到最小的j，满足prefix[i]-prefix[j]=k（j<i）
+    // 细节：用long型变量保存前缀和，防止溢出
 public:
     int maxSubArrayLen(const vector<int> &nums, int k) {
-        unordered_map<long, int> first_idx;
+        unordered_map<long, int> first_idx;  // first_idx[p]=i：满足prefix[i]=p的最小的i
         int ans = 0;
         long p = 0;
         first_idx[p] = 0;
