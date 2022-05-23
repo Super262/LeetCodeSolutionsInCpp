d8f5f35b08a8bda452b248d66dbc9d67004d1793
fengwei <virusformail@gmail.com>
2022-05-23 08:14:41 -0700

    Reviewed Problem 329.

--- Problem0329.h
+++ Problem0329.h
@@ -6,47 +6,101 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0329_H
 
 #include <vector>
+#include <queue>
 
 using namespace std;
 
 class Solution {
+    // 转化点和点之间的关系为DAG，拓扑排序：若a、b邻接且a<b，DAG中包含边(b,a)
+public:
+    int longestIncreasingPath(const vector<vector<int>> &matrix) {
+        const int dx[4] = {0, 0, 1, -1};
+        const int dy[4] = {1, -1, 0, 0};
+        const auto m = (int) matrix.size();
+        const auto n = (int) matrix[0].size();
+        queue<pair<int, int>> q;
+        int degree[m][n];
+        memset(degree, 0, sizeof degree);
+        for (int x = 0; x < m; ++x) {
+            for (int y = 0; y < n; ++y) {
+                for (int i = 0; i < 4; ++i) {
+                    auto nx = x + dx[i];
+                    auto ny = y + dy[i];
+                    if (nx < 0 || ny < 0 || nx >= m || ny >= n || matrix[nx][ny] <= matrix[x][y]) {
+                        continue;
+                    }
+                    ++degree[x][y];
+                }
+                if (!degree[x][y]) {
+                    q.emplace(x, y);
+                }
+            }
+        }
+        int ans = 0;
+        while (!q.empty()) {
+            ++ans;
+            for (auto i = (int) q.size(); i > 0; --i) {
+                auto t = q.front();
+                q.pop();
+                auto x = t.first;
+                auto y = t.second;
+                for (int j = 0; j < 4; ++j) {
+                    auto nx = x + dx[j];
+                    auto ny = y + dy[j];
+                    if (nx < 0 || ny < 0 || nx >= m || ny >= n || matrix[nx][ny] >= matrix[x][y]) {
+                        continue;
+                    }
+                    --degree[nx][ny];
+                    if (!degree[nx][ny]) {
+                        q.emplace(nx, ny);
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+/*
+class Solution {
+    // DFS，记忆化搜索严格递增序列，类似AcWing 901
 public:
     int longestIncreasingPath(const vector<vector<int>> &matrix) {
         const int m = (int) matrix.size();
         const int n = (int) matrix[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        int result = -1;
-        const int dx[] = {0, 0, 1, -1};
-        const int dy[] = {1, -1, 0, 0};
+        vector<vector<int>> f(m, vector<int>(n, -1));
+        int ans = -1;
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
-                result = max(result, dfs(i, j, matrix, dp, dx, dy));
+                ans = max(ans, dfs(i, j, matrix, f));
             }
         }
-        return result;
+        return ans;
     }
 
 private:
+    const int dx[4] = {0, 0, 1, -1};
+    const int dy[4] = {1, -1, 0, 0};
+
     int dfs(const int &x,
             const int &y,
             const vector<vector<int>> &matrix,
-            vector<vector<int>> &dp,
-            const int dx[],
-            const int dy[]) {
-        if (dp[x][y] != -1) {
-            return dp[x][y];
+            vector<vector<int>> &f) {
+        if (f[x][y] != -1) {
+            return f[x][y];
         }
-        dp[x][y] = 1;
+        f[x][y] = 1;
         for (int i = 0; i < 4; ++i) {
             auto nx = x + dx[i];
             auto ny = y + dy[i];
             if (nx < 0 || nx >= matrix.size() || ny < 0 || ny >= matrix[0].size() || matrix[nx][ny] <= matrix[x][y]) {
                 continue;
             }
-            dp[x][y] = max(dp[x][y], dfs(nx, ny, matrix, dp, dx, dy) + 1);
+            f[x][y] = max(f[x][y], dfs(nx, ny, matrix, f) + 1);
         }
-        return dp[x][y];
+        return f[x][y];
     }
 };
+*/
 
 #endif //LEETCODESOLUTIONSINCPP_PROBLEM0329_H
