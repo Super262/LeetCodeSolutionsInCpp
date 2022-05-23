1cad89d28189817e52034d2cbbd54d89b032f517
fengwei <virusformail@gmail.com>
2022-05-23 08:40:40 -0700

    Reviewed Problem 331.

--- Problem0331.h
+++ Problem0331.h
@@ -10,6 +10,8 @@
 using namespace std;
 
 class Solution {
+    // 将输入序列看作二叉树，先处理根结点，再处理左右子树，每次只处理一个节点
+    // 为简化处理，我们在输入序列的尾部补充分隔符"，"
 public:
     bool isValidSerialization(string preorder) {
         preorder.push_back(',');  // 添加分隔符，标识结尾
@@ -29,7 +31,7 @@ private:
             idx += 2;  // 跳过‘#’和分隔符
             return true;
         }
-        while (s[idx] != ',') {
+        while (s[idx] != ',') {  // 跳过当前节点和分隔符
             ++idx;
         }
         ++idx;
