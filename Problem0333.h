ee6c3ee83a2f2d3319061979e5234de77e6ed260
fengwei <virusformail@gmail.com>
2022-05-23 10:28:42 -0700

    Reviewed Problem 333.

--- Problem0333.h
+++ Problem0333.h
@@ -6,22 +6,12 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0333_H
 
 #include <algorithm>
+#include "treenode.h"
 
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
 class Solution {
+    // 类似后序遍历
 public:
     int largestBSTSubtree(TreeNode *root) {
         auto res = dfs(root);
