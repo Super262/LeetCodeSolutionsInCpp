677a3d85893b44633eeefbf9af640b2879aa7e9a
fengwei <virusformail@gmail.com>
2022-05-23 18:21:18 -0700

    Reviewed Problem 341.

--- Problem0341.h
+++ Problem0341.h
@@ -6,52 +6,48 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0341_H
 
 #include <vector>
-#include <queue>
+#include <stack>
+#include <algorithm>
+#include "nestedinteger.h"
 
 using namespace std;
 
-class NestedInteger {
-public:
-    // Return true if this NestedInteger holds a single integer, rather than a nested list.
-    bool isInteger() const;
-
-    // Return the single integer that this NestedInteger holds, if it holds a single integer
-    // The result is undefined if this NestedInteger holds a nested list
-    int getInteger() const;
-
-    // Return the nested list that this NestedInteger holds, if it holds a nested list
-    // The result is undefined if this NestedInteger holds a single integer
-    const vector<NestedInteger> &getList() const;
-};
-
 class NestedIterator {
+    // 巧妙做法：用栈模拟DFS过程，保存当前位置和末尾位置（迭代器变量）
 public:
-    NestedIterator(vector<NestedInteger> &nestedList) {
-        flatten(nestedList);
+    NestedIterator(vector<NestedInteger> &nested_list) {
+        stk.emplace(nested_list.begin(), nested_list.end());
     }
 
     int next() {
-        auto t = data.front();
-        data.pop();
-        return t;
+        // 由于保证调用 next 之前会调用 hasNext，直接返回栈顶列表的当前元素，然后迭代器指向下一个元素
+        auto &p = stk.top();
+        auto ans = p.first->getInteger();
+        p.first++;
+        return ans;
     }
 
     bool hasNext() {
-        return !data.empty();
+        while (!stk.empty()) {
+            auto &p = stk.top();
+            if (p.first == p.second) { // 遍历到当前列表末尾，出栈
+                stk.pop();
+                continue;
+            }
+            if (p.first->isInteger()) {
+                return true;
+            }
+            // 若当前元素为列表，则将其入栈，且迭代器指向下一个元素
+            auto &lst = p.first->getList();
+            stk.emplace(lst.begin(), lst.end());
+            p.first++;
+        }
+        return false;
     }
 
 private:
-    queue<int> data;
-
-    void flatten(const vector<NestedInteger> &nestedList) {
-        for (const auto &ni: nestedList) {
-            if (ni.isInteger()) {
-                data.emplace(ni.getInteger());
-            } else {
-                flatten(ni.getList());
-            }
-        }
-    }
+    // pair 中存储的是列表的当前遍历位置，以及一个尾后迭代器用于判断是否遍历到了列表末尾
+    stack<pair<vector<NestedInteger>::iterator, vector<NestedInteger>::iterator>> stk;
 };
 
 /**
--- nestedinteger.h
+++ nestedinteger.h
@@ -32,7 +32,7 @@ public:
 
     // Return the nested list that this NestedInteger holds, if it holds a nested list
     // The result is undefined if this NestedInteger holds a single integer
-    const vector<NestedInteger> &getList() const;
+    vector<NestedInteger> &getList() const;
 };
 
 #endif //LEETCODESOLUTIONSINCPP_NESTEDINTEGER_H
