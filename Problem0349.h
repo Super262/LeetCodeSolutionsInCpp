312b02ca8792caf9558b5f88323e69759090be80
fengwei <virusformail@gmail.com>
2022-05-23 23:53:24 -0700

    Reviewed Problem 349.

--- Problem0349.h
+++ Problem0349.h
@@ -11,18 +11,19 @@
 using namespace std;
 
 class Solution {
+    // 统计nums1中的数字种类；遍历nums2，查找交集
 public:
     vector<int> intersection(const vector<int> &nums1, const vector<int> &nums2) {
-        vector<int> result;
+        vector<int> ans;
         unordered_set<int> unique_nums(nums1.begin(), nums1.end());
         for (const auto &x: nums2) {
             if (!unique_nums.count(x)) {
                 continue;
             }
-            result.push_back(x);
+            ans.push_back(x);
             unique_nums.erase(x);
         }
-        return result;
+        return ans;
     }
 };
 
