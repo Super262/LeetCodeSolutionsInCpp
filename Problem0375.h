50d090041321be5200663bc0f5d77dffdaee8391
fengwei <virusformail@gmail.com>
2022-05-25 14:26:05 -0700

    Reviewed Problem 375.

--- Problem0375.h
+++ Problem0375.h
@@ -11,21 +11,22 @@
 using namespace std;
 
 class Solution {
-    // dp[i][j]：目标值 i <= x <= j 时，所有猜法中最坏情况的最小值
+    // f[i][j]：目标值 i <= x <= j 时，所有猜法中最坏情况的最小值
+    // 区间DP，从小到大枚举区间长度；在选k的前提条件下，最坏情况所用钱数为max(f[i,k-1],f[k+1,j])+k
 public:
     int getMoneyAmount(const int &n) {
-        int dp[n + 2][n + 2];
-        memset(dp, 0, sizeof dp);  // 注意，初始值为0：区间[i:j]中有1个或更少的数时（i >= j），代价为0
+        int f[n + 2][n + 2];
+        memset(f, 0, sizeof f);  // 注意，初始值为0：区间[i:j]中有1个或更少的数时（i >= j），代价为0
         for (int len = 2; len <= n; ++len) {
             for (int st = 1; st + len - 1 <= n; ++st) {
                 auto ed = st + len - 1;
-                dp[st][ed] = INT_MAX;
+                f[st][ed] = INT_MAX;
                 for (int k = st; k <= ed; ++k) {
-                    dp[st][ed] = min(dp[st][ed], max(dp[st][k - 1], dp[k + 1][ed]) + k);
+                    f[st][ed] = min(f[st][ed], max(f[st][k - 1], f[k + 1][ed]) + k);
                 }
             }
         }
-        return dp[1][n];
+        return f[1][n];
     }
 };
 
