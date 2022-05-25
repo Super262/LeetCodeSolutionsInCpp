1b5b994a9476c278f3afa427eea61fa190bc0166
fengwei <virusformail@gmail.com>
2022-05-25 14:36:29 -0700

    Reviewed Problem 376.

--- Problem0376.h
+++ Problem0376.h
@@ -11,19 +11,20 @@ using namespace std;
 
 class Solution {
     // 贪心算法，直接背诵！
+    // 先清除连续的重复元素，再统计满足条件的三元组（nums[i-1]>nums[i]<nums[i+1] 或 nums[i-1]<nums[i]>nums[i+1]）
 public:
     int wiggleMaxLength(vector<int> &nums) {
         nums.erase(unique(nums.begin(), nums.end()), nums.end());  // 清除连续的重复元素
         if (nums.size() < 2) {
             return (int) nums.size();
         }
-        int res = 2;
+        int ans = 2;
         for (int i = 1; i + 1 < (int) nums.size(); ++i) {
             if ((nums[i - 1] > nums[i] && nums[i + 1] > nums[i]) || (nums[i - 1] < nums[i] && nums[i + 1] < nums[i])) {
-                ++res;
+                ++ans;
             }
         }
-        return res;
+        return ans;
     }
 };
 
