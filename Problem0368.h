c27d65253c1628efcc5f8af42a07cf22941db863
fengwei <virusformail@gmail.com>
2022-05-25 11:20:42 -0700

    Reviewed Problem 368.

--- Problem0368.h
+++ Problem0368.h
@@ -11,37 +11,39 @@
 using namespace std;
 
 class Solution {
-    // 转化成LIS问题：先排序
+    // 我们将原问题转化为LIS问题：若不减序列nums任意项nums[i]满足nums[i]%nums[i-1]=0，由序列中元素构成的集合满足题目要求
+    // f[i]表示以nums[i]结尾的、满足题目要求的集合的最大长度
 public:
     vector<int> largestDivisibleSubset(vector<int> &nums) {
         sort(nums.begin(), nums.end());
         const auto n = (int) nums.size();
-        int dp[n];
-        int ans = 0;
+        int f[n];
+        int last_idx = 0;
         for (int i = 0; i < n; ++i) {
-            dp[i] = 1;
+            f[i] = 1;
             for (int j = 0; j < i; ++j) {
                 if (nums[i] % nums[j] != 0) {
                     continue;
                 }
-                dp[i] = max(dp[j] + 1, dp[i]);
+                f[i] = max(f[j] + 1, f[i]);
             }
-            if (dp[ans] < dp[i]) {
-                ans = i;
+            if (f[last_idx] < f[i]) {
+                last_idx = i;
             }
         }
-        vector<int> result(1, nums[ans]);
-        while (dp[ans] > 1) {
-            for (int i = 0; i < ans; ++i) {
-                if (dp[i] + 1 != dp[ans] || nums[ans] % nums[i] != 0) {
+        vector<int> ans;
+        ans.emplace_back(nums[last_idx]);
+        while (f[last_idx] > 1) {
+            for (int i = 0; i < last_idx; ++i) {
+                if (f[i] + 1 != f[last_idx] || nums[last_idx] % nums[i] != 0) {
                     continue;
                 }
-                result.push_back(nums[i]);
-                ans = i;
+                ans.push_back(nums[i]);
+                last_idx = i;
                 break;
             }
         }
-        return result;
+        return ans;
     }
 };
 
