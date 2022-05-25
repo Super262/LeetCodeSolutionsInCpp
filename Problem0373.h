6339775111643e26530165e77bee841e761b9b3f
fengwei <virusformail@gmail.com>
2022-05-25 14:10:21 -0700

    Reviewed Problem 373.

--- Problem0373.h
+++ Problem0373.h
@@ -16,7 +16,7 @@ public:
     vector<vector<int>> kSmallestPairs(const vector<int> &a, const vector<int> &b, const int &k) {
         const auto &n = (int) a.size();
         const auto &m = (int) b.size();
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> heap;
+        priority_queue<Node, vector<Node>, nodeGreater> heap;
         for (int i = 0; i < m; ++i) {
             heap.push({a[0] + b[i], 0, i});
         }
@@ -24,13 +24,26 @@ public:
         while (!heap.empty() && (int) ans.size() < k) {
             auto t = heap.top();
             heap.pop();
-            ans.push_back({a[t[1]], b[t[2]]});
-            if (t[1] + 1 < n) {
-                heap.push({a[t[1] + 1] + b[t[2]], t[1] + 1, t[2]});
+            ans.push_back({a[t.p1], b[t.p2]});
+            if (t.p1 + 1 < n) {
+                heap.push({a[t.p1 + 1] + b[t.p2], t.p1 + 1, t.p2});
             }
         }
         return ans;
     }
+
+private:
+    struct Node {
+        int sum;
+        int p1;
+        int p2;
+    };
+
+    struct nodeGreater {
+        bool operator()(const Node &a, const Node &b) {
+            return a.sum > b.sum;
+        }
+    };
 };
 
 #endif //LEETCODESOLUTIONSINCPP_PROBLEM0373_H
