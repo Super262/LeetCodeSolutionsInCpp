372cda230c4829e41dddecc5ea801b8eb3b57550
fengwei <virusformail@gmail.com>
2022-05-25 17:22:12 -0700

    Reviewed Problem 385.

--- Problem0386.h
+++ Problem0386.h
@@ -10,6 +10,27 @@
 using namespace std;
 
 class Solution {
+    // DFS，模拟对字典树的搜索，无需真正建立字典树
+    // 进一步优化：转递归为迭代，空间降至O(1)
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> ans;
+        for (int i = 0, num = 1; i < n; ++i) {
+            ans.emplace_back(num);
+            if (num * 10 <= n) {
+                num *= 10;
+            } else {
+                while (num % 10 == 9 || num + 1 > n) {
+                    num /= 10;
+                }
+                ++num;
+            }
+        }
+        return ans;
+    }
+};
+
+/*class Solution {
     // 使用DFS模拟对字典树的搜索，无需真正建立字典树
 public:
     vector<int> lexicalOrder(const int &n) {
@@ -25,7 +46,7 @@ private:
         if (idx > n) {
             return;
         }
-        answer.push_back(idx);
+        answer.emplace_back(idx);
         auto temp = idx * 10;
         for (int i = 0; i <= 9; ++i) {
             temp += i;
@@ -33,6 +54,6 @@ private:
             temp -= i;
         }
     }
-};
+};*/
 
 #endif //LEETCODESOLUTIONSINCPP_PROBLEM0386_H
