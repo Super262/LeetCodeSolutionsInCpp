e925c90a2f70c92db6262716f24da599352fb598
fengwei <virusformail@gmail.com>
2022-05-26 09:56:12 -0700

    Reviewed Problem 393.

--- Problem0393.h
+++ Problem0393.h
@@ -10,21 +10,23 @@
 using namespace std;
 
 class Solution {
+    // 获取每个起点数字的高位，统计"1"的个数
+    // 细节：注意判定无效情况（起点高位有1个"1"，或超过4个"1"）
 public:
     bool validUtf8(const vector<int> &data) {
         for (int i = 0; i < (int) data.size(); ++i) {
-            if (getBit(data[i], 0) == 0) {  // 1-byte character
+            if (bitAt(data[i], 0) == 0) {  // 1-byte character
                 continue;
             }
             int k = 0;
-            while (getBit(data[i], k) && k <= 4) {
+            while (bitAt(data[i], k) && k <= 4) {
                 ++k;
             }
             if (k > 4 || k == 1 || i + k - 1 >= (int) data.size()) {  // 不要忘记这个判断
                 return false;
             }
             for (int j = i + 1; j <= i + k - 1; ++j) {
-                if (getBit(data[j], 0) != 1 || getBit(data[j], 1) != 0) {
+                if (bitAt(data[j], 0) != 1 || bitAt(data[j], 1) != 0) {
                     return false;
                 }
             }
@@ -34,7 +36,7 @@ public:
     }
 
 private:
-    int getBit(int x, int k) {
+    int bitAt(int x, int k) {  // 获取第k位（从高位开始）
         return (x >> (7 - k)) & 1;
     }
 };
