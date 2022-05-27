a0cb4b9d607a63a93380d55b0644f436ae913f06
fengwei <virusformail@gmail.com>
2022-05-27 10:06:45 -0700

    Reviewed Problem 404.

--- Problem0404.h
+++ Problem0404.h
@@ -5,20 +5,10 @@
 #ifndef LEETCODESOLUTIONSINCPP_PROBLEM0404_H
 #define LEETCODESOLUTIONSINCPP_PROBLEM0404_H
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
+#include "treenode.h"
 
 class Solution {
+    // 类似先序遍历，判断当前节点的左儿子是否为叶节点
 public:
     int sumOfLeftLeaves(TreeNode *root) {
         int answer = 0;
