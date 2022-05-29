95a3fd9a9fbd605da9680de4bdfac0b1668b8f74
fengwei <virusformail@gmail.com>
2022-05-29 01:40:14 -0700

    Reviewed Problem 413.

--- Problem0413.h
+++ Problem0413.h
@@ -11,27 +11,29 @@
 using namespace std;
 
 class Solution {
+    // 差分数组的应用，设diff[i]=nums[i]-nums[i-1]，我们要统计长度至少为3的子数组nums[i:j]的个数，满足diff[i+1:j]内元素相等
+    // 长度为k的数组，有(1+2+...+(k-1))个长度不小于2的子数组
 public:
     int numberOfArithmeticSlices(const vector<int> &nums) {
-        auto n = (int) nums.size();
-        int dif[n];  // 差分数组
-        memset(dif, 0, sizeof dif);
-        dif[0] = nums[0];
+        const auto n = (int) nums.size();
+        int diff[n];  // 差分数组
+        memset(diff, 0, sizeof diff);
+        diff[0] = nums[0];
         for (auto i = n - 1; i >= 1; --i) {
-            dif[i] = nums[i] - nums[i - 1];
+            diff[i] = nums[i] - nums[i - 1];
         }
-        int res = 0;
-        // 注意：若原数组[i:j]是等差数列，则差分数组[i + 1:j]所有元素相等
+        int ans = 0;
+        // 若原数组[i:j]是等差数列，则差分数组[i + 1:j]所有元素相等
         for (int l = 1; l < n; ++l) {
             auto r = l;
-            while (r < n && dif[r] == dif[l]) {
+            while (r < n && diff[r] == diff[l]) {
                 ++r;
             }
             auto k = r - l;
-            res += k * (k - 1) / 2;  // 长度为k的等差数列，有(1 + 2 + ... + k - 1)个长度不小于2的等差子数列
+            ans += k * (k - 1) / 2;
             l = r - 1;
         }
-        return res;
+        return ans;
     }
 };
 
