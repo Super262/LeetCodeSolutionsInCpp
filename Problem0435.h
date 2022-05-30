04faa55e1c4f28942317e722b30c4741f24400b0
fengwei <virusformail@gmail.com>
2022-05-30 01:53:15 -0700

    Reviewed Problem 435.

--- Problem0435.h
+++ Problem0435.h
@@ -11,21 +11,22 @@
 using namespace std;
 
 class Solution {
+    // 依照右端点，从小到大排序所有区间：再从前向后遍历所有区间，统计不相交区间的个数ans
     // 经典贪心算法，理解并背诵
 public:
     int eraseOverlapIntervals(vector<vector<int>> &intervals) {
-        sort(intervals.begin(), intervals.end(), [](const vector<int> &a, const vector<int> &b) {
+        sort(intervals.begin(), intervals.end(), [&](const vector<int> &a, const vector<int> &b) {
             return a[1] < b[1];
         });
-        int res = 1;
+        int ans = 1;
         int right = intervals[0][1];
         for (int i = 1; i < intervals.size(); ++i) {
             if (intervals[i][0] >= right) {
-                ++res;
+                ++ans;
                 right = intervals[i][1];
             }
         }
-        return (int) intervals.size() - res;
+        return (int) intervals.size() - ans;
     }
 };
 
