f8f679c199ca196dcca52d16bf08424d9145161c
fengwei <virusformail@gmail.com>
2022-05-30 15:24:04 -0700

    Reviewed Problem 442.

--- Problem0442.h
+++ Problem0442.h
@@ -11,18 +11,18 @@
 using namespace std;
 
 class Solution {
-    // 直接背诵代码
+    // 技巧：设p是nums中任意数字的绝对值，设置num[p-1]=-nums[p-1]；若数字x重复出现，nums[x-1]是正数（负负为正）
 public:
     vector<int> findDuplicates(vector<int> &nums) {
-        vector<int> result;
+        vector<int> ans;
         for (const auto &x: nums) {
-            auto p = abs(x) - 1;
-            nums[p] = -nums[p];
-            if (nums[p] > 0) {
-                result.emplace_back(p + 1);
+            auto p = abs(x);
+            nums[p - 1] = -nums[p - 1];
+            if (nums[p - 1] > 0) {
+                ans.emplace_back(p);
             }
         }
-        return result;
+        return ans;
     }
 };
 
