40a6de643c57f1041b7a09832e28f312d8a04b66
fengwei <virusformail@gmail.com>
2022-05-30 01:22:08 -0700

    Reviewed Problem 432.

--- Problem0432.h
+++ Problem0432.h
@@ -12,7 +12,9 @@
 using namespace std;
 
 class AllOne {
-    // 双链表保存key的频率：1 -> 2 -> 3 -> ...
+    // 双链表+集合+哈希表：双链表的每个节点用集合pool存储所有频率为value的key；从前到后每个节点代表的频率递增；哈希表storage存储"key-双链表节点"映射
+    // 若某个key的频率增加/减少，我们从storage[key].pool中清除key，将key插入storage[key]后面/前面、频率为storage[key].value(+/-)1的节点
+    // 若某个节点node的集合node.pool为空，我们将node从双链表中移除
 public:
     AllOne() {
         head = new Node(INT_MIN);
@@ -22,15 +24,15 @@ public:
     }
 
     void inc(const string &key) {
-        if (storage.count(key) == 0) {
+        if (!storage.count(key)) {
             storage[key] = addToNext(head, key, 1);
-        } else {
-            auto node = storage[key];
-            node->pool.erase(key);
-            storage[key] = addToNext(node, key, node->value + 1);
-            if (node->pool.empty()) {
-                deleteNode(node);
-            }
+            return;
+        }
+        auto node = storage[key];
+        node->pool.erase(key);
+        storage[key] = addToNext(node, key, node->value + 1);
+        if (node->pool.empty()) {
+            deleteNode(node);
         }
     }
 
@@ -82,7 +84,7 @@ private:
     Node *tail;
     unordered_map<string, Node *> storage;
 
-    Node *addToNext(Node *node, const string &key, int val) {
+    Node *addToNext(Node *node, const string &key, int val) {  // 将key插入到node后面、代表频率为val的节点的pool内
         if (node->next->value == val) {
             node->next->pool.insert(key);
         } else {
@@ -96,7 +98,7 @@ private:
         return node->next;
     }
 
-    Node *addToPrev(Node *node, const string &key, int val) {
+    Node *addToPrev(Node *node, const string &key, int val) {  // 将key插入到node前面、代表频率为val的节点的pool内
         if (node->prev->value == val) {
             node->prev->pool.insert(key);
         } else {
@@ -117,5 +119,14 @@ private:
         node->next = nullptr;
     }
 };
+
+/**
+ * Your AllOne object will be instantiated and called as such:
+ * AllOne* obj = new AllOne();
+ * obj->inc(key);
+ * obj->dec(key);
+ * string param_3 = obj->getMaxKey();
+ * string param_4 = obj->getMinKey();
+ */
 
 #endif //LEETCODESOLUTIONSINCPP_PROBLEM0432_H
