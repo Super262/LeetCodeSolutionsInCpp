56f18ea637710f570541b83e9b478fd26190d174
fengwei <virusformail@gmail.com>
2022-05-29 18:04:57 -0700

    Reviewed Problem 430.

--- Problem0430.h
+++ Problem0430.h
@@ -6,46 +6,41 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0430_H
 
 #include <vector>
+#include "double_node.h"
 
 using namespace std;
 
-class Node {
-public:
-    int val;
-    Node *prev;
-    Node *next;
-    Node *child;
-};
-
 class Solution {
+    // 递归解析，返回解析后得到的头节点和尾节点
 public:
     Node *flatten(Node *head) {
+        if (!head) {
+            return nullptr;
+        }
         auto res = dfs(head);
         return res.first;
     }
 
 private:
     pair<Node *, Node *> dfs(Node *head) {  // 由于是双链表，所以头节点和尾节点都要被返回
-        if (!head) {
-            return {nullptr, nullptr};
-        }
         auto current = head;
         auto tail = head;
         while (current) {
             tail = current;
-            if (current->child) {
-                auto t = dfs(current->child);  // 递归解析
-                current->child = nullptr;  // 别忘记清除child指针
-                t.second->next = current->next;  // 接合链表
-                if (current->next) {
-                    current->next->prev = t.second;
-                }
-                current->next = t.first;
-                t.first->prev = current;
-                current = t.second;
-                tail = current;
+            if (!current->child) {
+                current = current->next;
+                continue;
             }
-            current = current->next;
+            auto t = dfs(current->child);
+            current->child = nullptr;  // 别忘记清除child指针
+            t.second->next = current->next;  // 接合链表
+            if (current->next) {
+                current->next->prev = t.second;
+            }
+            current->next = t.first;
+            t.first->prev = current;
+            tail = t.second;
+            current = tail->next;
         }
         return {head, tail};
     }
---
+++ double_node.h
@@ -0,0 +1,16 @@
+//
+// Created by Fengwei Zhang on 5/29/22.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_DOUBLE_NODE_H
+#define LEETCODESOLUTIONSINCPP_DOUBLE_NODE_H
+
+class Node {
+public:
+    int val;
+    Node *prev;
+    Node *next;
+    Node *child;
+};
+
+#endif //LEETCODESOLUTIONSINCPP_DOUBLE_NODE_H
