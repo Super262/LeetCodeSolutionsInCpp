8257e34ba9079f964d1fd060b761d8df6d0bc1e2
fengwei <virusformail@gmail.com>
2022-05-30 18:28:12 -0700

    Reviewed Problem 447.

--- Problem0447.h
+++ Problem0447.h
@@ -11,12 +11,14 @@
 using namespace std;
 
 class Solution {
+    // 对每个点points[i]，统计其它所有点与points[i]的距离的频率；我们可任选2个点points[j]、points[k]，它们与points[i]的距离相等
+    // 因此，对于points[i]，若距离d的频率是f，答案增加f*(f-1)
 public:
     int numberOfBoomerangs(const vector<vector<int>> &points) {
-        int res = 0;
-        for (int i = 0; i < points.size(); ++i) {
+        int ans = 0;
+        for (int i = 0; i < (int) points.size(); ++i) {
             unordered_map<int, int> counter;
-            for (int j = 0; j < points.size(); ++j) {
+            for (int j = 0; j < (int) points.size(); ++j) {
                 if (j == i) {
                     continue;
                 }
@@ -24,11 +26,11 @@ public:
                 auto dy = points[j][1] - points[i][1];
                 ++counter[dx * dx + dy * dy];
             }
-            for (auto item: counter) {
-                res += item.second * (item.second - 1);
+            for (const auto &item: counter) {
+                ans += item.second * (item.second - 1);
             }
         }
-        return res;
+        return ans;
     }
 };
 
