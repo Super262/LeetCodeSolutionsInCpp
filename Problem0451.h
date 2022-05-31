7805af5150f6af6ddb8100aca3832c5e1808d5c8
fengwei <virusformail@gmail.com>
2022-05-31 02:59:14 -0700

    Reviewed Problem 451.

--- Problem0451.h
+++ Problem0451.h
@@ -6,27 +6,38 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0451_H
 
 #include <string>
+#include <vector>
 #include <unordered_map>
-#include <algorithm>
 
 using namespace std;
 
 class Solution {
-    // 熟练使用Lambda表达式
+    // "桶排序"：先统计每种字符的频率，获得最大频率mf；根据频率，分桶0～mf存放字符；从0到mf，从每个桶取出对应个数的字符并将其加入结果
 public:
-    string frequencySort(string s) {
-        unordered_map<char, int> freq;
-        for (const auto &ch: s) {
-            ++freq[ch];
+    string frequencySort(const string &s) {
+        unordered_map<char, int> ch_freq;
+        unordered_map<int, vector<char>> bucket;
+        for (const auto &x: s) {  // 统计每种字符的频率
+            ++ch_freq[x];
         }
-        sort(s.begin(), s.end(), [&](char a, char b) {  // [&]表示外部变量以引用方式传入
-            if (freq[a] != freq[b]) {
-                return freq[a] > freq[b];
-            } else {
-                return a < b;
+        int max_freq = 0;
+        for (const auto &item: ch_freq) {  // 分桶存放字符
+            max_freq = max(max_freq, item.second);
+            bucket[item.second].emplace_back(item.first);
+        }
+        string ans;
+        ans.reserve(s.size());
+        for (auto f = max_freq; f > 0; --f) {
+            if (!bucket.count(f)) {
+                continue;
             }
-        });
-        return s;
+            for (const auto &ch: bucket[f]) {
+                for (int k = 0; k < f; ++k) {
+                    ans += ch;
+                }
+            }
+        }
+        return ans;
     }
 };
 
