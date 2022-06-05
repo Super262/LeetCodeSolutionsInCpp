c38e85266f95689b237a385c67114e513bcb8794
fengwei <virusformail@gmail.com>
2022-06-05 02:51:21 -0700

    Reviewed Problem 456.

--- Problem0456.h
+++ Problem0456.h
@@ -11,7 +11,9 @@
 using namespace std;
 
 class Solution {
-    // 直接掌握思路：利用单调栈记录之前元素的状态
+    // 贪心算法：从后向前遍历数组nums，变量pre_max存储当前nums[k]的最大值，栈stk存储nums[k]的候选值，栈顶是当前nums[j]的最小值
+    // 遍历到当前元素nums[i]时，弹出stk中较小元素，并更新pre_max（当前nums[k]的最大值），再将当前元素入栈
+    // 因此，pre_max < stk.top() 始终成立，若当前数字 x < pre_max 也成立，说明"132模式"存在
 public:
     bool find132pattern(const vector<int> &nums) {
         stack<int> stk;
