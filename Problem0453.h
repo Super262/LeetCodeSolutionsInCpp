0b64e91a2752664acbfce466ac3ce3cfca6d5aae
fengwei <virusformail@gmail.com>
2022-06-05 01:42:18 -0700

    Reviewed Problem 453.

--- Problem0453.h
+++ Problem0453.h
@@ -10,18 +10,18 @@
 using namespace std;
 
 class Solution {
-    // 为n-1个元素加1，等价于给1个元素减1：找到最小值minv，求其它元素和它的距离
+    // 为n-1个元素加1，等价于给1个元素减1；因此，我们找到最小值minv，求其它元素和它的距离
 public:
     int minMoves(const vector<int> &nums) {
-        int res = 0;
-        auto minv = INT_MAX;
+        int ans = 0;
+        int minv = INT_MAX;
         for (const auto &x: nums) {
             minv = min(minv, x);
         }
         for (const auto &x: nums) {
-            res += x - minv;
+            ans += x - minv;
         }
-        return res;
+        return ans;
     }
 };
 
