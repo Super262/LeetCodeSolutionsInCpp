ca4e162006c5f957b60886c3ccdd7a5aa7283783
fengwei <virusformail@gmail.com>
2022-06-05 08:47:38 -0700

    Reviewed Problem 458.

--- Problem0458.h
+++ Problem0458.h
@@ -11,9 +11,8 @@ using namespace std;
 
 class Solution {
     // 设k=minutesToTest/minutesToDie，我们最多可以给所有的x头猪喂k轮水
-    // 每头猪有(k+1)种状态：0表示活着，k表示在第k轮喝水后死去。我们依次用编号为1～n的水喂编号为0～x-1的猪
-    // 由x头猪的状态组成的(k+1)进制数的十进制表示即为有毒水桶的编号。因此(k+1)^x >= n，x=log(k+1,n)
-
+    // 由x头猪的状态组成的(k+1)进制数是最大的信息量。我们应使(k+1)^x >= n，x=log(k+1,n)
+    // 那么，如何利用这最大的信息量，举例：https://leetcode.cn/problems/poor-pigs/solution/hua-jie-suan-fa-458-ke-lian-de-xiao-zhu-by-guanpen/
 public:
     int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
         auto k = minutesToTest / minutesToDie;
