33779183710e5c761db9494e69a868cd9630790c
fengwei <virusformail@gmail.com>
2022-06-08 00:51:56 -0700

    Reviewed Problem 474.

--- Problem0474.h
+++ Problem0474.h
@@ -12,12 +12,15 @@
 using namespace std;
 
 class Solution {
+    // 转化原题为"二维背包"，类似AcWing 1020
+    // f[i][j]=k表示含"0"数不超过i、含"1"数不超过j时，最多可选择k个单词
 public:
     int findMaxForm(const vector<string> &strs, int m, int n) {
-        int dp[m + 1][n + 1];
-        memset(dp, 0, sizeof dp);
+        int f[m + 1][n + 1];
+        memset(f, 0, sizeof f);
         for (const auto &s: strs) {
-            int a = 0, b = 0;
+            int a = 0;
+            int b = 0;
             for (const auto &ch: s) {
                 if (ch == '0') {
                     ++a;
@@ -27,11 +30,11 @@ public:
             }
             for (int i = m; i >= a; --i) {
                 for (int j = n; j >= b; --j) {
-                    dp[i][j] = max(dp[i][j], dp[i - a][j - b] + 1);
+                    f[i][j] = max(f[i][j], f[i - a][j - b] + 1);
                 }
             }
         }
-        return dp[m][n];
+        return f[m][n];
     }
 };
 
