94ce371606079045e613be55d993e2121b117afe
fengwei <virusformail@gmail.com>
2022-06-10 02:06:27 -0700

    Reviewed Problem 486.

--- Problem0486.h
+++ Problem0486.h
@@ -11,8 +11,10 @@
 using namespace std;
 
 class Solution {
+    // 从最简单的问题开始考虑，假设只有一个数字，则只能玩家 A 选择这个数字；接着，问题的规模开始扩大
+    // 扩大后，两个玩家会有两种决策，一种是选择数组头部，一种是选择数组尾部，而这两种情况下的子问题都可以提前计算出
+    // 至此，动态规划的思路已经很明显；区间动态规划解决博弈论问题：f[i][j]表示先手玩家与后手玩家的得分差距的最大值
     // https://www.acwing.com/solution/content/392/
-    // 区间动态规划解决博弈论问题：f[i][j]表示先手玩家与后手玩家的得分差距的最大值
 public:
     bool PredictTheWinner(const vector<int> &nums) {
         const auto n = (int) nums.size();
