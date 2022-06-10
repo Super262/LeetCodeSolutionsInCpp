683bc1c521ea1350852d9154838009c7eca636a1
fengwei <virusformail@gmail.com>
2022-06-10 01:44:18 -0700

    Reviewed Problem 485.

--- Problem0485.h
+++ Problem0485.h
@@ -10,21 +10,22 @@
 using namespace std;
 
 class Solution {
+    // 双指针，找到全为"1"的nums[i:j]，使得j-i最大
 public:
     int findMaxConsecutiveOnes(const vector<int> &nums) {
-        int res = 0;
-        for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] == 0) {
+        int ans = 0;
+        for (int i = 0; i < (int) nums.size(); ++i) {
+            if (!nums[i]) {
                 continue;
             }
             auto j = i;
-            while (j < nums.size() && nums[j] == 1) {
+            while (j < (int) nums.size() && nums[j] == 1) {
                 ++j;
             }
-            res = max(j - i, res);
+            ans = max(j - i, ans);
             i = j - 1;
         }
-        return res;
+        return ans;
     }
 };
 
