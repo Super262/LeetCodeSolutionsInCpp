902d7e550281850ef1f2cd38d3bd76c59077a5a3
fengwei <virusformail@gmail.com>
2022-06-12 14:43:46 -0700

    Reviewed Problem 492.

--- Problem0492.h
+++ Problem0492.h
@@ -11,13 +11,14 @@
 using namespace std;
 
 class Solution {
+    // 设area=x^2，我们从大到小、遍历所有可能的W，W的初值为x；若发现可行的W（area%W=0），返回{area/W,W}
 public:
     vector<int> constructRectangle(const int area) {
-        for (auto a = (int) sqrt(area); a >= 1; --a) {
-            if (area % a) {
+        for (auto w = (int) sqrt(area); w >= 1; --w) {
+            if (area % w) {
                 continue;
             }
-            return {area / a, a};
+            return {area / w, w};
         }
         return {area, 1};
     }
