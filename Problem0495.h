432732c8e27f0f59812aa153fcc9233195ef613a
fengwei <virusformail@gmail.com>
2022-06-12 15:15:58 -0700

    Reviewed Problem 495.

--- Problem0495.h
+++ Problem0495.h
@@ -10,21 +10,22 @@
 using namespace std;
 
 class Solution {
+    // 枚举输入序列，根据当前的终点计算答案并更新终点
     // 注意：prev_end初始值为-1，因为有效时刻从0开始
 public:
     int findPoisonedDuration(const vector<int> &timeSeries, int duration) {
-        int result = 0;
+        int ans = 0;
         int prev_end = -1;
         for (const auto &x: timeSeries) {
             auto t = x + duration - 1;
             if (x > prev_end) {
-                result += duration;
+                ans += duration;
             } else {
-                result += duration - (prev_end - x + 1);
+                ans += duration - (prev_end - x + 1);
             }
             prev_end = t;
         }
-        return result;
+        return ans;
     }
 };
 
