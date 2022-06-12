1e136eef6e44a346ce0de95ad5b77c5f8a664df0
fengwei <virusformail@gmail.com>
2022-06-12 16:00:48 -0700

    Reviewed Problem 498.

--- Problem0498.h
+++ Problem0498.h
@@ -10,22 +10,23 @@
 using namespace std;
 
 class Solution {
+    // 直接背诵：枚举对角线编号i，枚举行号j（max(0,i-n+1)<=j<=min(i,m-1)），将mat[j][i-j]加入答案
 public:
     vector<int> findDiagonalOrder(const vector<vector<int>> &mat) {
         if (mat.empty() || mat[0].empty()) {
             return {};
         }
-        const auto n = (int) mat.size();
-        const auto m = (int) mat[0].size();
+        const auto m = (int) mat.size();
+        const auto n = (int) mat[0].size();
         vector<int> ans;
-        ans.reserve(m * n);
-        for (int i = 0; i < n + m - 1; ++i) {
+        ans.reserve(n * m);
+        for (int i = 0; i < m + n - 1; ++i) {
             if (i % 2) {
-                for (auto j = max(0, i - m + 1); j <= min(i, n - 1); ++j) {
+                for (auto j = max(0, i - n + 1); j <= min(i, m - 1); ++j) {
                     ans.emplace_back(mat[j][i - j]);
                 }
             } else {
-                for (auto j = min(i, n - 1); j >= max(0, i - m + 1); --j) {
+                for (auto j = min(i, m - 1); j >= max(0, i - n + 1); --j) {
                     ans.emplace_back(mat[j][i - j]);
                 }
             }
