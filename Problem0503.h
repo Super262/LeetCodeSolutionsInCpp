024d0e7cb2ccfad08579ba287f3b0cf43df1e289
fengwei <virusformail@gmail.com>
2022-06-16 01:41:21 -0700

    Reviewed Problem 503.

--- Problem0503.h
+++ Problem0503.h
@@ -11,11 +11,11 @@
 using namespace std;
 
 class Solution {
-    // 破环成链 + 单调栈
+    // 破环成链（将nums拷贝后，插到原nums尾部） + 单调栈（保证头部大于当前元素x，x对应的答案就是栈头；若栈为空，x对应的答案是-1）
 public:
     vector<int> nextGreaterElements(vector<int> &nums) {
         const auto n = (int) nums.size();
-        vector<int> res(n);
+        vector<int> ans(n);
         stack<int> stk;
         nums.insert(nums.end(), nums.begin(), nums.end());
         for (int i = (int) nums.size() - 1; i >= 0; --i) {
@@ -25,14 +25,14 @@ public:
             }
             if (i < n) {
                 if (stk.empty()) {
-                    res[i] = -1;
+                    ans[i] = -1;
                 } else {
-                    res[i] = stk.top();
+                    ans[i] = stk.top();
                 }
             }
             stk.emplace(x);
         }
-        return res;
+        return ans;
     }
 };
 
