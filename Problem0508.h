0dc0febb336b02970f43a33d0c0621c3a98549e1
fengwei <virusformail@gmail.com>
2022-06-16 18:56:28 -0700

    Reviewed Problem 508.

--- Problem0508.h
+++ Problem0508.h
@@ -7,34 +7,24 @@
 
 #include <vector>
 #include <unordered_map>
+#include "treenode.h"
 
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
 class Solution {
+    // 类似后序遍历：先求出左、右子树的元素和，再计算当前子树的元素和，更新元素和的频率和最大频率；最后，输出频率为最大频率的元素和
 public:
     vector<int> findFrequentTreeSum(TreeNode *root) {
-        vector<int> res;
         int max_freq = 0;
         unordered_map<int, int> freq;
         dfs(root, freq, max_freq);
+        vector<int> ans;
         for (const auto &x: freq) {
             if (x.second == max_freq) {
-                res.emplace_back(x.first);
+                ans.emplace_back(x.first);
             }
         }
-        return res;
+        return ans;
     }
 
 private:
