a237d4ce84f6bb6660fd0d98670b169ec0524a0f
fengwei <virusformail@gmail.com>
2022-06-17 01:45:17 -0700

    Reviewed Problem 517.

--- Problem0517.h
+++ Problem0517.h
@@ -15,7 +15,7 @@ class Solution {
     // 若不存在一个洗衣机，它运送的衣服数量等于答案，则必定是这种情况：洗衣机A运送了一次，洗衣机B不动；然后洗衣机B运送了一次，洗衣机A不动；但这种情况显然可以并行完成
     // 因此，我们可以枚举每一台洗衣机，计算经过它运送的衣服的数量；设当前洗衣机的编号是i，left_sum=sum(machines[0:i-1])，right_sum=sum(machines[i+1:n-1])
     // 故需要经过这台洗衣机从右向左运送的衣服数量为r_to_l=i*avg-left_sum；从左向右运行的衣服数量l_to_r同理
-    // r_2_l+l_2_r就是这台洗衣机的工作量，对每一台洗衣机都这样求和得到工作量，取工作量最大的洗衣机就是答案
+    // r_to_l+l_to_r就是这台洗衣机的工作量，对每一台洗衣机都这样求和得到工作量，取工作量最大的洗衣机就是答案
     // https://www.acwing.com/solution/content/413/
 public:
     int findMinMoves(const vector<int> &machines) {
