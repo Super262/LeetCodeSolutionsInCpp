7f33eb424d0801938d151550c6118f236089339c
fengwei <virusformail@gmail.com>
2022-06-17 00:46:00 -0700

    Reviewed Problem 515.

--- Problem0515.h
+++ Problem0515.h
@@ -8,20 +8,9 @@
 #include <vector>
 #include <queue>
 #include <unordered_map>
+#include "treenode.h"
 
 using namespace std;
-
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
 
 class Solution {
 public:
@@ -50,11 +39,11 @@ public:
             level_maximal[level] = maximal;
             ++level;
         }
-        vector<int> res(level);
+        vector<int> ans(level);
         for (const auto &x: level_maximal) {
-            res[x.first] = x .second;
+            ans[x.first] = x.second;
         }
-        return res;
+        return ans;
     }
 };
 
