2643bcd125a4e753aac75502644e2ef8096766e3
fengwei <virusformail@gmail.com>
2022-06-17 01:00:14 -0700

    Reviewed Problem 516.

--- Problem0516.h
+++ Problem0516.h
@@ -11,25 +11,25 @@
 using namespace std;
 
 class Solution {
-    // 区间DP：dp[i][j]表示s[i:j]段回文子序列最大长度
+    // 区间DP：f[i][j]表示s[i:j]段回文子序列最大长度
 public:
     int longestPalindromeSubseq(const string &s) {
         const auto n = (int) s.size();
-        int dp[n][n];
-        memset(dp, 0, sizeof dp);
+        int f[n][n];
+        memset(f, 0, sizeof f);
         for (int i = 0; i < n; ++i) {
-            dp[i][i] = 1;
+            f[i][i] = 1;
         }
         for (int length = 2; length <= n; ++length) {
             for (int i = 0; i + length - 1 < n; ++i) {
                 auto j = i + length - 1;
                 if (s[i] == s[j]) {
-                    dp[i][j] = max(dp[i][j], dp[i + 1][j - 1] + 2);
+                    f[i][j] = max(f[i][j], f[i + 1][j - 1] + 2);
                 }
-                dp[i][j] = max(dp[i][j], max(dp[i][j - 1], dp[i + 1][j]));
+                f[i][j] = max(f[i][j], max(f[i][j - 1], f[i + 1][j]));
             }
         }
-        return dp[0][n - 1];
+        return f[0][n - 1];
     }
 };
 
