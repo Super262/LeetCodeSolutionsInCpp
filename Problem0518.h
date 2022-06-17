c10e9457f21eca093bfb645c7dab019ff4f5820b
fengwei <virusformail@gmail.com>
2022-06-17 02:03:20 -0700

    Reviewed Problem 518.

--- Problem0518.h
+++ Problem0518.h
@@ -11,17 +11,18 @@
 using namespace std;
 
 class Solution {
+    // 转化为完全背包问题：f[i]表示凑出i的方案数，f[0]=1
 public:
     int change(const int amount, const vector<int> &coins) {
-        int dp[amount + 1];
-        memset(dp, 0, sizeof dp);
-        dp[0] = 1;
+        int f[amount + 1];
+        memset(f, 0, sizeof f);
+        f[0] = 1;
         for (const auto &x: coins) {
             for (int v = x; v <= amount; ++v) {
-                dp[v] += dp[v - x];
+                f[v] += f[v - x];
             }
         }
-        return dp[amount];
+        return f[amount];
     }
 };
 
