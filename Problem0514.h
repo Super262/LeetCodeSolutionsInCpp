23512908c944f6cfd34b8b7b13714c14513d5a99
fengwei <virusformail@gmail.com>
2022-06-17 00:24:00 -0700

    Reviewed Problem 514.

--- Problem0514.h
+++ Problem0514.h
@@ -11,30 +11,32 @@
 using namespace std;
 
 class Solution {
-    // dp[i][j]：拼写出key的前i个字符、ring指示在索引为j的位置时的最少操作步数
+    // f[i][j]：拼写出key的前i个字符、ring指示在索引为j的位置时的最少操作步数
 public:
     int findRotateSteps(const string &ring, const string &key) {
-        int dp[key.size() + 1][ring.size()];
-        memset(dp, 0x3f, sizeof dp);
-        for (int j = 0; j < ring.size(); ++j) {  // 初始化
-            dp[0][j] = j;
+        const auto m = (int) key.size();
+        const auto n = (int) ring.size();
+        int f[m + 1][n];
+        memset(f, 0x3f, sizeof f);
+        for (int j = 0; j < n; ++j) {  // 初始化
+            f[0][j] = j;
         }
-        for (int i = 1; i <= key.size(); ++i) {
-            for (int j = 0; j < ring.size(); ++j) {
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 0; j < n; ++j) {
                 if (key[i - 1] != ring[j]) {
                     continue;
                 }
-                for (int k = 0; k < ring.size(); ++k) {
+                for (int k = 0; k < n; ++k) {
                     auto t = abs(j - k);
-                    dp[i][j] = min(dp[i][j], dp[i - 1][k] + min(t, (int) ring.size() - t) + 1);
+                    f[i][j] = min(f[i][j], f[i - 1][k] + min(t, n - t) + 1);
                 }
             }
         }
-        int res = 0x3f3f3f3f;
-        for (int i = 0; i < ring.size(); ++i) {
-            res = min(res, dp[key.size()][i]);
+        int ans = 0x3f3f3f3f;
+        for (int i = 0; i < n; ++i) {
+            ans = min(ans, f[m][i]);
         }
-        return res;
+        return ans;
     }
 };
 
