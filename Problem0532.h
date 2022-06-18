c0be6b3cf18f503b9d5d7722010ce78ec570f5f1
fengwei <virusformail@gmail.com>
2022-06-18 03:54:05 -0700

    Reviewed Problem 532.

--- Problem0532.h
+++ Problem0532.h
@@ -6,29 +6,31 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0532_H
 
 #include <vector>
-#include <algorithm>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
+    // 哈希表counter统计每种数字的频率；对于数字x，若k为0且counter[x]>1，答案加1；若k>0且counter[x-k]>0，答案加1
 public:
-    int findPairs(vector<int> &nums, int k) {
-        sort(nums.begin(), nums.end());
-        int res = 0;
-        const auto n = (int) nums.size();
-        for (int i = 0, j = 0; i < n; ++i) {
-            // 出现一段相同的数：跳过前面，指向最后（不能指向第一个、跳过后面，这无法处理k=0）
-            while (i + 1 < n && nums[i + 1] == nums[i]) {
-                ++i;
-            }
-            while (j < i && nums[i] - nums[j] > k) {
-                ++j;
-            }
-            if (j < i && nums[i] - nums[j] == k) {
-                ++res;
+    int findPairs(const vector<int> &nums, int k) {
+        unordered_map<int, int> counter;
+        for (const auto &x: nums) {
+            counter[x]++;
+        }
+        int ans = 0;
+        for (const auto &item: counter) {
+            if (k == 0) {
+                if (item.second > 1) {
+                    ++ans;
+                }
+            } else {
+                if (counter.count(item.first - k)) {
+                    ++ans;
+                }
             }
         }
-        return res;
+        return ans;
     }
 };
 
