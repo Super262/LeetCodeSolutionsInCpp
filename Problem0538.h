5fc594406d277363f3b7d3f799ba43af2783d66b
fengwei <virusformail@gmail.com>
2022-06-18 15:52:04 -0700

    Reviewed Problem 538.

--- Problem0538.h
+++ Problem0538.h
@@ -5,22 +5,16 @@
 #ifndef LEETCODESOLUTIONSINCPP_PROBLEM0538_H
 #define LEETCODESOLUTIONSINCPP_PROBLEM0538_H
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
+#include "treenode.h"
 
 class Solution {
-    // 按右、中、左顺序遍历
+    // 根据题意，若当前BST的根结点是u，我们应先遍历u的右子树，获得右子树的和，再更新u->val，再遍历左子树
+    // 因此，我们按右、中、左顺序遍历BST，设置全局变量sum，sum是所有比当前根结点u大的节点的和
 public:
     TreeNode *convertBST(TreeNode *root) {
+        if (!root) {
+            return nullptr;
+        }
         int sum = 0;
         dfs(root, sum);
         return root;
@@ -28,14 +22,15 @@ public:
 
 private:
     void dfs(TreeNode *root, int &sum) {
-        if (!root) {
-            return;
+        if (root->right) {
+            dfs(root->right, sum);
         }
-        dfs(root->right, sum);
         auto x = root->val;
         root->val += sum;
         sum += x;
-        dfs(root->left, sum);
+        if (root->left) {
+            dfs(root->left, sum);
+        }
     }
 };
 
