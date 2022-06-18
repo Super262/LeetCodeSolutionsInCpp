f724537a2ab80ba55df1324b7cb64f93a39cb306
fengwei <virusformail@gmail.com>
2022-06-18 02:14:05 -0700

    Reviewed Problem 527.

--- Problem0527.h
+++ Problem0527.h
@@ -12,20 +12,24 @@
 using namespace std;
 
 class Solution {
+    // 根据题意，我们总结出以下贪心思想：对于任意单词w，我们力求将w缩写到最短，并且w的缩写与其它单词的缩写不同
+    // 若w的最短缩写w[0]+"K"+w[n-1]是唯一的，我们保留此缩写；若发生重复，我们选择尽可能少地暴露w的前缀，生成新缩写
+    // 涉及到字符串的前缀重合问题，我们可以使用Trie解决：设集合S，S中所有单词的最短缩写相同，将S中所有单词加入一个Trie
+    // Trie的每个节点记录它被遍历的频率；对于S中每个单词w，我们查找最小的i，满足w[i]所在节点的频率为1，w的缩写是w[0:i]+"n-i-2"+w[n-1]
 public:
     vector<string> wordsAbbreviation(const vector<string> &words) {
-        unordered_map<string, vector<int>> abbr_words;
+        unordered_map<string, vector<int>> abbr_words;  // abbr_words[s]：最短缩写为s的所有单词
         for (int i = 0; i < (int) words.size(); ++i) {
             abbr_words[abbreviate(words[i], 0)].emplace_back(i);
         }
         vector<string> ans(words.size());
         for (const auto &item: abbr_words) {
-            auto root = new Node();
+            auto root = new Node();  // 为每组单词新建Trie
             for (const auto &i: item.second) {
                 insertWord(root, words[i]);
             }
             for (const auto &i: item.second) {
-                auto j = findLeaf(root, words[i]);
+                auto j = findLeaf(root, words[i]);  // 查找"叶路径"的起点
                 if (j == -1) {
                     return {};
                 }
