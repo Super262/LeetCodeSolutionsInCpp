5a3f2eece120a95782b7c5cde04be7042b1e44b0
fengwei <virusformail@gmail.com>
2022-06-18 02:54:17 -0700

    Reviewed Problem 530.

--- Problem0530.h
+++ Problem0530.h
@@ -6,22 +6,13 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0530_H
 
 #include <algorithm>
+#include "treenode.h"
 
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
 class Solution {
+    // BST中序是有序的，所以最小差值一定出现在中序序列的相邻元素之间；因此，我们采取中序遍历，记录当前点root和上个点prev的差值，适时更新答案
+    // 同LeetCode 783
 public:
     int getMinimumDifference(TreeNode *root) {
         int prev = 0x3f3f3f3f;
