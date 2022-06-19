e25b26cdcbd9bf0bb48c28a6f80d99e5fe41fb44
fengwei <virusformail@gmail.com>
2022-06-19 13:37:26 -0700

    Reviewed Problem 552.

--- Problem0552.h
+++ Problem0552.h
@@ -10,33 +10,33 @@
 using namespace std;
 
 class Solution {
-    // dp[i][a][l]：长度为i的序列中包含a个A、结尾有l个连续的L；从前向后枚举
+    // f[i][a][l]：长度为i的序列中包含a个A、结尾有l个连续的L；从前向后枚举，由f[i]推导f[i+1]
 public:
     int checkRecord(int n) {
         const int M = 1000000007;
-        int dp[n + 1][2][3];
-        memset(dp, 0, sizeof dp);
-        dp[0][0][0] = 1;
+        int f[n + 1][2][3];
+        memset(f, 0, sizeof f);
+        f[0][0][0] = 1;
         for (int i = 0; i < n; ++i) {
             for (int j = 0; j <= 1; ++j) {
                 for (int k = 0; k <= 2; ++k) {
                     if (!j) {  // 在结尾加入A
-                        dp[i + 1][j + 1][0] = (int) (((long long) dp[i + 1][j + 1][0] + dp[i][j][k]) % M);
+                        f[i + 1][j + 1][0] = (int) (((long long) f[i + 1][j + 1][0] + f[i][j][k]) % M);
                     }
                     if (k + 1 <= 2) {  // 在结尾加入L
-                        dp[i + 1][j][k + 1] = (int) (((long long) dp[i + 1][j][k + 1] + dp[i][j][k]) % M);
+                        f[i + 1][j][k + 1] = (int) (((long long) f[i + 1][j][k + 1] + f[i][j][k]) % M);
                     }
-                    dp[i + 1][j][0] = (int) (((long long) dp[i + 1][j][0] + dp[i][j][k]) % M);  // 在结尾加入P
+                    f[i + 1][j][0] = (int) (((long long) f[i + 1][j][0] + f[i][j][k]) % M);  // 在结尾加入P
                 }
             }
         }
-        int res = 0;
+        int ans = 0;
         for (int j = 0; j < 2; ++j) {
             for (int k = 0; k < 3; ++k) {
-                res = (int) (((long long) res + dp[n][j][k]) % M);
+                ans = (int) (((long long) ans + f[n][j][k]) % M);
             }
         }
-        return res;
+        return ans;
     }
 };
 
