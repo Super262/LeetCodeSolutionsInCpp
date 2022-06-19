1864ae52f235eed7097d1f8c07d8225198a69b62
fengwei <virusformail@gmail.com>
2022-06-19 13:46:37 -0700

    Reviewed Problem 553.

--- Problem0553.h
+++ Problem0553.h
@@ -11,7 +11,8 @@
 using namespace std;
 
 class Solution {
-    // 脑筋急转弯：为[1:n-1]段加括号
+    // 贪心算法：设输入序列长度为n，将num[1:n-1]括出时，表达式的值最大
+    // 举例，设nums=[1,2,3,4]，最大值对应的表达式是"1/(2/3/4)"
 public:
     string optimalDivision(const vector<int> &nums) {
         const auto n = (int) nums.size();
@@ -24,16 +25,16 @@ public:
         if (n == 2) {
             return to_string(nums[0]) + "/" + to_string(nums[1]);
         }
-        string res = to_string(nums[0]);
-        res.push_back('/');
-        res.push_back('(');
-        res.append(to_string(nums[1]));
+        string ans = to_string(nums[0]);
+        ans += '/';
+        ans += '(';
+        ans.append(to_string(nums[1]));
         for (int i = 2; i < n; ++i) {
-            res.push_back('/');
-            res.append(to_string(nums[i]));
+            ans += '/';
+            ans.append(to_string(nums[i]));
         }
-        res.push_back(')');
-        return res;
+        ans += ')';
+        return ans;
     }
 };
 
