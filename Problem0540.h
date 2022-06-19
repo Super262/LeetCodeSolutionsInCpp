47a52d17e0a5fa08372544eb46646bd427e15392
fengwei <virusformail@gmail.com>
2022-06-18 17:43:51 -0700

    Reviewed Problem 540.

--- Problem0540.h
+++ Problem0540.h
@@ -10,26 +10,26 @@
 using namespace std;
 
 class Solution {
-    // 二分查找经典题目
-    // 给定序列[1,1,2,3,3,4,4,8,8]，在结尾加入8+1=9，得到[1,1,2,3,3,4,4,8,8,9]。
-    // 我们将数字结对分组，得到[(1,1),(2,3),(3,4),(4,8),(8,9)]。
-    // 经观察发现，存在组编号m，使m及其右侧的分组中2个元素不相等，m左侧分组中元素2个元素相等
+    // 设单独元素为c，我们将原数组中相同的元素归为一组，得到以下分组：(a,a)，(b,b)，(c)，(d,d)，……，
+    // 我们观察到如下规律：设索引为i的元素在c左侧，若i为偶数，nums[i]=nums[i+1]成立
+    // 若索引为i的元素在c右侧（包括c），若i为偶数，nums[i]!=nums[i+1]成立
+    // 因此，本题目可以用二分解决，查找满足nums[i]!=nums[i+1]成立的最小的i
 public:
-    int singleNonDuplicate(vector<int> &nums) {
-        // 暂时忽略最后一个分组（(a[n-1], a[n-1] + 1)）
-        int l = 0, r = (int) (nums.size() / 2 - 1);  // 二分查找分组编号（将a[0]~a[n-2]分组）
+    int singleNonDuplicate(const vector<int> &nums) {
+        int l = 0;
+        auto r = (int) nums.size() - 1;
         while (l < r) {
             auto mid = l + (r - l) / 2;
-            if (nums[mid * 2] != nums[mid * 2 + 1]) {
-                r = mid;
+            if (mid % 2) {
+                --mid;
+            }
+            if (nums[mid] == nums[mid + 1]) {
+                l = mid + 2;
             } else {
-                l = mid + 1;
+                r = mid;
             }
         }
-        if (r < 0 || nums[r * 2] == nums[r * 2 + 1]) {  // nums长度为1时，r<0
-            return nums.back();
-        }
-        return nums[2 * r];
+        return nums[r];
     }
 };
 
