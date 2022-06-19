a0e9f36b20aa8d48499f6da98a4504b26517ee2c
fengwei <virusformail@gmail.com>
2022-06-18 18:05:20 -0700

    Reviewed Problem 543.

--- Problem0543.h
+++ Problem0543.h
@@ -6,24 +6,17 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0543_H
 
 #include <algorithm>
+#include "treenode.h"
 
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
 class Solution {
+    // 类似后序遍历，先分别获得左、右子树的深度，再计算根的直径
 public:
     int diameterOfBinaryTree(TreeNode *root) {
+        if (!root) {
+            return 0;
+        }
         int answer = 0;
         dfs(root, answer);
         return answer;
@@ -31,11 +24,14 @@ public:
 
 private:
     int dfs(TreeNode *root, int &answer) {
-        if (!root) {
-            return 0;
+        int l = 0;
+        int r = 0;
+        if (root->left) {
+            l = dfs(root->left, answer);
         }
-        auto l = dfs(root->left, answer);
-        auto r = dfs(root->right, answer);
+        if (root->right) {
+            r = dfs(root->right, answer);
+        }
         answer = max(answer, l + r);
         return max(l, r) + 1;
     }
