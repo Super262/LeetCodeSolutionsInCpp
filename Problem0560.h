aa79998806eeb25c6867355b3a0e135491e0d81c
fengwei <virusformail@gmail.com>
2022-06-19 15:45:03 -0700

    Reviewed Problem 560.

--- Problem0560.h
+++ Problem0560.h
@@ -11,21 +11,21 @@
 using namespace std;
 
 class Solution {
-    // 经典操作：双指针 + 哈希表
+    // 前缀和+哈希表，固定索引i，查找索引j的个数，满足prefix[i]-prefix[j]=k
 public:
     int subarraySum(const vector<int> &nums, int k) {
-        int result = 0;
+        int ans = 0;
         int prefix = 0;
         unordered_map<int, int> prefix_cnt;
         prefix_cnt[0] = 1;
         for (const auto &x: nums) {
             prefix += x;
             if (prefix_cnt[prefix - k]) {
-                result += prefix_cnt[prefix - k];
+                ans += prefix_cnt[prefix - k];
             }
             ++prefix_cnt[prefix];
         }
-        return result;
+        return ans;
     }
 };
 
