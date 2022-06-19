ff2b8fe1b958d7c8c662eb9c043bb9465343d7ca
fengwei <virusformail@gmail.com>
2022-06-19 15:52:55 -0700

    Reviewed Problem 561.

--- Problem0561.h
+++ Problem0561.h
@@ -11,15 +11,17 @@
 using namespace std;
 
 class Solution {
-    // 经典贪心算法，利用"交换"可证明：https://www.acwing.com/solution/content/436/
+    // 很容易想到，让两个比较小的数在一起比一个小的一个大的数放在一起产生的结果要大
+    // 经典贪心算法：若原数组可以被分成n/2对，那么满足题意的sum是有序数组偶数位置元素的和
+    // 利用"交换"可证明：https://www.acwing.com/solution/content/436/
 public:
     int arrayPairSum(vector<int> &nums) {
         sort(nums.begin(), nums.end());
-        int res = 0;
-        for (int i = 0; i < nums.size(); i += 2) {
-            res += nums[i];
+        int ans = 0;
+        for (int i = 0; i < (int) nums.size(); i += 2) {
+            ans += nums[i];
         }
-        return res;
+        return ans;
     }
 };
 
