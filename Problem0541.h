68060ad4bf685615a4d2077de1450333a119d510
fengwei <virusformail@gmail.com>
2022-06-18 17:49:22 -0700

    Reviewed Problem 541.

--- Problem0541.h
+++ Problem0541.h
@@ -10,12 +10,15 @@
 using namespace std;
 
 class Solution {
+    // 将原数组从起点分成多个长度为2*k的分段，翻转每个分段内前k个字符
+    // 细节：注意防止越界
 public:
     string reverseStr(string s, int k) {
         const auto n = (int) s.size();
         for (int i = 0; i < n; i += 2 * k) {
-            int l = i, r = min(i + k, n);
-            reverse(s.begin() + l, s.begin() + r);  // 注意防止越界
+            auto l = i;
+            auto r = min(l + k, n);  // 注意防止越界
+            reverse(s.begin() + l, s.begin() + r);
         }
         return s;
     }
