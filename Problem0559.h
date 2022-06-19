2cc769dc4fedc0a51307021e36c82c229cdd6737
fengwei <virusformail@gmail.com>
2022-06-19 15:32:11 -0700

    Reviewed Problem 559.

--- Problem0559.h
+++ Problem0559.h
@@ -6,27 +6,12 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0559_H
 
 #include <vector>
+#include "nary_node.h"
 
 using namespace std;
 
-class Node {
-public:
-    int val;
-    vector<Node *> children;
-
-    Node() {}
-
-    Node(int _val) {
-        val = _val;
-    }
-
-    Node(int _val, vector<Node *> _children) {
-        val = _val;
-        children = _children;
-    }
-};
-
 class Solution {
+    // 类似后序遍历，先处理子节点，获得子节点的最大深度d，根结点深度为d+1
 public:
     int maxDepth(Node *root) {
         if (!root) {
