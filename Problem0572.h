caea3c4ca83525b52cf60597786d5dc4d970a371
fengwei <virusformail@gmail.com>
2022-06-20 02:15:11 -0700

    Reviewed Problem 572.

--- Problem0572.h
+++ Problem0572.h
@@ -5,43 +5,34 @@
 #ifndef LEETCODESOLUTIONSINCPP_PROBLEM0572_H
 #define LEETCODESOLUTIONSINCPP_PROBLEM0572_H
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
+#include "treenode.h"
 
 class Solution {
     // 完全线性的解法：树上哈希，类似字符串，以质数为基数
-    // H(root) = (H(root->val) + H(root->left) * P % MOD + H(root->right) * Q % MOD) % MOD;
+    // 哈希公式：H(root) = (H(root->val) + H(root->left) * P % MOD + H(root->right) * Q % MOD) % MOD；空树哈希值为-2
+    // 类似后序遍历，先计算左、右子树的哈希值，再计算当前树的哈希值，计算完成后将其和t比较
 public:
-    bool isSubtree(TreeNode *root, TreeNode *subRoot) {
-        bool answer = false;
-        auto t = dfs(subRoot, -1, answer);
-        if (t == dfs(root, t, answer)) {
-            answer = true;
+    bool isSubtree(TreeNode *root, TreeNode *sub_root) {
+        bool ans = false;
+        auto t = dfs(sub_root, -1, ans);  // 获得sub_root的哈希值
+        if (t == dfs(root, t, ans)) {  // root和sub_root相等
+            return true;
         }
-        return answer;
+        return ans;
     }
 
 private:
     const int P = 131, Q = 159, MOD = 1e9 + 7;
 
-    int dfs(TreeNode *root, const int t, bool &answer) {  // 计算哈希值，并查找有无子树哈希值为t
+    int dfs(TreeNode *root, const int t, bool &ans) {  // 计算哈希值，并查找有无子树哈希值为t
         if (!root) {
             return -2;
         }
-        auto lh = dfs(root->left, t, answer);
-        auto rh = dfs(root->right, t, answer);
+        auto lh = dfs(root->left, t, ans);
+        auto rh = dfs(root->right, t, ans);
         auto ph = (root->val % MOD + MOD) % MOD;
         if (lh == t || rh == t) {
-            answer = true;
+            ans = true;
         }
         return (int) ((ph + (long long) lh * P % MOD + (long long) rh * Q % MOD) % MOD);
     }
