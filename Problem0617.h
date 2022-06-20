e5326d8c02bc9a092acfaf9508779ec229028998
fengwei <virusformail@gmail.com>
2022-06-20 15:56:08 -0700

    Reviewed Problem 617.

--- Problem0617.h
+++ Problem0617.h
@@ -6,22 +6,12 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0617_H
 
 #include <algorithm>
+#include "treenode.h"
 
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
 class Solution {
+    // 类似后序遍历
 public:
     TreeNode *mergeTrees(TreeNode *t1, TreeNode *t2) {
         if (t2) {  // 技巧：保证t1非空，简化操作
