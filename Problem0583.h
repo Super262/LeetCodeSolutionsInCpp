abc85021c296f2d00c8a0983bbbcc3f409f7b7be
fengwei <virusformail@gmail.com>
2022-06-20 04:21:00 -0700

    Reviewed Problem 583.

--- Problem0583.h
+++ Problem0583.h
@@ -11,28 +11,29 @@
 using namespace std;
 
 class Solution {
+    // 动态规划，f[i][j]表示将w1[0:i-1]和w2[0:j-1]调整至相等，需要最少的操作次数
 public:
     int minDistance(const string &w1, const string &w2) {
-        const auto n = (int) w1.size();
-        const auto m = (int) w2.size();
-        int dp[n + 1][m + 1];
-        memset(dp, 0x3f, sizeof dp);
-        for (int l = 0; l <= m; ++l) {
-            dp[0][l] = l;
-        }
+        const auto m = (int) w1.size();
+        const auto n = (int) w2.size();
+        int f[m + 1][n + 1];
+        memset(f, 0x3f, sizeof f);
         for (int l = 0; l <= n; ++l) {
-            dp[l][0] = l;
+            f[0][l] = l;
         }
-        for (int l1 = 1; l1 <= n; ++l1) {
-            for (int l2 = 1; l2 <= m; ++l2) {
-                dp[l1][l2] = min(dp[l1 - 1][l2], dp[l1][l2 - 1]) + 1;
+        for (int l = 0; l <= m; ++l) {
+            f[l][0] = l;
+        }
+        for (int l1 = 1; l1 <= m; ++l1) {
+            for (int l2 = 1; l2 <= n; ++l2) {
+                f[l1][l2] = min(f[l1 - 1][l2], f[l1][l2 - 1]) + 1;
                 if (w1[l1 - 1] != w2[l2 - 1]) {
                     continue;
                 }
-                dp[l1][l2] = min(dp[l1][l2], dp[l1 - 1][l2 - 1]);
+                f[l1][l2] = min(f[l1][l2], f[l1 - 1][l2 - 1]);
             }
         }
-        return dp[n][m];
+        return f[m][n];
     }
 };
 
