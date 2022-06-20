c0c7cd9f1f05f14c251c96389e857178e7dcd582
fengwei <virusformail@gmail.com>
2022-06-20 12:40:06 -0700

    Reviewed Problem 589.

--- Problem0589.h
+++ Problem0589.h
@@ -6,26 +6,9 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0589_H
 
 #include <vector>
+#include "nary_node.h"
 
 using namespace std;
-
-class Node {
-public:
-    int val;
-    vector<Node *> children;
-
-    Node() {}
-
-    Node(int _val) {
-        val = _val;
-    }
-
-    Node(int _val, vector<Node *> _children) {
-        val = _val;
-        children = _children;
-    }
-};
-
 
 class Solution {
 public:
