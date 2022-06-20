4e7df1dbc147c2768200781b6ca54084ea36708e
fengwei <virusformail@gmail.com>
2022-06-20 03:07:57 -0700

    Reviewed Problem 576.

--- Problem0576.h
+++ Problem0576.h
@@ -10,25 +10,25 @@
 using namespace std;
 
 class Solution {
-    // 动态规划：dp[i][j][k]，从(i,j)移动k步后出界的方案数
+    // 动态规划：f[i][j][k]，从(i,j)移动k步后出界的方案数
 public:
     int findPaths(int m, int n, int q, int sr, int sc) {
         if (!q) {  // 不要忘记这个特殊情况，否则会发生越界异常！
             return 0;
         }
-        const int MOD = 1e9 + 7;
-        int dp[m][n][q + 1];
-        memset(dp, 0, sizeof dp);
+        int f[m][n][q + 1];
+        memset(f, 0, sizeof f);
         for (int i = 0; i < m; ++i) {  // 初始化左、右两侧边界点的方案数
-            ++dp[i][0][1];
-            ++dp[i][n - 1][1];
+            ++f[i][0][1];
+            ++f[i][n - 1][1];
         }
         for (int i = 0; i < n; ++i) {  // 初始化上、下两侧边界点的方案数
-            ++dp[0][i][1];
-            ++dp[m - 1][i][1];
+            ++f[0][i][1];
+            ++f[m - 1][i][1];
         }
         const int dx[] = {0, 1, 0, -1};
         const int dy[] = {1, 0, -1, 0};
+        const int MOD = 1e9 + 7;
         for (int k = 1; k <= q; ++k) {  // 在外层，从小到大枚举步数
             // 从外到内依次枚举(i,j)
             for (int x = 0; x < m; ++x) {
@@ -39,16 +39,16 @@ public:
                         if (px < 0 || py < 0 || px >= m || py >= n) {
                             continue;
                         }
-                        dp[x][y][k] = (int) ((dp[x][y][k] + (long long) dp[px][py][k - 1]) % MOD);
+                        f[x][y][k] = (int) ((f[x][y][k] + (long long) f[px][py][k - 1]) % MOD);
                     }
                 }
             }
         }
-        int res = 0;
+        int ans = 0;
         for (int i = 1; i <= q; ++i) {
-            res = (int) (((long long) res + dp[sr][sc][i]) % MOD);
+            ans = (int) (((long long) ans + f[sr][sc][i]) % MOD);
         }
-        return res;
+        return ans;
     }
 };
 
