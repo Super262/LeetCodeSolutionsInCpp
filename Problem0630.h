86f996b30d4c75c772f1a21e4599614b3186da2f
fengwei <virusformail@gmail.com>
2022-06-22 18:19:08 -0700

    Reviewed Problem 630.

--- Problem0630.h
+++ Problem0630.h
@@ -11,8 +11,9 @@
 using namespace std;
 
 class Solution {
-    // 经典贪心，必须掌握
-    // 类似动态规划的思考方式：https://www.acwing.com/solution/content/526/
+    // 贪心算法，假设现在有2节课，(d1,t1)、(d2,t2)，t1<t2；若d1+d2<=t1，可以任意安排这2节课；若d1+d2>t1且d1+d2<=t2，应先上第1节课，再上第2节课
+    // 若d1+d2>t2，只能上1门课，即时间最短的课；因此，我们可以按照课程的结束时间对所有课程排序，用大根堆记录被选择的课程的时长
+    // 从头遍历课程，维护总的上课时间total；设当前课程为(di,ti)，若total+di<=ti，证明当前课程可选；若total+di>ti，若堆顶课程耗时更长，则用当前课程替换堆顶课程
 public:
     int scheduleCourse(vector<vector<int>> &courses) {
         sort(courses.begin(), courses.end(), [](const vector<int> &a, const vector<int> &b) {
