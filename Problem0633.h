488035ef7155eba58ba56d0adcfc323045679c84
fengwei <virusformail@gmail.com>
2022-06-24 00:23:51 -0700

    Reviewed Problem 633.

--- Problem0633.h
+++ Problem0633.h
@@ -10,12 +10,12 @@
 using namespace std;
 
 class Solution {
+    // 根据题意，可知0<=b^2<=c，我们枚举b，计算出a=sqrt(c-b^2)，判断a是否为整数
 public:
     bool judgeSquareSum(int c) {
         for (int b = 0; (long long) b * b <= c; ++b) {
-            auto x = c - b * b;
-            auto a = (int) sqrt(x);
-            if (a * a == x) {
+            auto a = sqrt(c - b * b);
+            if (a == (int) a) {
                 return true;
             }
         }
