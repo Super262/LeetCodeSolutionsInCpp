3d09be3e683106bda55601e6c00e652afbe077ef
fengwei <virusformail@gmail.com>
2022-06-26 18:55:36 -0700

    Reviewed Problem 636.

--- Problem0636.h
+++ Problem0636.h
@@ -12,9 +12,12 @@
 using namespace std;
 
 class Solution {
+    // 根据题意，在任意时段，我们只能执行1个程序；若当前正执行程序a，新程序b开始执行，我们需要将a入栈，并记录a的执行时长
+    // 我们维护栈stk和变量last；新程序启动时，我们入栈新程序，更新last为当前时刻，并记录上个程序的执行时长
+    // 某个程序终止时，我们记录上个程序的执行时长，弹出栈顶，更新last为下个时刻
 public:
     vector<int> exclusiveTime(const int n, const vector<string> &logs) {
-        vector<int> res(n, 0);
+        vector<int> ans(n, 0);
         stack<int> stk;
         int last = 0;
         for (const auto &log: logs) {
@@ -24,17 +27,17 @@ public:
             auto t = stoi(log.substr(c2 + 1));
             if (log.substr(c1 + 1, c2 - c1 - 1) == "start") {
                 if (!stk.empty()) {
-                    res[stk.top()] += t - last;
+                    ans[stk.top()] += t - last;
                 }
                 last = t;
                 stk.emplace(id);
             } else {
-                res[stk.top()] += t - last + 1;
+                ans[stk.top()] += t - last + 1;
                 stk.pop();
                 last = t + 1;
             }
         }
-        return res;
+        return ans;
     }
 };
 
