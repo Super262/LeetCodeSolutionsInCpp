aaaa41f3afd32caf076323f14d8e0eb49d5ba04b
fengwei <virusformail@gmail.com>
2022-06-28 01:38:55 -0700

    Reviewed Problem 641.

--- Problem0641.h
+++ Problem0641.h
@@ -10,15 +10,20 @@
 using namespace std;
 
 class MyCircularDeque {
+    // 变量cap记录队列的容量，size记录队列长度，tail记录队尾第1个空白位置，head记录队头第1个数据位置
+    // 队头扩展时，head减1；队尾扩展时，tail加1；这里使用"模cap"限制下的运算
     // 经典算法，必须掌握：https://www.acwing.com/solution/content/5004/
 private:
-    int head, tail, size, n;
+    int head;
+    int tail;
+    int size;
+    int cap;
     vector<int> buffer;
 
 public:
     MyCircularDeque(int k) {
         buffer.resize(k);
-        n = k;
+        cap = k;
         size = 0;
         head = 0;
         tail = 0;
@@ -28,7 +33,7 @@ public:
         if (isFull()) {
             return false;
         }
-        head = (head + n - 1) % n;  // 相当于head减1
+        head = (head + cap - 1) % cap;  // 相当于head减1
         buffer[head] = value;
         ++size;
         return true;
@@ -39,7 +44,7 @@ public:
             return false;
         }
         buffer[tail] = value;
-        tail = (tail + 1) % n;
+        tail = (tail + 1) % cap;
         ++size;
         return true;
     }
@@ -48,7 +53,7 @@ public:
         if (isEmpty()) {
             return false;
         }
-        head = (head + 1) % n;  // 相当于head加1
+        head = (head + 1) % cap;  // 相当于head加1
         --size;
         return true;
     }
@@ -57,7 +62,7 @@ public:
         if (isEmpty()) {
             return false;
         }
-        tail = (tail - 1 + n) % n;  // 相当于tail减1
+        tail = (tail - 1 + cap) % cap;  // 相当于tail减1
         --size;
         return true;
     }
@@ -73,7 +78,7 @@ public:
         if (isEmpty()) {
             return -1;
         }
-        return buffer[(tail - 1 + n) % n];  // tail-1位置是数据
+        return buffer[(tail - 1 + cap) % cap];  // tail-1位置是数据
     }
 
     bool isEmpty() {
@@ -81,7 +86,7 @@ public:
     }
 
     bool isFull() {
-        return size == n;
+        return size == cap;
     }
 };
 
