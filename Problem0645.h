bc11dabec21a6b14410bff4e771bf4dfb695639f
fengwei <virusformail@gmail.com>
2022-06-28 23:07:38 -0700

    Reviewed Problem 645.

--- Problem0645.h
+++ Problem0645.h
@@ -10,24 +10,24 @@
 using namespace std;
 
 class Solution {
-    // 经典算法，必须掌握：翻转nums[k]符号位后，若nums[x]符号为正，则x出现0次或2次
+    // 设x=nums[i]，翻转x的符号位后，若nums[x-1]符号为正，则x出现0次或2次
 public:
     vector<int> findErrorNums(vector<int> &nums) {
-        vector<int> res(2, 0);
-        for (auto &x: nums) {
+        vector<int> ans(2, 0);
+        for (const auto &x: nums) {
             auto k = abs(x);
             nums[k - 1] *= -1;
             if (nums[k - 1] > 0) {  // 发现第1个正数
-                res[0] = k;
+                ans[0] = k;
             }
         }
         for (int i = 0; i < (int) nums.size(); ++i) {
-            if (nums[i] > 0 && i + 1 != res[0]) {  // 发现第2个正数
-                res[1] = i + 1;
+            if (nums[i] > 0 && i + 1 != ans[0]) {  // 发现第2个正数
+                ans[1] = i + 1;
                 break;
             }
         }
-        return res;
+        return ans;
     }
 };
 
