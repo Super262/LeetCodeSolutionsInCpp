5f8f8ac17b6f021b2beecdec99040f122b463e13
fengwei <virusformail@gmail.com>
2022-06-28 23:15:13 -0700

    Reviewed Problem 646.

--- Problem0646.h
+++ Problem0646.h
@@ -11,22 +11,23 @@
 using namespace std;
 
 class Solution {
-    // 经典贪心题，直接背诵：最多不相交区间
+    // 贪心，按照右端点对所有区间升序排序，再从头开始遍历，判断相交关系
+    // 最多不相交区间，同AcWing 908
 public:
     int findLongestChain(vector<vector<int>> &pairs) {
-        sort(pairs.begin(), pairs.end(), [](const vector<int> &a, const vector<int> &b) {
+        sort(pairs.begin(), pairs.end(), [&](const vector<int> &a, const vector<int> &b) {
             return a[1] < b[1];
         });
-        int res = 1;
+        int ans = 1;
         auto ed = pairs[0][1];
         for (int i = 1; i < (int) pairs.size(); ++i) {
             if (pairs[i][0] <= ed) {
                 continue;
             }
-            ++res;
+            ++ans;
             ed = max(ed, pairs[i][1]);
         }
-        return res;
+        return ans;
     }
 };
 
