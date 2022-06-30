bc1758e16dea5376561ee677466613287e71624f
fengwei <wuzhongfengwei@163.com>
2022-06-30 09:39:37 -0700

    Reviewed Problem 650.

--- Problem0650.h
+++ Problem0650.h
@@ -6,8 +6,10 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0650_H
 
 class Solution {
-    // 经典算法，直接背诵：试除法求解质因子
-    // https://www.acwing.com/solution/content/502/
+    // 试除法求解质因子；假设当前已经得到了n的所有质因数a1，a2，……，ai，n的分解方式可能如下：最后一步必定是由a1次n/a1个"A"操作而成，
+    // 倒数第二步一定是由a2次n/(a1*a2)个"A"操作而成，直到第一步为止；可以发现不管分解顺序如何，最终答案都是质因数之和
+    // 因此本题的核心思想即粘贴的次数为质数次，否则可以有更有的分解方式；设当前字符串的长度为l，目标长度为l*p；若直接拷贝达到目标长度，需要p次操作
+    // 若p是合数，即p=i*j（i>1，j>1），那么可以将拷贝分成2次，先操作i次，再操作j次，这样只需要i+j次操作；显然i*j>=i+j，因此上述思想正确
 public:
     int minSteps(int n) {
         int ans = 0;
