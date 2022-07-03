3e65bbf9cca1ee8aa7b9937cb17db7cd3ef6eace
fengwei <virusformail@gmail.com>
2022-07-03 11:45:08 -0700

    Reviewed Problem 653.

--- Problem0653.h
+++ Problem0653.h
@@ -6,23 +6,13 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0653_H
 
 #include <unordered_set>
+#include "treenode.h"
 
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
 class Solution {
-    // 经典算法，直接背诵：中序遍历
+    // 若给定有序数组a，找到和为k的2个数字，我们可以通过集合（保存已经遍历过的元素）实现
+    // BST的中序是有序的，我们通过"中序遍历+集合"找到和为k的2个数字
 public:
     bool findTarget(TreeNode *root, int k) {
         unordered_set<int> nums;
