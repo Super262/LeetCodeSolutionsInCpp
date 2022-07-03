9c186047579b63fffdf5fb9370c14e04976bf495
fengwei <virusformail@gmail.com>
2022-07-03 15:14:12 -0700

    Reviewed Problem 662.

--- Problem0662.h
+++ Problem0662.h
@@ -6,30 +6,19 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0662_H
 
 #include <queue>
+#include "treenode.h"
 
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
 class Solution {
-    // 经典算法，必须掌握：给节点编号，节点i的孩子为2*i和2*i+1，计算每层左、右端点编号的差
-    // 细节：避免编号值溢出整数，下一层编号0开始
+    // 给节点编号，节点i的孩子为2*i和2*i+1，计算每层左、右端点编号的差
+    // 细节：避免编号值溢出整数，每层从0开始对节点重新编号
 public:
     int widthOfBinaryTree(TreeNode *root) {
         if (!root) {
             return 0;
         }
-        unsigned long long res = 1;
+        unsigned long long ans = 1;
         queue<pair<TreeNode *, unsigned long long>> q;
         q.emplace(root, 1);
         while (!q.empty()) {
@@ -47,9 +36,9 @@ public:
                     q.emplace(t->right, 2 * (ed - st) + 1);
                 }
             }
-            res = max(res, ed - st + 1);
+            ans = max(ans, ed - st + 1);
         }
-        return (int) res;
+        return (int) ans;
     }
 };
 
