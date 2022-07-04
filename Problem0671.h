dd70f78fe41bce5e7ddf76f3d94bbb025dfdf3ce
fengwei <virusformail@gmail.com>
2022-07-04 11:22:35 -0700

    Reviewed Problem 671.

--- Problem0671.h
+++ Problem0671.h
@@ -6,24 +6,12 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0671_H
 
 #include <algorithm>
+#include "treenode.h"
 
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
 class Solution {
-    // 直接遍历，记录最小值和次小值
-    // 注意细节：根结点最小，选择先序遍历；x>d1&&x<d2时，更新d2=x
+    // 先序遍历，记录最小值和次小值；注意细节：x>d1&&x<d2时，更新d2=x
 public:
     int findSecondMinimumValue(TreeNode *root) {
         long long d1 = 1ll + INT_MAX;
