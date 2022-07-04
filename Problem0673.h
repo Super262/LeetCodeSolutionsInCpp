450d1bf1f8d0e239d76ca46ba601744521a3e0fd
fengwei <virusformail@gmail.com>
2022-07-04 16:17:48 -0700

    Reviewed Problem 673.

--- Problem0673.h
+++ Problem0673.h
@@ -11,12 +11,15 @@
 using namespace std;
 
 class Solution {
+    // f[i]表示以nums[i]结尾的LIS的最大长度；g[i]表示以nums[i]结尾的LIS的个数
+    // 
 public:
     int findNumberOfLIS(const vector<int> &nums) {
         const int n = (int) nums.size();
-        int f[n];  // f[i]表示以nums[i]结尾的LIS的最大长度
-        int g[n];  // g[i]表示以nums[i]结尾的LIS的个数
-        int max_l = 0, max_cnt = 0;
+        int f[n];
+        int g[n];
+        int max_l = 0;
+        int max_cnt = 0;
         for (int i = 0; i < n; ++i) {
             f[i] = 1;
             g[i] = 1;
