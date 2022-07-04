2a35ab9235cda119f20b95f74ac80eedc4cb3737
fengwei <virusformail@gmail.com>
2022-07-03 23:59:28 -0700

    Reviewed Problem 664.

--- Problem0664.h
+++ Problem0664.h
@@ -11,9 +11,10 @@
 using namespace std;
 
 class Solution {
-    // 经典DP，直接背诵
-    // dp[i][j]：打印s[i:j]的最小操作数
-    // https://www.acwing.com/solution/content/5208/
+    // 区间DP，f[l][r]表示将[l:r]染色为最终样子的最少操作次数；若先打印s[l]，再打印s[l:r]，操作次数为f[l+1][r]+1，这也是f[l][r]的初始值
+    // 若s[l+1]=s[l]，那么f[l][r]=f[l][l]+f[l+2][r]；否则，f[l][r]=f[l][l+1]+f[l+2][r]
+    // 以此类推，若s[k]=s[l]，那么f[l][r]=f[l][k-1]+f[k+1][r]；否则，f[l][r]=f[l][k]+f[k+1][r]
+    // 类似矩阵连乘，https://www.acwing.com/solution/content/5208/
 public:
     int strangePrinter(const string &s) {
         const auto n = (int) s.size();
