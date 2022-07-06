ed00a2d22775b6aac50e08ec1401e2ea26f1c482
fengwei <virusformail@gmail.com>
2022-07-06 10:02:34 -0700

    Reviewed Problem 685.

--- Problem0685.h
+++ Problem0685.h
@@ -12,8 +12,10 @@
 using namespace std;
 
 class Solution {
-    // 经典算法，必须掌握：有向图找环
-    // https://www.acwing.com/solution/content/20909/
+    // 经观察，我们把输入的图分为3种：(1)只有环；(2)无环，存在入度为2的点；(3)上述2种情况均存在
+    // 对于(1)，我们返回1条环中的边；对于(2)，我们返回入度为2的点的两条入边之一；对于(3)，我们返回(1)、(2)的公共边
+    // 我们可以使用Tarjan算法找有向图中的环，由于本题的输入是有环的树，可以使用"简化版"Tarjan算法，O(n)时间找到环
+    // st1[i]=true：第i条边在环中；st2[i]=true：第i条边是某个点两条入边之一
 public:
     vector<int> findRedundantDirectedConnection(const vector<vector<int>> &edges) {
         bool st1[edges.size()];  // 边i是否在环中
@@ -21,14 +23,14 @@ public:
         memset(st1, 0, sizeof st1);
         memset(st2, 0, sizeof st2);
         const auto n = (int) edges.size();
-        work1(edges, n, st1);
-        work2(edges, n, st2);
-        for (int i = (int) edges.size() - 1; i >= 0; --i) {
+        work1(edges, n, st1);  // 探索情况(1)是否存在
+        work2(edges, n, st2);  // 探索情况(2)是否存在
+        for (int i = (int) edges.size() - 1; i >= 0; --i) {  // 先判断情况(3)
             if (st1[i] && st2[i]) {
                 return edges[i];
             }
         }
-        for (int i = (int) edges.size() - 1; i >= 0; --i) {
+        for (int i = (int) edges.size() - 1; i >= 0; --i) {  // 再判断情况(1)、(2)
             if (st1[i] || st2[i]) {
                 return edges[i];
             }
@@ -37,8 +39,22 @@ public:
     }
 
 private:
+    void work2(const vector<vector<int>> &edges, int n, bool st2[]) {
+        int parent[n + 1];  // parent[v]：点v的前驱边(u,v)的编号
+        memset(parent, -1, sizeof parent);
+        for (int i = 0; i < (int) edges.size(); ++i) {
+            auto b = edges[i][1];
+            if (parent[b] != -1) {  // 发现入度为2的点
+                st2[parent[b]] = true;
+                st2[i] = true;
+                return;
+            }
+            parent[b] = i;
+        }
+    }
+
     void work1(const vector<vector<int>> &edges, int n, bool st1[]) {
-        vector<vector<int>> graph(n + 1, vector<int>());
+        vector<vector<int>> graph(n + 1, vector<int>());  // 邻接表建图
         for (const auto &e: edges) {
             graph[e[0]].emplace_back(e[1]);
         }
@@ -69,7 +85,7 @@ private:
              stack<int> &stk,
              bool visited[],
              bool in_stk[],
-             bool in_cycle[]) {
+             bool in_cycle[]) {  // 简化的Tarjan算法
         visited[v] = true;
         stk.emplace(v);
         in_stk[v] = true;
@@ -79,8 +95,9 @@ private:
                     return true;
                 }
             } else if (in_stk[x]) {
-                while (!stk.empty() && stk.top() != x) {
+                while (!stk.empty() && stk.top() != x) {  // x的所有前驱都出现在环中
                     in_cycle[stk.top()] = true;
+                    in_stk[stk.top()] = false;
                     stk.pop();
                 }
                 in_cycle[x] = true;
@@ -90,21 +107,6 @@ private:
         stk.pop();
         in_stk[v] = false;
         return false;
-    }
-
-    void work2(const vector<vector<int>> &edges, int n, bool st2[]) {
-        int parent[n + 1];  // 点v的前驱边的编号
-        memset(parent, -1, sizeof parent);
-        for (int i = 0; i < (int) edges.size(); ++i) {
-            auto b = edges[i][1];
-            if (parent[b] == -1) {
-                parent[b] = i;
-            } else {
-                st2[parent[b]] = true;
-                st2[i] = true;
-                return;
-            }
-        }
     }
 };
 
