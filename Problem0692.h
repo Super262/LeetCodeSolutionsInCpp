3beef88021309891e3fbd94ef9e43a3aeec9842a
fengwei <virusformail@gmail.com>
2022-07-08 03:05:42 -0700

    Reviewed Problem 692.

--- Problem0692.h
+++ Problem0692.h
@@ -13,7 +13,8 @@
 using namespace std;
 
 class Solution {
-    // 使用堆避免全排序，节约时间；线性时间构建堆
+    // 首先，利用哈希表统计每个单词的频率；然后，我们遍历哈希表，将"(词频，单词)"压入大根堆；最后，弹出堆内前k个单词
+    // 使用堆避免全排序，节约时间；make_heap()是线性时间构建堆；make_heap默认建立最大堆，注意自定义比较器的实现（相当于less）
 public:
     vector<string> topKFrequent(const vector<string> &words, int k) {
         unordered_map<string, int> counter;
@@ -25,21 +26,21 @@ public:
         for (const auto &item: counter) {
             heap.emplace_back(item.second, item.first);
         }
-        auto cmp = [](const pair<int, string> &a, const pair<int, string> &b) {
+        auto cmp = [&](const pair<int, string> &a, const pair<int, string> &b) {
             if (a.first != b.first) {
                 return a.first < b.first;
             }
             return a.second > b.second;
         };
-        make_heap(heap.begin(), heap.end(), cmp);  // make_heap默认建立最大堆，注意自定义比较器的实现（相当于less）
-        vector<string> res;
-        res.reserve(k);
-        while (k--) {
-            res.emplace_back(heap[0].second);
+        make_heap(heap.begin(), heap.end(), cmp);
+        vector<string> ans;
+        ans.reserve(k);
+        while (!heap.empty() && (int) ans.size() < k) {
+            ans.emplace_back(heap[0].second);
             pop_heap(heap.begin(), heap.end(), cmp);
             heap.pop_back();
         }
-        return res;
+        return ans;
     }
 };
 
