5f2bddc20f3842f6984f5ed03542d6917238566e
fengwei <virusformail@gmail.com>
2022-07-08 04:13:19 -0700

    Reviewed Problem 696.

--- Problem0696.h
+++ Problem0696.h
@@ -10,22 +10,24 @@
 using namespace std;
 
 class Solution {
-    // 直接背诵：若s由k个连续的1和k个连续的0组成，s共包含k个满足题意的子串
+    // 我们可以将字符串s分成多个组，每组由相同字符组成；例如，s="110001111000000"，每组长度为[2,3,4,6]
+    // 分组后，我们可以计算出相邻两组长度的最小值，得到[2,3,4]；易知，答案即为2+3+4=7（例如，"0011"包含2个满足题意的子串）
+    // 我们无须使用哈希表统计每组的长度，只需边遍历、边累计
 public:
     int countBinarySubstrings(const string &s) {
-        int res = 0;
-        int last = 0;
+        int ans = 0;
+        int prev = 0;
         for (int i = 0; i < (int) s.size(); ++i) {
             auto j = i + 1;
             while (j < (int) s.size() && s[j] == s[i]) {
                 ++j;
             }
             auto cur = j - i;
+            ans += min(prev, cur);
+            prev = cur;
             i = j - 1;
-            res += min(last, cur);
-            last = cur;
         }
-        return res;
+        return ans;
     }
 };
 
