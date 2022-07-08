f859c45b617eb91af6bc6b58d4e8491434aea77f
fengwei <virusformail@gmail.com>
2022-07-08 03:47:04 -0700

    Reviewed Problem 695.

--- Problem0695.h
+++ Problem0695.h
@@ -12,33 +12,34 @@
 using namespace std;
 
 class Solution {
+    // BFS，返回所有连通块的最大值
 public:
     int maxAreaOfIsland(const vector<vector<int>> &grid) {
         const auto m = (int) grid.size();
         const auto n = (int) grid[0].size();
-        int maximal = 0;
+        int ans = 0;
         vector<vector<bool>> visited(m, vector<bool>(n, false));
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
                 if (!grid[i][j] || visited[i][j]) {
                     continue;
                 }
-                maximal = max(maximal, bfs({i, j}, grid, visited, m, n));
+                ans = max(ans, bfs({i, j}, grid, visited, m, n));
             }
         }
-        return maximal;
+        return ans;
     }
 
 private:
     int bfs(pair<int, int> st, const vector<vector<int>> &grid, vector<vector<bool>> &visited, int m, int n) {
         queue<pair<int, int>> q;
-        int res = 0;
+        int ans = 0;
         q.emplace(st);
         visited[st.first][st.second] = true;
         while (!q.empty()) {
             auto t = q.front();
             q.pop();
-            ++res;
+            ++ans;
             for (int i = 0; i < 4; ++i) {
                 auto nx = t.first + dx[i];
                 auto ny = t.second + dy[i];
@@ -49,7 +50,7 @@ private:
                 q.emplace(nx, ny);
             }
         }
-        return res;
+        return ans;
     }
 
     const int dx[4] = {0, 1, 0, -1};
