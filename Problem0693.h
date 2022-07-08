e54a9527ceff4ee9229a1743701230ca6e759a75
fengwei <virusformail@gmail.com>
2022-07-08 03:18:16 -0700

    Reviewed Problem 693.

--- Problem0693.h
+++ Problem0693.h
@@ -6,11 +6,13 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0693_H
 
 class Solution {
+    // 若对于任意0<i<n，二进制序列s满足s[i-1]!=s[i]，返回true；否则返回false
 public:
     bool hasAlternatingBits(int n) {
-        for (int i = 1; (1ll << i) <= n; ++i) {
-            auto a = (n >> (i - 1)) & 1;
-            auto b = (n >> i) & 1;
+        while (n) {
+            auto a = n % 2;
+            n /= 2;
+            auto b = n % 2;
             if (a == b) {
                 return false;
             }
