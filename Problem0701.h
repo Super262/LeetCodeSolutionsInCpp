aa01780fdd878c7d89d0ccc1dccf626a9c14b992
fengwei <virusformail@gmail.com>
2022-07-08 09:10:57 -0700

    Reviewed Problem 701.

--- Problem0701.h
+++ Problem0701.h
@@ -5,30 +5,31 @@
 #ifndef LEETCODESOLUTIONSINCPP_PROBLEM0701_H
 #define LEETCODESOLUTIONSINCPP_PROBLEM0701_H
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
+#include "treenode.h"
 
 class Solution {
+    // 根据BST的性质，迭代搜索合适的插入位置
 public:
     TreeNode *insertIntoBST(TreeNode *root, int val) {
-        if (!root) {
-            return new TreeNode(val);
+        auto node = root;
+        while (node) {
+            if (val > node->val) {
+                if (node->right) {
+                    node = node->right;
+                } else {
+                    node->right = new TreeNode(val);
+                    return root;
+                }
+            } else {
+                if (node->left) {
+                    node = node->left;
+                } else {
+                    node->left = new TreeNode(val);
+                    return root;
+                }
+            }
         }
-        if (root->val < val) {
-            root->right = insertIntoBST(root->right, val);
-        } else {
-            root->left = insertIntoBST(root->left, val);
-        }
-        return root;
+        return new TreeNode(val);
     }
 };
 
