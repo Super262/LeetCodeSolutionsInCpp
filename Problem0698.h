f4eb8f3d687d2c65b4874402ae0c96c85dce85f3
fengwei <virusformail@gmail.com>
2022-07-08 04:45:59 -0700

    Reviewed Problem 698.

--- Problem0698.h
+++ Problem0698.h
@@ -13,7 +13,10 @@
 using namespace std;
 
 class Solution {
-    // 经典题目，必须背诵：DFS + 4个剪枝
+    // 经典题目，DFS + 4个剪枝；我们枚举每个数字nums[i]的使用情况，尝试构建k个相同数字
+    // 剪枝1：DFS前，降序排序nums，先尝试较大元素
+    // 剪枝2：若nums[i]不可行，跳过i后面和nums[i]相等的其它元素
+    // 剪枝3、4：若尝试nums[i]失败，当前正处理头部或尾部，说明本层DFS必失败，直接返回false
 public:
     bool canPartitionKSubsets(vector<int> &nums, const int k) {
         auto sum = accumulate(nums.begin(), nums.end(), 0);
