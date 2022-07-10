557cd7cae8874b6d7214a31f10b9a9ddd0e08964
fengwei <virusformail@gmail.com>
2022-07-10 13:44:02 -0700

    Reviewed Problem 713.

--- Problem0713.h
+++ Problem0713.h
@@ -10,10 +10,11 @@
 using namespace std;
 
 class Solution {
-    // nums中只包含正数，可用双指针
+    // 由于nums中只包含正数，乘积和数组长度正相关，可用"双指针"解决；若nums[l:r]满足乘积小于k，那么nums[l:r]向答案新贡献了(r-l+1)
+    // 因为新增加的以nums[r]结尾的子数组有r-l+1个（双指针：r每次右移1，l向左移动以满足题意）
 public:
     int numSubarrayProductLessThanK(const vector<int> &nums, int k) {
-        int res = 0;
+        int ans = 0;
         int temp = 1;
         for (int l = 0, r = 0; r < (int) nums.size(); ++r) {
             temp *= nums[r];
@@ -21,9 +22,9 @@ public:
                 temp /= nums[l];
                 ++l;
             }
-            res += r - l + 1;
+            ans += r - l + 1;
         }
-        return res;
+        return ans;
     }
 };
 
