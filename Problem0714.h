40d76a9357a95da5dc5b621c91d0dfed8694e45c
fengwei <virusformail@gmail.com>
2022-07-10 13:48:25 -0700

    Reviewed Problem 714.

--- Problem0714.h
+++ Problem0714.h
@@ -11,20 +11,21 @@
 using namespace std;
 
 class Solution {
-    // 状态机DP：f[i][0]表示无股票，f[i][1]表示有股票
+    // 状态机DP，f[i][0]表示无股票，f[i][1]表示有股票
+    // 我们可以在f[i-1][0]买入股票，转移到f[i][1]；也可以在f[i-1][1]卖出股票，转移到f[i][0]
 public:
     int maxProfit(const vector<int> &prices, int fee) {
         const auto n = (int) prices.size();
         int f[2][2];
-        int res = 0;
+        int ans = 0;
         memset(f, -0x3f, sizeof f);
         f[0][0] = 0;
         for (int i = 1; i <= n; ++i) {
             f[i % 2][0] = max(f[(i - 1) % 2][0], f[(i - 1) % 2][1] + prices[i - 1]);
             f[i % 2][1] = max(f[(i - 1) % 2][1], f[(i - 1) % 2][0] - prices[i - 1] - fee);
-            res = max(res, f[i % 2][0]);
+            ans = max(ans, f[i % 2][0]);
         }
-        return res;
+        return ans;
     }
 };
 
