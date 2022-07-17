41131f1a1ace3cc6f39f0ee40978550f22825416
fengwei <virusformail@gmail.com>
2022-07-17 09:42:25 -0700

    Reviewed Problem 725.

--- Problem0725.h
+++ Problem0725.h
@@ -6,36 +6,27 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0725_H
 
 #include <vector>
+#include "listnode.h"
 
 using namespace std;
 
-struct ListNode {
-    int val;
-    ListNode *next;
-
-    ListNode() : val(0), next(nullptr) {}
-
-    ListNode(int x) : val(x), next(nullptr) {}
-
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-
-class Solution {
+class Problem0725 {
+    // 首先，遍历链表，获得总长度n；设r=n%k，l=n/k，当前区段的索引为i；若i<r，区段i的节点个数为l+1；若i>=r，区段i的节点个数为l（0<=i<=n-1）
 public:
     vector<ListNode *> splitListToParts(ListNode *head, int k) {
         int n = 0;
         for (auto p = head; p; p = p->next) {
             ++n;
         }
-        vector<ListNode *> res;
-        res.reserve(k);
+        vector<ListNode *> ans;
+        ans.reserve(k);
         auto p = head;
         for (int i = 0; i < k; ++i) {
             auto length = n / k;
             if (i < n % k) {
                 ++length;
             }
-            res.emplace_back(p);
+            ans.emplace_back(p);
             for (int j = 0; j < length - 1; ++j) {  // 找到最后结点的前驱
                 p = p->next;
             }
@@ -45,7 +36,7 @@ public:
                 p = q;
             }
         }
-        return res;
+        return ans;
     }
 };
 
