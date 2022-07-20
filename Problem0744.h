f8468eb4fa360bff99f60045280712a17364888b
fengwei <virusformail@gmail.com>
2022-07-19 21:38:32 -0700

    Reviewed Problem 744.

--- Problem0744.h
+++ Problem0744.h
@@ -9,7 +9,8 @@
 
 using namespace std;
 
-class Solution {
+class Problem0744 {
+    // 因为输入序列是有序的，我们二分查找第1个大于t的字符letters[r]；若letters[r]小于或等于t，我们返回letters[0]
 public:
     char nextGreatestLetter(const vector<char> &letters, const char t) {
         int l = 0;
