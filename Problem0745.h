de0e706338fa9b55d5351b8f68a9105135dfe408
fengwei <virusformail@gmail.com>
2022-07-19 21:57:30 -0700

    Reviewed Problem 745.

--- Problem0745.h
+++ Problem0745.h
@@ -14,6 +14,8 @@ class WordFilter {
     // 直接背诵思路：https://www.acwing.com/solution/content/3916/
     // 朴素想法就是分别建立前缀树和后缀树，然后用DFS求解前缀集合和后缀集合，求两个集合的交集
     // 我们对每个单词的所有后缀（包括空串）以及自己，拼接上"#"，再拼接上原单词，把整个字符串插入到Trie中
+    // 例如，对于单词"apple"，我们插入"#apple", "e#apple", "le#apple", "ple#apple", "pple#apple", "apple#apple"
+    // 根据题目要求，我们从前向后插入单词序列，确保公共前缀节点保存的是最大的索引
 public:
     WordFilter(const vector<string> &words) {
         root = new Node();
