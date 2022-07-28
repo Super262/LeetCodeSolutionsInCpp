22ca7f8e280622893a76472ae1b811aa0af098b4
fengwei <virusformail@gmail.com>
2022-07-28 09:06:29 -0700

    Reviewed Problem 762.

--- Problem0762.h
+++ Problem0762.h
@@ -9,24 +9,30 @@
 
 using namespace std;
 
-class Solution {
+class Problem0762 {
+    // 枚举[l:r]中每个数字，验证它是否有质数个1；我们用哈希表存储[2:31]内所有质数，便于判断
 public:
-    int countPrimeSetBits(int left, int right) {
-        if (left > right) {
+    int countPrimeSetBits(int l, int r) {
+        if (l > r) {
             return 0;
         }
-        unordered_set<int> primes({2, 3, 5, 7, 11, 13, 17, 19});
-        int res = 0;
-        for (auto i = left; i <= right; ++i) {
+        unordered_set<int> primes({2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31});
+        int ans = 0;
+        for (auto i = l; i <= r; ++i) {
             int counter = 0;
-            for (auto j = i; j > 0; j >>= 1) {
-                counter += j & 1;
+            for (auto j = lowBit(i); j > 0; j -= lowBit(j)) {
+                ++counter;
             }
             if (primes.count(counter)) {
-                ++res;
+                ++ans;
             }
         }
-        return res;
+        return ans;
+    }
+
+private:
+    int lowBit(int x) {
+        return x & -x;
     }
 };
 
