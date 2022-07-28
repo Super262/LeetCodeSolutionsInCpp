5696ad534bf88d20d1752e0af499223cdfbed654
fengwei <virusformail@gmail.com>
2022-07-28 09:15:41 -0700

    Reviewed Problem 763.

--- Problem0763.h
+++ Problem0763.h
@@ -11,25 +11,25 @@
 
 using namespace std;
 
-class Solution {
+class Problem0763 {
 public:
     vector<int> partitionLabels(const string &s) {
         unordered_map<char, int> last_idx;
         for (int i = 0; i < (int) s.size(); ++i) {
             last_idx[s[i]] = i;
         }
-        vector<int> res;
+        vector<int> ans;
         int st = 0;
         int ed = -1;
         for (int i = 0; i < (int) s.size(); ++i) {
             ed = max(last_idx[s[i]], ed);
             if (i == ed) {
-                res.emplace_back(ed - st + 1);
+                ans.emplace_back(ed - st + 1);
                 ed = i + 1;
                 st = i + 1;
             }
         }
-        return res;
+        return ans;
     }
 };
 
