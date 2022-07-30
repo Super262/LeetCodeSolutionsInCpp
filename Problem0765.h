7205606861a0a297a5d458d1774556c1509fe1b5
fengwei <virusformail@gmail.com>
2022-07-30 14:04:38 -0700

    Reviewed Problem 765.

--- Problem0765.h
+++ Problem0765.h
@@ -9,23 +9,29 @@
 
 using namespace std;
 
-class Solution {
-    // 环状图的特点：若交换环内的点，一个环被断开，会多出一个环；若交换不同环的点，两环合并，会少一个环
-    // 点是每对情侣(i,i)，边是座位(i,j)，题目要求将原来的x个环转化为n个环，因此，最少操作次数为n-x
-    // 由于是无向图，环的数量 == 连通块的数量
-    // https://www.acwing.com/solution/content/3957/
+class Problem0765 {
+    // 通过观察，我们发现：如果有2对情侣互相坐错了位置，我们至多只需要交换1次；如果有3对情侣互相坐错了位置，"A1, B2, B1, C2, C1, A2"，
+    // 他们之间形成了1个环，我们只需要交换2次；如果有4对情侣互相坐错了位置，"A1, B2, B1, C2, C1, D2, D1, A2"，我们需要交换3次
+    // 以此类推，若k对情侣形成一个环状的错误链，我们只需要交换k-1次就可以将这k对情侣的位置排好；因此，问题转化成n对情侣中，有几个错误环
+    // 我们可以使用并查集来处理：每次遍历相邻的2个位置，若他们是情侣，他们处于大小为1的错误环，只需要交换0次；若他们不是情侣，我们将他们合并，
+    // 将所有的错坐情侣合并和后，答案就是情侣对数减环个数；https://www.acwing.com/solution/content/3957/
 public:
     int minSwapsCouples(const vector<int> &row) {
-        auto n = (int) row.size();
-        auto m = n / 2;
+        const auto n = (int) row.size();
+        const auto m = n / 2;
         int parent[m];
-        int size[m];
+        int set_size[m];
         for (int i = 0; i < m; ++i) {
             parent[i] = i;
-            size[i] = 1;
+            set_size[i] = 1;
         }
         for (int i = 0; i < n; i += 2) {
-            unionSet(row[i] / 2, row[i + 1] / 2, parent, size);  // 建立并查集
+            auto pa = findRoot(row[i] / 2, parent);
+            auto pb = findRoot(row[i + 1] / 2, parent);
+            if (pa == pb) {
+                continue;
+            }
+            mergeSets(pa, pb, parent, set_size);  // 合并2个不同环
         }
         int cycles = 0;  // 环的数量
         for (int i = 0; i < m; ++i) {
@@ -38,25 +44,30 @@ public:
 
 private:
     int findRoot(int x, int parent[]) {
-        if (x != parent[x]) {
-            parent[x] = findRoot(parent[x], parent);
+        auto u = x;
+        while (parent[u] != u) {
+            u = parent[u];
         }
-        return parent[x];
+        while (x != u) {
+            auto p = parent[x];
+            parent[x] = u;
+            x = p;
+        }
+        return u;
     }
 
-    void unionSet(int a, int b, int parent[], int size[]) {  // 按秩合并
-        auto pa = findRoot(a, parent);
-        auto pb = findRoot(b, parent);
+    int mergeSets(int pa, int pb, int parent[], int set_size[]) {  // 按秩合并
         if (pa == pb) {
-            return;
+            return -1;
         }
-        if (size[pa] < size[pb]) {
+        if (set_size[pa] < set_size[pb]) {
             parent[pa] = pb;
-            size[pb] += size[pa];
-        } else {
-            parent[pb] = pa;
-            size[pa] += size[pb];
+            set_size[pb] += set_size[pa];
+            return pb;
         }
+        parent[pb] = pa;
+        set_size[pa] += set_size[pb];
+        return pa;
     }
 };
 
