524f980399206689d39ea54559e2383aaf6fd3c9
fengwei <virusformail@gmail.com>
2022-07-30 16:42:31 -0700

    Reviewed Problem 767.

--- Problem0767.h
+++ Problem0767.h
@@ -10,40 +10,40 @@
 
 using namespace std;
 
-class Solution {
-    // 构造n/2个抽屉：注意特判，将频率大于n/2的字符放在偶数位置，因为n/2等于奇数位的数量
+class Problem0767 {
+    // 构造n/2个抽屉，每个抽屉包含2个不同字符：注意特判，将频率大于n/2的字符放在偶数位置，因为n/2等于奇数位的数量
 public:
     string reorganizeString(const string &s) {
         const int n = (int) s.size();
         unordered_map<char, int> counter;
         for (const auto &ch: s) {
-            ++counter[ch];
-            if (counter[ch] > (n + 1) / 2) {  // 会相邻，无解
+            counter[ch]++;
+            if (counter[ch] > (n + 1) / 2) {  // 相同字符会相邻，无解
                 return "";
             }
         }
-        string res(s.size(), '\0');
+        string ans(s.size(), '\0');
         int i = 1;  // 先奇数位，再偶数位
         int j = 0;
         for (const auto &item: counter) {
             auto ch = item.first;
-            if (counter[ch] == 0) {
+            if (item.second == 0) {
                 continue;
             }
             if (counter[ch] <= n / 2) {  // 放在奇数位
                 while (counter[ch] && i < n) {
-                    res[i] = ch;
-                    --counter[ch];
+                    ans[i] = ch;
+                    counter[ch]--;
                     i += 2;
                 }
             }
             while (counter[ch] && j < n) {  // 放在偶数位
-                res[j] = ch;
-                --counter[ch];
+                ans[j] = ch;
+                counter[ch]--;
                 j += 2;
             }
         }
-        return res;
+        return ans;
     }
 };
 
