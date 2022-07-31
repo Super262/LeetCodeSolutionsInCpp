a90bb19928d3a4ba9ee79c29178abbd6f1e3aa53
fengwei <virusformail@gmail.com>
2022-07-31 14:51:17 -0700

    Reviewed Problem 787.

--- Problem0787.h
+++ Problem0787.h
@@ -14,14 +14,14 @@ using namespace std;
 class Problem0787 {
     // Bellman-Ford算法查找最多包含k+1条边的最短路径，k是中间节点的个数（不包括终点和起点）
 public:
-    int findCheapestPrice(int n, vector<vector<int>> &flights, int src, int dst, int k) {
+    int findCheapestPrice(int n, const vector<vector<int>> &flights, int src, int dst, int k) {
         const int INF = 0x3f3f3f3f;
         int dist[2][n + 1];
         memset(dist[0], 0x3f, sizeof dist[0]);
         dist[0][src] = 0;
         for (int i = 1; i <= k + 1; ++i) {
             memcpy(dist[i % 2], dist[(i - 1) % 2], sizeof dist[(i - 1) % 2]);
-            for (auto &e: flights) {
+            for (const auto &e: flights) {
                 if (dist[(i - 1) % 2][e[0]] == INF) {
                     continue;
                 }
