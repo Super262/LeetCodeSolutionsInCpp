f2014b51f53e2f433c3febaedebba139e13919ce
fengwei <virusformail@gmail.com>
2022-07-31 10:54:53 -0700

    Reviewed Problem 781.

--- Problem0781.h
+++ Problem0781.h
@@ -10,13 +10,14 @@
 
 using namespace std;
 
-class Solution {
-    // https://www.acwing.com/solution/content/24647/
+class Problem0781 {
+    // 数学：统计每种数字x出现的次数num；若num是x+1的倍数，说明这num只兔子正好有num/(x+1)种颜色，需要num只兔子
+    // 否则，说明这些兔子有t=num/(x+1)+1种颜色，需要t*(x+1)只兔子
 public:
     int numRabbits(const vector<int> &nums) {
         unordered_map<int, int> counter;
         for (const auto &x: nums) {
-            ++counter[x];
+            counter[x]++;
         }
         int ans = 0;
         for (const auto &item: counter) {
