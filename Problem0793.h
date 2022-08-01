b1daae8dddaf8d6ccf0e94ad5e98b9d539f2fda2
fengwei <virusformail@gmail.com>
2022-07-31 18:31:33 -0700

    Reviewed Problem 793.

--- Problem0793.h
+++ Problem0793.h
@@ -5,7 +5,7 @@
 #ifndef LEETCODESOLUTIONSINCPP_PROBLEM0793_H
 #define LEETCODESOLUTIONSINCPP_PROBLEM0793_H
 
-class Solution {
+class Problem0793 {
     // 阶乘x!尾部0的个数是min(a, b)，a是质因子5的个数，b是质因子2的个数；由于5大于2，a等于min(a, b)
     // 我们用二分法查找满足阶乘尾部0的个数小于或等于k的数字，计作g(k)；本题的解为 g(k) - g(k - 1)
 public:
@@ -14,15 +14,6 @@ public:
     }
 
 private:
-    long long countFactorInFactorial(long long n, long long x) {
-        long long ans = 0;
-        while (n) {
-            ans += n / x;
-            n /= x;
-        }
-        return ans;
-    }
-
     long long getKZero(int k) {
         long long l = -1;  // 边界值，可以保证k=0时返回正确的结果
         long long r = 1e18;
@@ -35,6 +26,15 @@ private:
             }
         }
         return l;
+    }
+
+    long long countFactorInFactorial(long long n, long long x) {
+        long long ans = 0;
+        while (n) {
+            ans += n / x;
+            n /= x;
+        }
+        return ans;
     }
 };
 
