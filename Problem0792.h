ced0bd6deb3be6662c35e38222954891081b8864
fengwei <virusformail@gmail.com>
2022-07-31 18:23:58 -0700

    Reviewed Problem 792.

--- Problem0792.h
+++ Problem0792.h
@@ -11,8 +11,12 @@
 
 using namespace std;
 
-class Solution {
+class Problem0792 {
     // 采用类似"多路归并问题"的解决方法：记录words每个字符的指针，若匹配则后移指针
+    // 初始化：创建哈希表candidates；若words[i][0]=ch，candidates[ch]加入"pair(i,0)"
+    // 遍历s：设当前字符是ch；枚举candidates[ch]内所有pair，若pair.second+1=words[pair.first].size()成立，答案ans++
+    // 若pair.second+1<words[pair.first].size()成立，向缓存加入(pair.first,pair.second+1)；
+    // 更新：清空candidates[ch]，将缓存加入candidates
 public:
     int numMatchingSubseq(const string &s, const vector<string> &words) {
         unordered_map<char, vector<pair<int, int>>> candidates;
