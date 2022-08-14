bf1f0ca8a94857085e0bd0ec7e76bc0615b13938
fengwei <virusformail@gmail.com>
2022-08-14 14:12:29 -0700

    Reviewed Problem 821.

--- Problem0821.h
+++ Problem0821.h
@@ -10,18 +10,19 @@
 
 using namespace std;
 
-class Solution {
-    // 双指针，2次扫描
+class Problem0821 {
+    // 双指针，2次遍历；第1次遍历，从左至右，在i的左侧找到距离当前字符s[i]最近的目标字符
+    // 第2次遍历，从右至左，在i的右侧找到距离当前字符s[i]最近的目标字符
 public:
     vector<int> shortestToChar(const string &s, char ch) {
         const auto n = (int) s.size();
-        vector<int> res(n, INT_MAX);
+        vector<int> ans(n, INT_MAX);
         for (int i = 0, j = -1; i < n; ++i) {
             if (s[i] == ch) {
                 j = i;
             }
             if (j != -1) {
-                res[i] = min(res[i], i - j);
+                ans[i] = min(ans[i], i - j);
             }
         }
         for (int i = n - 1, j = n; i >= 0; --i) {
@@ -29,10 +30,10 @@ public:
                 j = i;
             }
             if (j != n) {
-                res[i] = min(res[i], j - i);
+                ans[i] = min(ans[i], j - i);
             }
         }
-        return res;
+        return ans;
     }
 };
 
