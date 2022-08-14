3c4f6badd1c21656a25d130829d24629d46e1c9c
fengwei <virusformail@gmail.com>
2022-08-13 17:06:58 -0700

    Reviewed Problem 809.

--- Problem0809.h
+++ Problem0809.h
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-class Solution {
+class Problem0809 {
     // 对s和word分别执行分段操作，得到的段长分别为a和b
     // a<b时：无解；
     // a>=b时：若a<2，a==b时有解；若a>2，有解
@@ -25,43 +25,44 @@ public:
             }
             return ans;
         }
-        vector<pair<char, int>> segs_of_s;  // 数组保存分段信息，可以记录各分段出现的先后顺序
-        for (int i = 0; i < (int) s.size(); ++i) {
-            auto j = i;
-            while (j < (int) s.size() && s[i] == s[j]) {
-                ++j;
-            }
-            segs_of_s.emplace_back(s[i], j - i);
-            i = j - 1;
-        }
+        const auto &segs_of_s = splitToSegments(s);
         for (const auto &w: words) {
-            int cnt = 0;
-            for (int i = 0; i < (int) w.size(); ++i) {
-                if (cnt == (int) segs_of_s.size()) {  // 容易被遗漏的细节！
-                    cnt = -1;
-                    break;
-                }
-                if (segs_of_s[cnt].first != w[i]) {
-                    break;
-                }
-                auto j = i;
-                while (j < (int) s.size() && w[i] == w[j]) {
-                    ++j;
-                }
-                auto a = segs_of_s[cnt].second;
-                auto b = j - i;
-                if (a < b) {
-                    break;
-                }
-                if (a < 3 && a != b) {
-                    break;
-                }
-                ++cnt;
-                i = j - 1;
+            const auto &segs_of_w = splitToSegments(w);
+            if (segs_of_w.size() != segs_of_s.size()) {
+                continue;
             }
-            if (cnt == (int) segs_of_s.size()) {
+            bool is_valid = true;
+            for (int i = 0; i < (int) segs_of_w.size(); ++i) {
+                if (segs_of_w[i].first != segs_of_s[i].first) {
+                    is_valid = false;
+                    break;
+                }
+                if (segs_of_w[i].second > segs_of_s[i].second) {
+                    is_valid = false;
+                    break;
+                }
+                if (segs_of_w[i].second < segs_of_s[i].second && segs_of_s[i].second <= 2) {
+                    is_valid = false;
+                    break;
+                }
+            }
+            if (is_valid) {
                 ++ans;
             }
+        }
+        return ans;
+    }
+
+private:
+    vector<pair<char, int>> splitToSegments(const string &s) {
+        vector<pair<char, int>> ans;
+        for (int l = 0; l < (int) s.size(); ++l) {
+            auto r = l;
+            while (r < (int) s.size() && s[l] == s[r]) {
+                ++r;
+            }
+            ans.emplace_back(s[l], r - l);
+            l = r - 1;
         }
         return ans;
     }
