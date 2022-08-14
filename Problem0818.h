3f336cc2e2e6a5c83a679b9a15fd402bb9593e08
fengwei <virusformail@gmail.com>
2022-08-14 13:30:58 -0700

    Reviewed Problem 818.

--- Problem0818.h
+++ Problem0818.h
@@ -13,7 +13,8 @@ using namespace std;
 class Problem0818 {
     // BFS+剪枝；建立3维的状态：(pos,spd,dir)，表示当前位置为pos、速度为2^spd、方向为dir（true：正向，false：反向）
     // 哈希表dist指示(pos,spd,dir)距起点的距离；注意掌握自定义数据结构的哈希函数实现
-    // 剪枝：进入负位置是无意义的；越过target也是无意义的；搜索范围为[0, target]；时间复杂度为O(target*log(target))
+    // 剪枝：进入负位置是无意义的，因为总可以找到一个更快得不进入负位置的答案；越过2*target也是无意义的，因为我们倒车不可能超过之前一步走的距离
+    // 搜索范围为[0,2*target]；时间复杂度为O(target*log(target))
 public:
     int racecar(const int target) {
         queue<Node> q;
@@ -28,12 +29,12 @@ public:
                 return dist[t];
             }
             Node ne_a(t.pos + (t.dir ? 1 : -1) * (1 << t.spd), t.spd + 1, t.dir);  // 操作A后的状态
-            if (ne_a.pos >= 0 && ne_a.pos <= (target << 1) && (!dist.count(ne_a) || dist[ne_a] > dist[t] + 1)) {
+            if (ne_a.pos >= 0 && ne_a.pos <= (target * 2) && (!dist.count(ne_a) || dist[ne_a] > dist[t] + 1)) {
                 q.emplace(ne_a);
                 dist[ne_a] = dist[t] + 1;
             }
             Node ne_r(t.pos, 0, !t.dir);  // 操作R后的状态
-            if (ne_r.pos >= 0 && ne_r.pos <= (target << 1) && (!dist.count(ne_r) || dist[ne_r] > dist[t] + 1)) {
+            if (ne_r.pos >= 0 && ne_r.pos <= (target * 2) && (!dist.count(ne_r) || dist[ne_r] > dist[t] + 1)) {
                 q.emplace(ne_r);
                 dist[ne_r] = dist[t] + 1;
             }
