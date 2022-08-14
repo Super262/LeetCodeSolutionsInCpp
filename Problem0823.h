5f422af637b0ea2a22079ffc6f37f5fe4e9fec4e
fengwei <virusformail@gmail.com>
2022-08-14 14:44:17 -0700

    Reviewed Problem 823.

--- Problem0823.h
+++ Problem0823.h
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-class Solution {
+class Problem0823 {
     // 动态规划；f[i]：以有序数组第i个元素为根的树的个数
     // 设d=arr[i]/arr[j]，若arr[i]是arr[j]的倍数，且d存在于arr中，f[i]+=f[j]*f[idx(d)]
     // 由于我们要遍历比当前元素arr[i]小的元素arr[j]，我们应先把arr升序排序，简化搜索过程，同时不增加复杂度
