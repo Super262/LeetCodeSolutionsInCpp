77ccecbb9ddafa0392b493be9de177222321cd83
fengwei <wuzhongfengwei@163.com>
2022-09-07 17:28:50 -0700

    Reviewed Problem 1034.

--- Problem1034.h
+++ Problem1034.h
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-class Solution {
+class Problem1034 {
     // BFS搜索连通块，并标记边界点
 public:
     vector<vector<int>> colorBorder(vector<vector<int>> &grid, int row, int col, int color) {
@@ -19,10 +19,10 @@ public:
         const auto n = (int) grid[0].size();
         const int dx[4] = {0, 1, 0, -1};
         const int dy[4] = {1, 0, -1, 0};
-        int ans[m][n];  // 0：未被访问；1：被访问；2：被访问，是边界
+        int mark[m][n];  // 0：未被访问；1：被访问；2：被访问，是边界
         queue<pair<int, int>> q;
-        memset(ans, 0, sizeof ans);
-        ans[row][col] = 1;
+        memset(mark, 0, sizeof mark);
+        mark[row][col] = 1;
         q.emplace(row, col);
         while (!q.empty()) {  // BFS
             auto u = q.front();
@@ -33,19 +33,19 @@ public:
                 auto nx = x + dx[i];
                 auto ny = y + dy[i];
                 if (nx < 0 || ny < 0 || nx >= m || ny >= n || grid[nx][ny] != grid[row][col]) {
-                    ans[x][y] = 2;
+                    mark[x][y] = 2;
                     continue;
                 }
-                if (ans[nx][ny]) {
+                if (mark[nx][ny]) {
                     continue;
                 }
-                ans[nx][ny] = 1;
+                mark[nx][ny] = 1;
                 q.emplace(nx, ny);
             }
         }
         for (int i = 0; i < m; ++i) {  // 染色
             for (int j = 0; j < n; ++j) {
-                if (ans[i][j] != 2) {
+                if (mark[i][j] != 2) {
                     continue;
                 }
                 grid[i][j] = color;
