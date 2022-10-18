0387cf52963989a9013b324a9eebe1b4d2ea6c39
fengwei <virusformail@gmail.com>
2022-10-18 02:43:57 -0700

    Reviewed Problem 1017.

--- Problem1017.h
+++ Problem1017.h
@@ -9,10 +9,11 @@
 
 using namespace std;
 
-class Solution {
-    // 短除法：注意区分C++中取余运算和数学中取余运算的区别
-    // 为了使余数为正数，我们可以将余数加上基数的绝对值保证其为正数，同时更新商为：(被除数-余数) / 除数
-    // https://www.acwing.com/solution/content/9902/
+class Problem1017 {
+    // 如果n=0，直接返回"0"；若n!=0，重复执行如下操作，直到n=0：
+    //    (1) 计算n的绝对值除以-2的余数，确保余数是0或1，将余数拼接到字符串的末尾
+    //    (2) 将n的值减去余数，然后将n的值除以−2
+    // 上述操作结束之后，字符串为负二进制数反转之后的结果，将字符串翻转之后得到负二进制数
 public:
     string baseNeg2(int n) {
         if (!n) {
@@ -20,9 +21,10 @@ public:
         }
         string ans;
         while (n) {
-            auto r = ((n % (-2)) + 2) % 2;
-            n = (n - r) / -2;
+            auto r = abs(n) % (-2);
             ans += (char) ('0' + r);
+            n -= r;
+            n /= -2;
         }
         reverse(ans.begin(), ans.end());
         return ans;
