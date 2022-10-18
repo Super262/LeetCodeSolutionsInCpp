ea1a166b3143d4acfe96963f4edd863cd12b34f1
fengwei <virusformail@gmail.com>
2022-10-18 14:54:19 -0700

    Reviewed Problem 1008.

--- Problem1008.h
+++ Problem1008.h
@@ -6,26 +6,12 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM1008_H
 
 #include <vector>
+#include "treenode.h"
 
 using namespace std;
 
-/**
- * Definition for a binary tree node.
- */
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
-class Solution {
-    // 二分查找左右子树的分界线
+class Problem1008 {
+    // 二分查找左右子树的分界线，递归构造BST，类似先序遍历
 public:
     TreeNode *bstFromPreorder(const vector<int> &preorder) {
         if (preorder.empty()) {
