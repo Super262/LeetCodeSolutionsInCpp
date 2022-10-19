4d58c047c2195d7fbf3b9dafaa53dd0c9aa27c05
fengwei <virusformail@gmail.com>
2022-10-19 03:38:58 -0700

    Reviewed Problem 988.

--- Problem0988.h
+++ Problem0988.h
@@ -6,26 +6,12 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0988_H
 
 #include <string>
+#include "treenode.h"
 
 using namespace std;
 
-/**
- * Definition for a binary tree node.
- */
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
-class Solution {
-    // 暴力法：遍历出所有的字符串，逐个比较
+class Problem0988 {
+    // 暴力法：；类似先序遍历，遍历出所有的字符串，逐个比较
 public:
     string smallestFromLeaf(TreeNode *root) {
         if (!root) {
