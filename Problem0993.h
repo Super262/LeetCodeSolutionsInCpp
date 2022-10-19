1279cedca57688556af154cb28b45f68c7c44017
fengwei <virusformail@gmail.com>
2022-10-19 02:17:26 -0700

    Reviewed Problem 993.

--- Problem0993.h
+++ Problem0993.h
@@ -6,25 +6,11 @@
 #define LEETCODESOLUTIONSINCPP_PROBLEM0993_H
 
 #include <algorithm>
+#include "treenode.h"
 
 using namespace std;
 
-/**
- * Definition for a binary tree node.
- */
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
-class Solution {
+class Problem0993 {
     // 直接遍历，分别寻找x、y的父节点和深度
 public:
     bool isCousins(TreeNode *root, int x, int y) {
