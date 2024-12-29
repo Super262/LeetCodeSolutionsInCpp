61d00dcdaed40f5fc8eacf20468c292fc469b6b9
Super262 <fengweiz929@yahoo.com>
2024-12-29 02:32:07 -0800

    Updated 912.

--- Problem0912.h
+++ Problem0912.h
@@ -11,29 +11,46 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> sortArray(vector<int> &nums) {
-        const auto n = (int) nums.size();
-        quick_sort(nums, 0, n - 1);
+    vector<int> sortArray(vector<int>& nums) {
+        myQuickSort(nums, 0, nums.size() - 1);
         return nums;
     }
 
 private:
-    void quick_sort(vector<int> &nums, int st, int ed) {
+    void myQuickSort(vector<int>& nums, size_t st, size_t ed) {
         if (st >= ed) {
-            return;
+            return; // No need to sort
         }
-        auto l = st - 1;
-        auto r = ed + 1;
-        auto p = nums[l + (r - l) / 2];
-        while (l < r) {
-            while (nums[++l] < p);
-            while (nums[--r] > p);
+
+        size_t p = myPartition(nums, st, ed); // Get pivot index
+        if (p > 0) { // Ensure no underflow when subtracting 1
+            myQuickSort(nums, st, p - 1); // Recursively sort left part
+        }
+        myQuickSort(nums, p + 1, ed);     // Recursively sort right part
+    }
+
+    size_t myPartition(vector<int>& nums, size_t st, size_t ed) {
+        size_t p = st + (ed - st) / 2; // Choose middle as pivot
+        int pivot = nums[p];          // Store the pivot value
+        size_t l = st + 1;
+        size_t r = ed;
+
+        swap(nums[p], nums[st]); // Move pivot to start
+
+        while (l <= r) {
+            while (l <= r && nums[l] <= pivot) {
+                ++l;
+            }
+            while (l <= r && nums[r] >= pivot) {
+                --r;
+            }
             if (l < r) {
                 swap(nums[l], nums[r]);
             }
         }
-        quick_sort(nums, st, r);
-        quick_sort(nums, r + 1, ed);
+
+        swap(nums[st], nums[r]); // Place pivot in its correct position
+        return r; // Return the partition index
     }
 };
 
