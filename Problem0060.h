94383d55e987c8ed588d9d202950e88414c04720
Eric (Fengwei) Zhang <42017768+Super262@users.noreply.github.com>
2026-09-01 21:40:29 -0700

    Refactor getPermutation method for clarity and efficiency

--- Problem0060.h
+++ Problem0060.h
@@ -12,35 +12,37 @@
 using namespace std;
 
 class Problem0060 {
-    // 我们从高位到低位依次生成n个数，形成最终结果ans；设i的阶乘为f(i)
-    // 对于结果ans中的第i位（0<=i<=n-1），若尝试将第i位设置为x，若k>f(i)，我们应尝试x+1，并更新k=k-f(n-i-1)
-    // 若k<=f(n-i-1)，设置第i位为x，标记x以避免重复，继续尝试第i+1位
 public:
-    string getPermutation(int n, int k) {
-        int factorial[n + 1];  // 预计算n!（由n个数字组成的、各位不重复的n位数有n!个）
-        factorial[0] = 1;
-        for (int i = 1; i <= n; ++i) {
-            factorial[i] = factorial[i - 1] * i;
+    string getPermutation(int n, int k)
+    {
+        /* （逆）康托展开：https://zhuanlan.zhihu.com/p/109700398 */
+        vector<int> fact;
+        vector<int> nums;
+        string answer;
+    
+        nums.reserve(n);
+        fact.reserve(n + 1);
+        fact.emplace_back(1);
+        answer.reserve(n + 1);
+
+        for (int i = 1; i <= n; ++i)
+        {
+            nums.emplace_back(i);
+            fact.emplace_back(fact.back() * i);
         }
-        bool used[n + 1];  // 标识当前数字是否已经被使用过
-        string ans;
-        ans.reserve(n);
-        memset(used, 0, sizeof used);
-        for (int i = 0; i < n; ++i) {  // 从高位到低位，填充结果
-            for (int digit = 1; digit <= n; ++digit) {  // 由小到大，尝试每个数字
-                if (used[digit]) {
-                    continue;
-                }
-                if (k > factorial[n - i - 1]) {  // 检测k是否大于以当前数字开头的(n-i-1)位数的个数
-                    k -= factorial[n - i - 1];
-                    continue;
-                }
-                ans += (char) ('0' + digit);
-                used[digit] = true;
-                break;
-            }
+        
+        /* 知乎文章里的k是0-based，这里的输入是1-based，所以我们先减1 */
+        --k；
+        for (int i = n, idx, r; i > 0; --i)
+        {
+            idx = k / fact[i - 1];
+            r = k % fact[i - 1];
+            answer += (char)(nums[idx] + '0');
+            nums.erase(nums.begin() + idx);
+            k = r;
         }
-        return ans;
+
+        return answer;
     }
 };
 
