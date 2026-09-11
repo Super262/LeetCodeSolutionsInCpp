225b472fda92325e7e6000a6be72684e2e13797c
Eric (Fengwei) Zhang <42017768+Super262@users.noreply.github.com>
2026-09-10 23:35:28 -0700

    Add Problem2585 class with waysToReachTarget method
    Implement a class for solving problem 2585 with a method to calculate the number of ways to reach a target using given types.

---
+++ Problem2585.h
@@ -0,0 +1,41 @@
+//
+// Created by Fengwei Zhang on 9/10/26.
+//
+
+#ifndef LEETCODESOLUTIONSINCPP_PROBLEM2585_H
+#define LEETCODESOLUTIONSINCPP_PROBLEM2585_H
+
+#include <vector>
+#include <cstring>
+
+using namespace std;
+
+#define PROBLEM_2585_MOD (1000000007)
+class Problem2585
+{
+public:
+    int waysToReachTarget(int target, const vector<vector<int>> &types)
+    {
+        int *f = (int *)calloc(target + 1, sizeof(int));
+        int j, k, answer;
+
+        f[0] = 1;
+        for (const vector<int> &item : types)
+        {
+            for (j = target; j; --j)
+            {
+                for (k = 1; k <= item[0] && k * item[1] <= j; ++k)
+                {
+                    if (f[j - k * item[1]])
+                        f[j] = (int)((long)((f[j] + f[j - k * item[1]]) % PROBLEM_2585_MOD));
+                }
+            }
+        }
+
+        answer = f[target];
+        free(f);
+        return answer;
+    }
+};
+#undef PROBLEM_2585_MOD
+#endif // LEETCODESOLUTIONSINCPP_PROBLEM2585_H
